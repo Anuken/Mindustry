d6286006576cf29b6580d2f13fab0f59f7667744
Anuken <arnukren@gmail.com>
2017-12-29 23:05:52 -0500

    Added some new block types, fixed some save dialog issues

---
+++ core/src/io/anuke/mindustry/world/blocks/types/production/LiquidDrill.java
@@ -0,0 +1,10 @@
+package io.anuke.mindustry.world.blocks.types.production;
+
+import io.anuke.mindustry.world.Block;
+
+public class LiquidDrill extends Block {
+
+    public LiquidDrill(String name) {
+        super(name);
+    }
+}
