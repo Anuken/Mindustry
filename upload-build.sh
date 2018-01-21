eac6a4a67f82a93c2863908d165f4d3eed787c3d
Epowerj <epowerj@gmail.com>
2018-01-21 12:30:50 -0500

    Fixed syntax in upload_build.sh

--- upload-build.sh
+++ upload-build.sh
@@ -30,7 +30,7 @@ OLD_TRAVIS_BUILD_NUMBER=`expr $TRAVIS_BUILD_NUMBER - 7`
 OLD_DESKFILE=$OLD_TRAVIS_BUILD_BUMBER"-desktop-bleeding-edge.jar"
 OLD_FILE1="Bleeding-Edge-Build-$OLD_TRAVIS_BUILD_NUMBER.md"
 
-if [ -e $OLD_FILE1 ]; then
+if [ -e "$OLD_FILE1" ]; then
     rm -f $OLD_FILE1
     rm -f $OLD_DESKFILE
     git add $OLD_FILE1
