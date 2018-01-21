f8514a0f3de72eafa9e6d46d4729a07a09a0312c
Anuken <arnukren@gmail.com>
2018-01-20 23:01:54 -0500

    Changed application icon

--- core/src/io/anuke/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/io/anuke/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -81,8 +81,10 @@ public class SettingsMenuDialog extends SettingsDialog{
 		menu.addButton("$text.settings.graphics", () -> visible(1));
 		menu.row();
 		menu.addButton("$text.settings.sound", () -> visible(2));
-		menu.row();
-		menu.addButton("$text.settings.controls", Vars.ui.controls::show);
+		if(!Vars.android) {
+			menu.row();
+			menu.addButton("$text.settings.controls", Vars.ui.controls::show);
+		}
 		menu.row();
 		menu.addButton("$text.settings.language", Vars.ui.language::show);
 
