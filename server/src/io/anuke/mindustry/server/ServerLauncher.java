b7acab42b14e0a3ae656b496b1946d344475c1d9
Anuken <arnukren@gmail.com>
2018-01-28 23:28:15 -0500

    Made server exit on libGDX crash

--- server/src/io/anuke/mindustry/server/ServerLauncher.java
+++ server/src/io/anuke/mindustry/server/ServerLauncher.java
@@ -7,14 +7,16 @@ import io.anuke.mindustry.MindustryServer;
 import io.anuke.mindustry.net.Net;
 import io.anuke.ucore.util.Log;
 
+import java.lang.reflect.Method;
+
 public class ServerLauncher{
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
 
         Net.setClientProvider(new KryoClient());
         Net.setServerProvider(new KryoServer());
 
-        new HeadlessApplication(new MindustryServer()){
+        HeadlessApplication app = new HeadlessApplication(new MindustryServer()){
             @Override
             public boolean executeRunnables() {
                 try {
@@ -26,6 +28,30 @@ public class ServerLauncher{
                 }
             }
         };
+
+        Method method = app.getClass().getDeclaredMethod("mainLoop");
+        method.setAccessible(true);
+
+        //kill default libGDX thread
+        for(Thread thread : Thread.getAllStackTraces().keySet()){
+            if(thread.getName().equals("\"HeadlessApplication\"")){
+                thread.interrupt();
+            }
+        }
+
+        //replace it with my own
+        Thread mainLoopThread = new Thread("HeadlessApplication") {
+            @Override
+            public void run () {
+                try {
+                    method.invoke(app);
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+        };
+        mainLoopThread.start();
 
     }
 }
