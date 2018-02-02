39c304815cc05f9ac85878c4487e6e05759032c8
Epowerj <epowerj@gmail.com>
2018-02-02 15:24:45 -0500

    Man I hate bash

--- upload-build.sh
+++ upload-build.sh
@@ -17,8 +17,7 @@ if [ ! -e $FILE1 ]; then
     touch $FILE1
 fi
 
-NEWLINE="\n"
-echo "### Commit #"$TRAVIS_COMMIT".${NEWLINE}Desktop JAR download: [Link]("$DESKFILE")" >> $FILE1
+echo "### Commit #"$TRAVIS_COMMIT"   Desktop JAR download: [Link]("$DESKFILE")" >> $FILE1
 
 git add $FILE1
 git add $DESKFILE
