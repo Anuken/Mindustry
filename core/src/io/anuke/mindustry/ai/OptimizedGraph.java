beded95ffaa173a8691022f5b7c66e8ec7bd3694
Anuken <arnukren@gmail.com>
2018-02-01 19:31:13 -0500

    Updated Gradle to 4.5

--- core/src/io/anuke/mindustry/ai/OptimizedGraph.java
+++ core/src/io/anuke/mindustry/ai/OptimizedGraph.java
@@ -5,5 +5,5 @@ import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
 /**An interface for an indexed graph that doesn't use allocations for connections.*/
 public interface OptimizedGraph<N> extends IndexedGraph<N> {
     /**This is used in the same way as getConnections(), but does not use Connection objects.*/
-    public N[] connectionsOf(N node);
+    N[] connectionsOf(N node);
 }
