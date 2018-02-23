de340255fea4f4cc85c934cbab6774ab6ed0493c
Anuken <arnukren@gmail.com>
2018-02-23 15:11:42 -0500

    Updated to build 28

--- core/src/io/anuke/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/io/anuke/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -32,16 +32,19 @@ public class SettingsMenuDialog extends SettingsDialog{
 	public SettingsMenuDialog(){
 		setStyle(Core.skin.get("dialog", WindowStyle.class));
 
-		hidden(()->{
+		hidden(() -> {
 			if(!state.is(State.menu)){
 				if(!wasPaused || Net.active())
 					state.set(State.playing);
 			}
 		});
 
-		shown(()->{
+		shown(() -> {
 			if(!state.is(State.menu)){
 				wasPaused = state.is(State.paused);
+				if(ui.paused.getScene() != null){
+					wasPaused = ui.paused.wasPaused;
+				}
 				if(!Net.active()) state.set(State.paused);
 				ui.paused.hide();
 			}
