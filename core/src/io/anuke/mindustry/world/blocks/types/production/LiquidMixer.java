539971b5a069bfb31bb96e17e060f1ee3a611715
Anuken <arnukren@gmail.com>
2018-03-13 11:38:56 -0400

    Changed oil extractor sprite

---
+++ core/src/io/anuke/mindustry/world/blocks/types/production/LiquidMixer.java
@@ -0,0 +1,10 @@
+package io.anuke.mindustry.world.blocks.types.production;
+
+import io.anuke.mindustry.world.blocks.types.LiquidBlock;
+
+public class LiquidMixer extends LiquidBlock{
+
+    public LiquidMixer(String name) {
+        super(name);
+    }
+}
