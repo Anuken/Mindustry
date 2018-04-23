19d058a644adb4f5a47a016d15e831a9547ae8c9
Anuken <arnukren@gmail.com>
2018-04-23 19:14:52 -0400

    More base unit types added

--- core/src/io/anuke/mindustry/entities/units/types/Reaper.java
+++ core/src/io/anuke/mindustry/entities/units/types/Reaper.java
@@ -3,6 +3,7 @@ package io.anuke.mindustry.entities.units.types;
 import io.anuke.mindustry.entities.units.FlyingUnitType;
 
 public class Reaper extends FlyingUnitType {
+
     public Reaper(String name) {
         super(name);
     }
--- core/src/io/anuke/mindustry/entities/units/types/Vtol.java
+++ core/src/io/anuke/mindustry/entities/units/types/Vtol.java
@@ -1,11 +1,22 @@
 package io.anuke.mindustry.entities.units.types;
 
+import io.anuke.mindustry.entities.Unit;
+import io.anuke.mindustry.entities.units.BaseUnit;
 import io.anuke.mindustry.entities.units.FlyingUnitType;
+import io.anuke.ucore.graphics.Draw;
 
 public class Vtol extends FlyingUnitType {
 
     public Vtol(){
-        super("flier");
+        super("vtol");
     }
 
+    @Override
+    public void draw(BaseUnit unit) {
+        Draw.alpha(unit.hitTime / Unit.hitDuration);
+
+        Draw.rect(name, unit.x, unit.y, unit.rotation - 90);
+
+        Draw.alpha(1f);
+    }
 }
