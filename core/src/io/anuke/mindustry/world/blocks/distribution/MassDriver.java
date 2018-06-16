6281826b92d96aeff54eb384992d2fa679421db3
Anuken <arnukren@gmail.com>
2018-06-15 21:54:26 -0400

    Updated uCore

--- core/src/io/anuke/mindustry/world/blocks/distribution/MassDriver.java
+++ core/src/io/anuke/mindustry/world/blocks/distribution/MassDriver.java
@@ -3,8 +3,11 @@ package io.anuke.mindustry.world.blocks.distribution;
 import io.anuke.mindustry.world.Block;
 
 public class MassDriver extends Block {
+    protected float range;
 
     public MassDriver(String name) {
         super(name);
+        update = true;
+        solid = true;
     }
 }
