25051980ce265707ee82242297c425786192466f
Anuken <arnukren@gmail.com>
2018-08-17 14:51:18 -0400

    Fixed category bundle properties missing

--- core/src/io/anuke/mindustry/input/DefaultKeybinds.java
+++ core/src/io/anuke/mindustry/input/DefaultKeybinds.java
@@ -16,7 +16,7 @@ public class DefaultKeybinds{
         for(String section : sections){
 
             KeyBinds.defaultSection(section, DeviceType.keyboard,
-                    new Category("General"),
+                    new Category("general"),
                     "move_x", new Axis(Input.A, Input.D),
                     "move_y", new Axis(Input.S, Input.W),
                     //"select", Input.MOUSE_LEFT,
@@ -25,14 +25,14 @@ public class DefaultKeybinds{
                     "rotate", new Axis(Input.SCROLL),
                     "dash", Input.SHIFT_LEFT,
                     "drop_unit", Input.SHIFT_LEFT,
-                    new Category("View"),
+                    new Category("view"),
                     "zoom_hold", Input.CONTROL_LEFT,
                     "zoom", new Axis(Input.SCROLL),
                     "zoom_minimap", new Axis(Input.MINUS, Input.PLUS),
                     "menu", Gdx.app.getType() == ApplicationType.Android ? Input.BACK : Input.ESCAPE,
                     "pause", Input.SPACE,
                     "toggle_menus", Input.C,
-                    new Category("Multiplayer"),
+                    new Category("multiplayer"),
                     "player_list", Input.TAB,
                     "chat", Input.ENTER,
                     "chat_history_prev", Input.UP,
@@ -42,7 +42,7 @@ public class DefaultKeybinds{
             );
 
             KeyBinds.defaultSection(section, DeviceType.controller,
-                    new Category("General"),
+                    new Category("general"),
                     "move_x", new Axis(Input.CONTROLLER_L_STICK_HORIZONTAL_AXIS),
                     "move_y", new Axis(Input.CONTROLLER_L_STICK_VERTICAL_AXIS),
                     "cursor_x", new Axis(Input.CONTROLLER_R_STICK_HORIZONTAL_AXIS),
@@ -53,12 +53,12 @@ public class DefaultKeybinds{
                     "dash", Input.CONTROLLER_Y,
                     "rotate_alt", new Axis(Input.CONTROLLER_DPAD_RIGHT, Input.CONTROLLER_DPAD_LEFT),
                     "rotate", new Axis(Input.CONTROLLER_A, Input.CONTROLLER_B),
-                    new Category("View"),
+                    new Category("view"),
                     "zoom_hold", Input.ANY_KEY,
                     "zoom", new Axis(Input.CONTROLLER_DPAD_DOWN, Input.CONTROLLER_DPAD_UP),
                     "menu", Input.CONTROLLER_X,
                     "pause", Input.CONTROLLER_L_TRIGGER,
-                    new Category("Multiplayer"),
+                    new Category("multiplayer"),
                     "player_list", Input.CONTROLLER_START
             );
 
