0c63b27eea237391b61c0d8ad92178d5467ffe3f
Anuken <arnukren@gmail.com>
2018-09-18 09:17:50 -0400

    Removed old korean font / Fixed targeting going through UI

--- core/src/io/anuke/mindustry/input/MobileInput.java
+++ core/src/io/anuke/mindustry/input/MobileInput.java
@@ -546,13 +546,13 @@ public class MobileInput extends InputHandler implements GestureListener{
 
         float worldx = Graphics.world(x, y).x, worldy = Graphics.world(x, y).y;
 
-        checkTargets(worldx, worldy);
-
         //get tile on cursor
         Tile cursor = tileAt(x, y);
 
         //ignore off-screen taps
         if(cursor == null || ui.hasMouse(x, y)) return false;
+
+        checkTargets(worldx, worldy);
 
         //remove if request present
         if(hasRequest(cursor)){
