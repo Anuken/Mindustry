ee5a3269f28b2356ffbb32d7a9ffaf2c7ce13702
Anuken <arnukren@gmail.com>
2018-10-03 21:17:36 -0400

    Failure confirmed working

--- tests/src/test/java/ApplicationTests.java
+++ tests/src/test/java/ApplicationTests.java
@@ -95,11 +95,6 @@ public class ApplicationTests{
     }
 
     @Test
-    void tryFail(){
-        fail("a");
-    }
-
-    @Test
     void initialization(){
         assertTrue(logic != null);
         assertTrue(world != null);
