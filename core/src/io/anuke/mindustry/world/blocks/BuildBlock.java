d1bb073112f87e5b4c15627ec7a289161029775c
LQ <islqislq@gmail.com>
2018-10-13 15:26:11 +0800

    fire BlockBuildCompletEvent onConstructFinish

--- core/src/io/anuke/mindustry/world/blocks/BuildBlock.java
+++ core/src/io/anuke/mindustry/world/blocks/BuildBlock.java
@@ -10,6 +10,7 @@ import io.anuke.mindustry.entities.TileEntity;
 import io.anuke.mindustry.entities.Unit;
 import io.anuke.mindustry.entities.effect.RubbleDecal;
 import io.anuke.mindustry.entities.traits.BuilderTrait.BuildRequest;
+import io.anuke.mindustry.game.EventType;
 import io.anuke.mindustry.game.Team;
 import io.anuke.mindustry.gen.Call;
 import io.anuke.mindustry.graphics.Layer;
@@ -24,6 +25,7 @@ import io.anuke.mindustry.world.Tile;
 import io.anuke.mindustry.world.meta.BlockBar;
 import io.anuke.mindustry.world.modules.InventoryModule;
 import io.anuke.ucore.core.Effects;
+import io.anuke.ucore.core.Events;
 import io.anuke.ucore.core.Graphics;
 import io.anuke.ucore.graphics.Draw;
 import io.anuke.ucore.util.Mathf;
@@ -59,6 +61,8 @@ public class BuildBlock extends Block{
         world.setBlock(tile, block, team);
         Effects.effect(Fx.placeBlock, tile.drawx(), tile.drawy(), block.size);
         threads.runDelay(() -> tile.block().placed(tile));
+
+        threads.runDelay(() -> Events.fire(new EventType.BlockBuildCompletEvent(tile, team)));
 
         //last builder was this local client player, call placed()
         if(!headless && builderID == players[0].id){
