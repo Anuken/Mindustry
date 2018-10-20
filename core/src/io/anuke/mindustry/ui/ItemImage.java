85cde62039a49e4d2f14a435530aa28495de66f2
Anuken <arnukren@gmail.com>
2018-10-20 13:53:25 -0400

    Fixed phase weaver blink / Fixed item image offset (again)

--- core/src/io/anuke/mindustry/ui/ItemImage.java
+++ core/src/io/anuke/mindustry/ui/ItemImage.java
@@ -14,7 +14,7 @@ public class ItemImage extends Stack{
     public ItemImage(TextureRegion region, Supplier<CharSequence> text){
         Table t = new Table().left().bottom();
 
-        t.label(text).color(Color.DARK_GRAY).padBottom(-26).get().setFontScale(Unit.dp.scl(0.5f));
+        t.label(text).color(Color.DARK_GRAY).padBottom(-20).get().setFontScale(Unit.dp.scl(0.5f));
         t.row();
         t.label(text).get().setFontScale(Unit.dp.scl(0.5f));
 
@@ -25,7 +25,7 @@ public class ItemImage extends Stack{
     public ItemImage(ItemStack stack){
         Table t = new Table().left().bottom();
 
-        t.add(stack.amount + "").color(Color.DARK_GRAY).padBottom(-26).get().setFontScale(Unit.dp.scl(0.5f));
+        t.add(stack.amount + "").color(Color.DARK_GRAY).padBottom(-20).get().setFontScale(Unit.dp.scl(0.5f));
         t.row();
         t.add(stack.amount + "").get().setFontScale(Unit.dp.scl(0.5f));
 
--- core/src/io/anuke/mindustry/world/blocks/production/PhaseWeaver.java
+++ core/src/io/anuke/mindustry/world/blocks/production/PhaseWeaver.java
@@ -40,30 +40,27 @@ public class PhaseWeaver extends PowerSmelter{
 
         Draw.rect(bottomRegion, tile.drawx(), tile.drawy());
 
-        if(entity.heat > 0.001f){
-            float progress = 0.5f;
+        float progress = 0.5f;
 
-            Shaders.build.region = weaveRegion;
-            Shaders.build.progress = progress;
-            Shaders.build.color.set(Palette.accent);
-            Shaders.build.color.a = entity.heat;
-            Shaders.build.time = -entity.time / 10f;
+        Shaders.build.region = weaveRegion;
+        Shaders.build.progress = progress;
+        Shaders.build.color.set(Palette.accent);
+        Shaders.build.time = -entity.time / 10f;
 
-            Graphics.shader(Shaders.build, false);
-            Shaders.build.apply();
-            Draw.rect(weaveRegion, tile.drawx(), tile.drawy(), entity.time);
-            Graphics.shader();
+        Graphics.shader(Shaders.build, false);
+        Shaders.build.apply();
+        Draw.rect(weaveRegion, tile.drawx(), tile.drawy(), entity.time);
+        Graphics.shader();
 
-            Draw.color(Palette.accent);
+        Draw.color(Palette.accent);
 
-            Lines.lineAngleCenter(
-                    tile.drawx() + Mathf.sin(entity.time, 6f, Vars.tilesize / 3f * size),
-                    tile.drawy(),
-                    90,
-                    size * Vars.tilesize / 2f);
+        Lines.lineAngleCenter(
+                tile.drawx() + Mathf.sin(entity.time, 6f, Vars.tilesize / 3f * size),
+                tile.drawy(),
+                90,
+                size * Vars.tilesize / 2f);
 
-            Draw.reset();
-        }
+        Draw.reset();
 
         Draw.rect(region, tile.drawx(), tile.drawy());
     }
