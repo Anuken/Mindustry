3925d5ec27d607f2079f761d88a475f27b124dc6
Anuken <arnukren@gmail.com>
2018-10-29 15:41:13 -0400

    Removed useless power modules methods

--- core/src/io/anuke/mindustry/world/modules/BlockModule.java
+++ core/src/io/anuke/mindustry/world/modules/BlockModule.java
@@ -4,6 +4,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+/**A class that represents compartmentalized tile entity state.*/
 public abstract class BlockModule{
     public abstract void write(DataOutput stream) throws IOException;
 
