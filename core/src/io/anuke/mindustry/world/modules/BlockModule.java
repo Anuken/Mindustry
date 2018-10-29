b39f25ed519b831546da4203f1d4b443780c4e63
Anuken <arnukren@gmail.com>
2018-10-29 16:02:09 -0400

    Fixed build error

--- core/src/io/anuke/mindustry/world/modules/BlockModule.java
+++ core/src/io/anuke/mindustry/world/modules/BlockModule.java
@@ -7,6 +7,5 @@ import java.io.IOException;
 /**A class that represents compartmentalized tile entity state.*/
 public abstract class BlockModule{
     public abstract void write(DataOutput stream) throws IOException;
-
     public abstract void read(DataInput stream) throws IOException;
 }
