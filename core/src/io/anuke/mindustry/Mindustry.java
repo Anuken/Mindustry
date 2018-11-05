995476db50199000b37e9d70d45eea507d785248
LQ <islqislq@gmail.com>
2018-11-05 14:34:37 +0800

    change to game/screen mode of libgdx

--- core/src/io/anuke/mindustry/Mindustry.java
+++ core/src/io/anuke/mindustry/Mindustry.java
@@ -1,47 +1,10 @@
 package io.anuke.mindustry;
 
-import io.anuke.mindustry.core.*;
-import io.anuke.mindustry.game.EventType.GameLoadEvent;
-import io.anuke.mindustry.io.BundleLoader;
-import io.anuke.ucore.core.Events;
-import io.anuke.ucore.core.Timers;
-import io.anuke.ucore.modules.ModuleCore;
-import io.anuke.ucore.util.Log;
+import com.badlogic.gdx.Game;
 
-import static io.anuke.mindustry.Vars.*;
-
-public class Mindustry extends ModuleCore{
-
+public class Mindustry extends Game {
     @Override
-    public void init(){
-        Timers.mark();
-
-        Vars.init();
-
-        Log.setUseColors(false);
-        BundleLoader.load();
-        content.load();
-
-        module(logic = new Logic());
-        module(world = new World());
-        module(control = new Control());
-        module(renderer = new Renderer());
-        module(ui = new UI());
-        module(netServer = new NetServer());
-        module(netClient = new NetClient());
+    public void create() {
+        setScreen(new SplashScreen(this));
     }
-
-    @Override
-    public void postInit(){
-        Log.info("Time to load [total]: {0}", Timers.elapsed());
-        Events.fire(new GameLoadEvent());
-    }
-
-    @Override
-    public void render(){
-        threads.handleBeginRender();
-        super.render();
-        threads.handleEndRender();
-    }
-
 }
