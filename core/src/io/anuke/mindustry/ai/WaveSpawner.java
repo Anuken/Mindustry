8d28976b0d54dcc9c1114ef3fe6bed5982d2ac4f
Anuken <arnukren@gmail.com>
2018-12-05 19:00:10 -0500

    Misc cleanup

--- core/src/io/anuke/mindustry/ai/WaveSpawner.java
+++ core/src/io/anuke/mindustry/ai/WaveSpawner.java
@@ -164,7 +164,7 @@ public class WaveSpawner{
             for(int y = quady * quadsize; y < world.height() && y < (quady + 1) * quadsize; y++){
                 Tile tile = world.tile(x, y);
 
-                if(tile == null || tile.solid() || world.pathfinder.getValueforTeam(Team.red, x, y) == Float.MAX_VALUE || tile.floor().isLiquid){
+                if(tile == null || tile.solid() || tile.getTeam() == defaultTeam || world.pathfinder.getValueforTeam(Team.red, x, y) == Float.MAX_VALUE || tile.floor().isLiquid){
                     setQuad(quadx, quady, false);
                     break outer;
                 }
