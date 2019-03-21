74f16d0137560c792b4094659a13441ddd9f3195
Anuken <arnukren@gmail.com>
2019-03-21 10:43:04 -0400

    Reduced drone factory cost

--- core/src/io/anuke/mindustry/content/TechTree.java
+++ core/src/io/anuke/mindustry/content/TechTree.java
@@ -263,7 +263,7 @@ public class TechTree implements ContentList{
     private TechNode node(Block block, Runnable children){
         ItemStack[] requirements = new ItemStack[block.buildRequirements.length];
         for(int i = 0; i < requirements.length; i++){
-            requirements[i] = new ItemStack(block.buildRequirements[i].item, block.buildRequirements[i].amount * 8);
+            requirements[i] = new ItemStack(block.buildRequirements[i].item, block.buildRequirements[i].amount * 6);
         }
         
         return new TechNode(block, requirements, children);
