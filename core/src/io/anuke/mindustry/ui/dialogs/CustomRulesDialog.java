819262c93fcbbb896434fbc4ee5a08895e9110e4
Anuken <arnukren@gmail.com>
2019-04-21 16:34:02 -0400

    Removed GameMode requirements

--- core/src/io/anuke/mindustry/ui/dialogs/CustomRulesDialog.java
+++ core/src/io/anuke/mindustry/ui/dialogs/CustomRulesDialog.java
@@ -38,32 +38,35 @@ public class CustomRulesDialog extends FloatingDialog{
         cont.clear();
         cont.pane(m -> main = m);
         main.margin(10f);
-        main.addButton("$settings.reset", () -> {rules = selectedGamemode.get(); setup();}).size(300f, 50f);
+        main.addButton("$settings.reset", () -> {
+            rules = selectedGamemode.get(); 
+            setup();
+        }).size(300f, 50f);
         main.left().defaults().fillX().left().pad(5);
         main.row();
-        title("$rules.title.waves", Gamemode.survival, Gamemode.sandbox);
-        check("$rules.waves", b -> rules.waves = b, () -> rules.waves, ()->selectedGamemode!=Gamemode.survival, Gamemode.survival, Gamemode.sandbox);
-        check("$rules.wavetimer", b -> rules.waveTimer = b, () -> rules.waveTimer, ()->rules.waves, Gamemode.survival, Gamemode.sandbox);
-        check("$rules.waitForWaveToEnd", b -> rules.waitForWaveToEnd = b, () -> rules.waitForWaveToEnd, ()->rules.waves, Gamemode.survival, Gamemode.sandbox);
-        number("$rules.wavespacing", f -> rules.waveSpacing = f * 60f, () -> rules.waveSpacing / 60f, ()->rules.waves, Gamemode.survival, Gamemode.sandbox);
+        title("$rules.title.waves");
+        check("$rules.waves", b -> rules.waves = b, () -> rules.waves, ()-> true);
+        check("$rules.wavetimer", b -> rules.waveTimer = b, () -> rules.waveTimer, () -> rules.waves);
+        check("$rules.waitForWaveToEnd", b -> rules.waitForWaveToEnd = b, () -> rules.waitForWaveToEnd, () -> rules.waves);
+        number("$rules.wavespacing", f -> rules.waveSpacing = f * 60f, () -> rules.waveSpacing / 60f, () -> rules.waves);
         title("$rules.title.respawns");
-        check("$rules.limitedRespawns", b -> rules.limitedRespawns= b, () -> rules.limitedRespawns, ()->true);
-        integer("$rules.respawns", f -> rules.respawns = (int) f, () -> rules.respawns, ()->rules.limitedRespawns);
-        number("$rules.respawntime", f -> rules.respawnTime = f * 60f, () -> rules.respawnTime / 60f, ()->true);
-        title("$rules.title.resourcesbuilding", Gamemode.attack, Gamemode.pvp, Gamemode.survival, Gamemode.sandbox);
-        check("$rules.infiniteresources", b -> rules.infiniteResources = b, () -> rules.infiniteResources, ()->true, Gamemode.attack, Gamemode.pvp, Gamemode.survival, Gamemode.sandbox);
-        number("$rules.buildcostmultiplier", f -> rules.buildCostMultiplier = f, () -> rules.buildCostMultiplier, ()->!rules.infiniteResources, Gamemode.attack, Gamemode.pvp, Gamemode.survival, Gamemode.sandbox);
-        number("$rules.buildspeedmultiplier", f -> rules.buildSpeedMultiplier = f, () -> rules.buildSpeedMultiplier, ()->true, Gamemode.attack, Gamemode.pvp, Gamemode.survival, Gamemode.sandbox);
+        check("$rules.limitedRespawns", b -> rules.limitedRespawns= b, () -> rules.limitedRespawns, () -> true);
+        integer("$rules.respawns", f -> rules.respawns = (int) f, () -> rules.respawns, () -> rules.limitedRespawns);
+        number("$rules.respawntime", f -> rules.respawnTime = f * 60f, () -> rules.respawnTime / 60f, () -> true);
+        title("$rules.title.resourcesbuilding");
+        check("$rules.infiniteresources", b -> rules.infiniteResources = b, () -> rules.infiniteResources, () -> true);
+        number("$rules.buildcostmultiplier", f -> rules.buildCostMultiplier = f, () -> rules.buildCostMultiplier, () -> !rules.infiniteResources);
+        number("$rules.buildspeedmultiplier", f -> rules.buildSpeedMultiplier = f, () -> rules.buildSpeedMultiplier, () -> true);
         title("$rules.title.player");
-        number("$rules.playerdamagemultiplier", f -> rules.playerDamageMultiplier = f, () -> rules.playerDamageMultiplier, ()->true);
-        number("$rules.playerhealthmultiplier", f -> rules.playerHealthMultiplier = f, () -> rules.playerHealthMultiplier, ()-> true);
+        number("$rules.playerdamagemultiplier", f -> rules.playerDamageMultiplier = f, () -> rules.playerDamageMultiplier, () -> true);
+        number("$rules.playerhealthmultiplier", f -> rules.playerHealthMultiplier = f, () -> rules.playerHealthMultiplier, () -> true);
         title("$rules.title.unit");
         check("$rules.unitdrops", b -> rules.unitDrops = b, () -> rules.unitDrops, ()->true);
         number("$rules.unitbuildspeedmultiplier", f -> rules.unitBuildSpeedMultiplier = f, () -> rules.unitBuildSpeedMultiplier, ()->true);
         number("$rules.unithealthmultiplier", f -> rules.unitHealthMultiplier = f, () -> rules.unitHealthMultiplier, ()->true);
         number("$rules.unitdamagemultiplier", f -> rules.unitDamageMultiplier = f, () -> rules.unitDamageMultiplier, ()->true);
-        title("$rules.title.enemy", Gamemode.attack, Gamemode.pvp);
-        number("$rules.enemycorebuildradius", f -> rules.enemyCoreBuildRadius = f * tilesize, () -> Math.min(rules.enemyCoreBuildRadius / tilesize, 200), ()->true, Gamemode.attack, Gamemode.pvp);
+        title("$rules.title.enemy");
+        number("$rules.enemycorebuildradius", f -> rules.enemyCoreBuildRadius = f * tilesize, () -> Math.min(rules.enemyCoreBuildRadius / tilesize, 200), ()->true);
     }
 
     void number(String text, FloatConsumer cons, FloatProvider prov, BooleanProvider condition){
@@ -79,12 +82,6 @@ public class CustomRulesDialog extends FloatingDialog{
         main.row();
     }
 
-    void number(String text, FloatConsumer cons, FloatProvider prov, BooleanProvider condition, Gamemode... gamemodes){
-        if(Structs.contains(gamemodes, selectedGamemode)){
-            number(text, cons, prov, condition);
-        }
-    }
-
     void integer(String text, FloatConsumer cons, FloatProvider prov, BooleanProvider condition){
         main.table(t -> {
             t.left();
@@ -98,27 +95,9 @@ public class CustomRulesDialog extends FloatingDialog{
         main.row();
     }
 
-    void integer(String text, FloatConsumer cons, FloatProvider prov, BooleanProvider condition, Gamemode... gamemodes){
-        if(Structs.contains(gamemodes, selectedGamemode)){
-            integer(text, cons, prov, condition);
-        }
-    }
-
     void check(String text, BooleanConsumer cons, BooleanProvider prov, BooleanProvider condition){
         main.addCheck(text, cons).checked(prov.get()).update(a -> a.setDisabled(!condition.get())).padRight(100f);
         main.row();
-    }
-
-    void check(String text, BooleanConsumer cons, BooleanProvider prov, BooleanProvider condition, Gamemode... gamemodes){
-        if(Structs.contains(gamemodes, selectedGamemode)){
-            check(text, cons, prov, condition);
-        }
-    }
-
-    void title(String text, Gamemode... gamemodes){
-        if(Structs.contains(gamemodes, selectedGamemode)){
-            title(text);
-        }
     }
 
     void title(String text){
