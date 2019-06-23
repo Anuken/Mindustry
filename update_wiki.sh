e552915637981dce792c8d8b669dfac27081e1f0
Anuken <arnukren@gmail.com>
2019-06-23 10:33:25 -0400

    Test 2

--- update_wiki.sh
+++ update_wiki.sh
@@ -2,7 +2,7 @@ git config --global user.name "Wiki Updater"
 git clone --depth=1 --branch=master https://github.com/MindustryGame/wiki ../wiki
 git clone --depth=1 --branch=master https://github.com/Anuken/Mindustry-Wiki-Generator ../Mindustry-Wiki-Generator
 cd ../Mindustry-Wiki-Generator
-"./gradlew run"
+./gradlew run
 cd ../wiki
 git add .
 git commit -m "Update to match commit ${TRAVIS_COMMIT}"
