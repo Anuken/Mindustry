7f2a1374e40f7ac28bdbcddba5de52c3f6fdb96f
Anuken <arnukren@gmail.com>
2019-08-15 14:16:41 -0400

    Fixed truncated sounds

--- core/convert_sounds.sh
+++ core/convert_sounds.sh
@@ -1,5 +1,12 @@
 #convert from stereo to mono
-for i in assets/sounds/*.ogg; do
+cd assets/sounds/
+for i in *.ogg; do
   echo $i
-  ffmpeg -i "$i" -y -ac 1 "$i"
+  ffmpeg -i "$i" -ac 1 "OUT_$i"
 done
+
+find . -type f ! -name "OUT_*" -delete
+
+for file in OUT_*; do mv "$file" "${file#OUT_}"; done;
+
+cd ../../
