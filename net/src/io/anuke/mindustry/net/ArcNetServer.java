d481af43c7bc4043cc6a8100f71121dd1cc5aa90
Anuken <arnukren@gmail.com>
2019-09-01 10:19:13 -0400

    Fixed "disconnected" bug / Updated menu music

--- net/src/io/anuke/mindustry/net/ArcNetServer.java
+++ net/src/io/anuke/mindustry/net/ArcNetServer.java
@@ -104,6 +104,34 @@ public class ArcNetServer implements ServerProvider{
     }
 
     @Override
+    public void sendStream(int id, Streamable stream){
+        ArcConnection connection = getByID(id);
+        if(connection == null) return;
+
+        connection.connection.addListener(new InputStreamSender(stream.stream, 512){
+            int id;
+
+            @Override
+            protected void start(){
+                //send an object so the receiving side knows how to handle the following chunks
+                StreamBegin begin = new StreamBegin();
+                begin.total = stream.stream.available();
+                begin.type = Registrator.getID(stream.getClass());
+                connection.connection.sendTCP(begin);
+                id = begin.id;
+            }
+
+            @Override
+            protected Object next(byte[] bytes){
+                StreamChunk chunk = new StreamChunk();
+                chunk.id = id;
+                chunk.data = bytes;
+                return chunk; //wrap the byte[] with an object so the receiving side knows how to handle it.
+            }
+        });
+    }
+
+    @Override
     public void host(int port) throws IOException{
         connections.clear();
         server.bind(port, port);
