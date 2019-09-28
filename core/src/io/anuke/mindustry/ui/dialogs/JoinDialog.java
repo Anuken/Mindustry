2019196f65163bec31f52bc579708eb833f3f672
Anuken <arnukren@gmail.com>
2019-09-28 18:01:55 -0400

    Sort Steam server lists, ignore version when unimportant

--- core/src/io/anuke/mindustry/ui/dialogs/JoinDialog.java
+++ core/src/io/anuke/mindustry/ui/dialogs/JoinDialog.java
@@ -202,10 +202,12 @@ public class JoinDialog extends FloatingDialog{
         }else if(host.version > Version.build && Version.build != -1){
             versionString = Core.bundle.get("server.outdated.client") + "\n" +
             Core.bundle.format("server.version", host.version, "");
+        }else if(host.version == Version.build && Version.type.equals(host.versionType)){
+            //not important
+            versionString = "";
         }else{
             versionString = Core.bundle.format("server.version", host.version, host.versionType);
         }
-
 
         content.table(t -> {
             t.add("[lightgray]" + host.name + "   " + versionString).width(targetWidth() - 10f).left().get().setEllipsis(true);
