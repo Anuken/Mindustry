912a40c9cdcdcd195c5222c98d1a8d752c628728
Anuken <arnukren@gmail.com>
2019-10-03 22:39:57 -0400

    Cleanup

--- core/assets/shaders/light.fragment.glsl
+++ core/assets/shaders/light.fragment.glsl
@@ -11,12 +11,7 @@ uniform vec4 u_ambient;
 varying vec4 v_color;
 varying vec2 v_texCoord;
 
-float stepped(float inp){
-    return inp;
-}
-
 void main(){
 	vec4 color = texture2D(u_texture, v_texCoord.xy);
-	float rounded = stepped(color.a);
-	gl_FragColor = clamp(vec4(mix(u_ambient.rgb, color.rgb, rounded), u_ambient.a - rounded), 0.0, 1.0);
+	gl_FragColor = clamp(vec4(mix(u_ambient.rgb, color.rgb, color.a), u_ambient.a - color.a), 0.0, 1.0);
 }
