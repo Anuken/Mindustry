d28ae1b30422541061ccbca776f7791ff5bbb122
Anuken <arnukren@gmail.com>
2019-10-27 16:05:13 -0400

    Bugfixes

--- core/src/io/anuke/mindustry/world/blocks/units/MechPad.java
+++ core/src/io/anuke/mindustry/world/blocks/units/MechPad.java
@@ -114,7 +114,7 @@ public class MechPad extends Block{
         MechFactoryEntity entity = tile.entity();
 
         if(entity.player != null){
-            RespawnBlock.drawRespawn(tile, entity.heat, entity.progress, entity.time, entity.player, (!entity.sameMech && entity.player.mech == mech ? Mechs.starter : mech));
+            RespawnBlock.drawRespawn(tile, entity.heat, entity.progress, entity.time, entity.player, (!entity.sameMech && entity.player.mech == mech ? mech : Mechs.starter));
         }
     }
 
