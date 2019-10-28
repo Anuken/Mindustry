61bb2a504b56ecfeaa68a2a2224e32c0a52206dc
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-10-28 10:40:10 +0100

    Update heat bar accordingly

--- core/src/io/anuke/mindustry/world/blocks/production/HeatedSmelter.java
+++ core/src/io/anuke/mindustry/world/blocks/production/HeatedSmelter.java
@@ -4,6 +4,7 @@ import io.anuke.arc.Core;
 import io.anuke.mindustry.entities.type.TileEntity;
 import io.anuke.mindustry.graphics.Pal;
 import io.anuke.mindustry.ui.Bar;
+import io.anuke.mindustry.world.Tile;
 import io.anuke.mindustry.world.meta.Attribute;
 
 import java.io.DataInput;
@@ -17,6 +18,14 @@ public class HeatedSmelter extends GenericSmelter {
     }
 
     @Override
+    public void placed(Tile tile) {
+        super.placed(tile);
+
+        HeatedSmelterEntity entity = tile.entity();
+        entity.GroundHeat = sumAttribute(Attribute.heat, tile.x, tile.y) / 10;
+    }
+
+    @Override
     public void drawPlace(int x, int y, int rotation, boolean valid){
         drawPlaceText(Core.bundle.formatFloat("bar.efficiency", sumAttribute(Attribute.heat, x, y) * 10, 1), x, y, valid);
     }
@@ -24,7 +33,7 @@ public class HeatedSmelter extends GenericSmelter {
     @Override
     public void setBars(){
         super.setBars();
-        bars.add("heat", entity -> new Bar("bar.heat", Pal.lightOrange, () -> ((HeatedSmelterEntity)entity).heat));
+        bars.add("heat", entity -> new Bar("bar.heat", Pal.lightOrange, () -> ((HeatedSmelterEntity)entity).GroundHeat));
     }
 
     @Override
@@ -34,17 +43,23 @@ public class HeatedSmelter extends GenericSmelter {
 
     public static class HeatedSmelterEntity extends GenericCrafterEntity {
         public float heat;
+        public float GroundHeat = 0.0f;
+        public float AmbientHeat = 0.0f;
 
         @Override
         public void write(DataOutput stream) throws IOException {
             super.write(stream);
             stream.writeFloat(heat);
+            stream.writeFloat(GroundHeat);
+            stream.writeFloat(AmbientHeat);
         }
 
         @Override
         public void read(DataInput stream, byte revision) throws IOException{
             super.read(stream, revision);
             heat = stream.readFloat();
+            GroundHeat = stream.readFloat();
+            AmbientHeat = stream.readFloat();
         }
     }
 }
