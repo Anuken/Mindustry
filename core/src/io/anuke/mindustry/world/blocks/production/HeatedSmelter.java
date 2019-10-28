b1d338fd75df78bddabd577e27df84bf1c700665
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-10-28 11:27:13 +0100

    Syphon heat from reactors

--- core/src/io/anuke/mindustry/world/blocks/production/HeatedSmelter.java
+++ core/src/io/anuke/mindustry/world/blocks/production/HeatedSmelter.java
@@ -1,10 +1,13 @@
 package io.anuke.mindustry.world.blocks.production;
 
 import io.anuke.arc.Core;
+import io.anuke.arc.collection.Array;
+import io.anuke.arc.math.Mathf;
 import io.anuke.mindustry.entities.type.TileEntity;
 import io.anuke.mindustry.graphics.Pal;
 import io.anuke.mindustry.ui.Bar;
 import io.anuke.mindustry.world.Tile;
+import io.anuke.mindustry.world.blocks.power.NuclearReactor;
 import io.anuke.mindustry.world.meta.Attribute;
 
 import java.io.DataInput;
@@ -18,12 +21,45 @@ public class HeatedSmelter extends GenericSmelter {
     }
 
     @Override
-    protected float getProgressIncrease(TileEntity entity, float baseTime) {
-        return super.getProgressIncrease(entity, baseTime) * ((HeatedSmelterEntity)entity).GroundHeat;
+    public void onProximityUpdate(Tile tile){
+        HeatedSmelterEntity entity = (HeatedSmelterEntity) tile.entity;
+        super.onProximityUpdate(tile);
+
+        entity.reactors.clear();
+        entity.proximity().each(t -> {
+            if t.block() != null && t.block() instanceof NuclearReactor){
+                entity.reactors.add(t);
+            }
+        });
+
     }
 
     @Override
-    public void placed(Tile tile) {
+    public void update(Tile tile){
+        super.update(tile);
+
+        HeatedSmelterEntity entity = ((HeatedSmelterEntity) tile.entity);
+        for(Tile reactor: ((HeatedSmelterEntity) tile.entity).reactors){
+            NuclearReactor.NuclearReactorEntity e = (NuclearReactor.NuclearReactorEntity) reactor.entity;
+
+            if(e.heat > entity.heat()){
+                e.heat -= 0.01f;
+                entity.AmbientHeat += 0.01f;
+            }
+        }
+
+        if(entity.AmbientHeat > 0f){
+            entity.AmbientHeat -= 0.001f;
+        }
+    }
+
+    @Override
+    protected float getProgressIncrease(TileEntity entity, float baseTime){
+        return super.getProgressIncrease(entity, baseTime) * ((HeatedSmelterEntity)entity).heat();
+    }
+
+    @Override
+    public void placed(Tile tile){
         super.placed(tile);
 
         HeatedSmelterEntity entity = tile.entity();
@@ -38,23 +74,26 @@ public class HeatedSmelter extends GenericSmelter {
     @Override
     public void setBars(){
         super.setBars();
-        bars.add("heat", entity -> new Bar("bar.heat", Pal.lightOrange, () -> ((HeatedSmelterEntity)entity).GroundHeat));
+        bars.add("heat", entity -> new Bar("bar.heat", Pal.lightOrange, ((HeatedSmelterEntity) entity)::heat));
     }
 
     @Override
-    public TileEntity newEntity() {
+    public TileEntity newEntity(){
         return new HeatedSmelterEntity();
     }
 
-    public static class HeatedSmelterEntity extends GenericCrafterEntity {
-        public float heat;
+    public static class HeatedSmelterEntity extends GenericCrafterEntity{
         public float GroundHeat = 0.0f;
         public float AmbientHeat = 0.0f;
+        public Array<Tile> reactors = new Array<Tile>();
+
+        public float heat(){
+            return Mathf.clamp(GroundHeat + AmbientHeat);
+        }
 
         @Override
-        public void write(DataOutput stream) throws IOException {
+        public void write(DataOutput stream) throws IOException{
             super.write(stream);
-            stream.writeFloat(heat);
             stream.writeFloat(GroundHeat);
             stream.writeFloat(AmbientHeat);
         }
@@ -62,7 +101,6 @@ public class HeatedSmelter extends GenericSmelter {
         @Override
         public void read(DataInput stream, byte revision) throws IOException{
             super.read(stream, revision);
-            heat = stream.readFloat();
             GroundHeat = stream.readFloat();
             AmbientHeat = stream.readFloat();
         }
