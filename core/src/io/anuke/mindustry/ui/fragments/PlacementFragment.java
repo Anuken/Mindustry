c540f8e5f53d3d5aecaa6169747bac74a51f52f7
Anuken <arnukren@gmail.com>
2019-10-28 21:47:55 -0400

    Bugfixes

--- core/src/io/anuke/mindustry/ui/fragments/PlacementFragment.java
+++ core/src/io/anuke/mindustry/ui/fragments/PlacementFragment.java
@@ -185,7 +185,7 @@ public class PlacementFragment extends Fragment{
                                     req.table(line -> {
                                         line.left();
                                         line.addImage(stack.item.icon(Cicon.small)).size(8 * 2);
-                                        line.add(stack.item.localizedName()).color(Color.lightGray).padLeft(2).left();
+                                        line.add(stack.item.localizedName).maxWidth(150f).fillX().color(Color.lightGray).padLeft(2).left().get().setEllipsis(true);
                                         line.labelWrap(() -> {
                                             TileEntity core = player.getClosestCore();
                                             if(core == null || state.rules.infiniteResources) return "*/*";
