891332a05de34fd4485d33bce4d54973fb128827
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-10-31 08:09:42 +0100

    Display in two columns

--- core/src/io/anuke/mindustry/ui/ItemsDisplay.java
+++ core/src/io/anuke/mindustry/ui/ItemsDisplay.java
@@ -35,14 +35,19 @@ public class ItemsDisplay extends Table{
                 t.row();
             }
 
+            int i = 0;
             for(Item item : content.items()){
                 if(item.type == ItemType.material && data.isUnlocked(item)){
                     t.label(() -> format(item)).left().minWidth(50);
                     t.addImage(item.icon(Cicon.small)).size(8 * 3).padLeft(4).padRight(4);
                     if(campaign){
                         t.add(item.localizedName()).color(Color.lightGray).left();
+                        t.row();
+                    } else {
+                        if (++i % 2 == 0){
+                            t.row();
+                        }
                     }
-                    t.row();
                 }
             }
         });
