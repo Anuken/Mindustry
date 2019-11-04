c06539449517dffbecfa93ddc1955a2e9ebf9be1
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-04 14:25:20 +0100

    Equalize graph storage percentage

--- core/src/io/anuke/mindustry/world/blocks/power/PowerDiode.java
+++ core/src/io/anuke/mindustry/world/blocks/power/PowerDiode.java
@@ -3,7 +3,9 @@ package io.anuke.mindustry.world.blocks.power;
 import io.anuke.arc.Core;
 import io.anuke.arc.graphics.g2d.Draw;
 import io.anuke.arc.graphics.g2d.TextureRegion;
-import io.anuke.arc.math.Mathf;
+import io.anuke.mindustry.entities.type.TileEntity;
+import io.anuke.mindustry.graphics.Pal;
+import io.anuke.mindustry.ui.Bar;
 import io.anuke.mindustry.world.Block;
 import io.anuke.mindustry.world.Tile;
 
@@ -25,34 +27,19 @@ public class PowerDiode extends Block{
     public void update(Tile tile){
         super.update(tile);
 
-        Tile back = getNearby(tile, (tile.rotation() + 2) % 4);
-        Tile front = getNearby(tile, tile.rotation());
+        DiodeEntity entity = (DiodeEntity) tile.entity;
 
-        if(!back.block().hasPower || !front.block().hasPower) return;
-        PowerGraph backGraph = back.entity.power.graph;
-        PowerGraph frontGraph = front.entity.power.graph;
-        if(backGraph == frontGraph) return;
-
-        // skip if the receiving graph is already full
-        if(frontGraph.getBatteryStored() == frontGraph.getTotalBatteryCapacity()) return;
-
-        // half the difference
-        float send = Mathf.clamp((backGraph.getBatteryStored() - frontGraph.getBatteryStored()) / 2, 0f, Integer.MAX_VALUE);
-
-        // send all overflow if all batteries of the sending graph are full
-        if(backGraph.getBatteryStored() == backGraph.getTotalBatteryCapacity()){
-            send += backGraph.getLastPowerProduced() - backGraph.getPowerNeeded();
+        if (!entity.connected()) return;
+        if (entity.input() > entity.output()) {
+            entity.transfer(entity.backGraph().getBatteryStored() * (entity.input() - entity.output()) / 2);
         }
+    }
 
-        // limit offering to space available
-        send = Mathf.clamp(send, 0f, frontGraph.getTotalBatteryCapacity() - frontGraph.getBatteryStored());
-
-        // limit to sendable power
-        send = Mathf.clamp(send, 0f, backGraph.getBatteryStored());
-
-        if (send == 0f) return;
-        backGraph.useBatteries(send);
-        frontGraph.chargeBatteries(send);
+    @Override
+    public void setBars() {
+        super.setBars();
+        bars.add("back", entity -> new Bar("bar.input", Pal.lighterOrange, () -> ((DiodeEntity) entity).input()) );
+        bars.add("front", entity -> new Bar("bar.output", Pal.lighterOrange, () -> ((DiodeEntity) entity).output()) );
     }
 
     @Override
@@ -72,14 +59,53 @@ public class PowerDiode extends Block{
         return new TextureRegion[]{Core.atlas.find(name + "-arrow"), Core.atlas.find(name)};
     }
 
-    public Tile getNearby(Tile tile, int rotation){
-        int x = tile.x;
-        int y = tile.y;
+    @Override
+    public TileEntity newEntity(){
+        return new DiodeEntity();
+    }
 
-        if(rotation == 0) return world.ltile(x + 1, y);
-        if(rotation == 1) return world.ltile(x, y + 1);
-        if(rotation == 2) return world.ltile(x - 1, y);
-        if(rotation == 3) return world.ltile(x, y - 1);
-        return null;
+    public static class DiodeEntity extends TileEntity{
+
+        public Tile backTile(){
+            return getNearby(tile, (tile.rotation() + 2) % 4);
+        }
+        public Tile frontTile(){
+            return getNearby(tile, tile.rotation());
+        }
+        //
+        public PowerGraph backGraph(){
+            if (!backTile().block().hasPower) return null;
+            return backTile().entity.power.graph;
+        }
+        public PowerGraph frontGraph(){
+            if (!frontTile().block().hasPower) return null;
+            return frontTile().entity.power.graph;
+        }
+        public boolean connected(){
+            return backGraph() != null && frontGraph() != null;
+        }
+        //
+        public float input(){
+            return backGraph() == null ? 0f : backGraph().getBatteryStored() / backGraph().getTotalBatteryCapacity();
+        }
+        public float output(){
+            return frontGraph() == null ? 0f : frontGraph().getBatteryStored() / frontGraph().getTotalBatteryCapacity();
+        }
+        public void transfer(float amount){
+            backGraph().useBatteries(amount);
+            frontGraph().chargeBatteries(amount);
+        }
+        //
+        public Tile getNearby(Tile tile, int rotation){
+            int x = tile.x;
+            int y = tile.y;
+
+            if(rotation == 0) return world.ltile(x + 1, y);
+            if(rotation == 1) return world.ltile(x, y + 1);
+            if(rotation == 2) return world.ltile(x - 1, y);
+            if(rotation == 3) return world.ltile(x, y - 1);
+            return null;
+        }
+
     }
 }
