0cd83825aed66f31d2429a374bc00f364d7a53fa
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-05 08:39:10 +0100

    Cleanup remnant discovered via pull request diff

--- core/src/io/anuke/mindustry/content/Blocks.java
+++ core/src/io/anuke/mindustry/content/Blocks.java
@@ -10,7 +10,6 @@ import io.anuke.mindustry.*;
 import io.anuke.mindustry.ctype.ContentList;
 import io.anuke.mindustry.entities.*;
 import io.anuke.mindustry.entities.bullet.*;
-import io.anuke.mindustry.entities.traits.BuilderTrait;
 import io.anuke.mindustry.entities.type.*;
 import io.anuke.mindustry.gen.*;
 import io.anuke.mindustry.graphics.*;
@@ -104,11 +103,6 @@ public class Blocks implements ContentList{
                     variantRegions = new TextureRegion[]{Core.atlas.find("clear")};
                 }
                 return variantRegions;
-            }
-
-            @Override
-            public void drawRequestRegion(BuilderTrait.BuildRequest req, Eachable<BuilderTrait.BuildRequest> list) {
-                //
             }
         };
 
