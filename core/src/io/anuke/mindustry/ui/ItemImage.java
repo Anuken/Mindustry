9e892dfe0a88c2a32177f8050e4d56a08f61d5a3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-07 13:52:53 +0100

    Remove hack attempt

--- core/src/io/anuke/mindustry/ui/ItemImage.java
+++ core/src/io/anuke/mindustry/ui/ItemImage.java
@@ -3,14 +3,9 @@ package io.anuke.mindustry.ui;
 import io.anuke.arc.graphics.g2d.*;
 import io.anuke.arc.scene.ui.*;
 import io.anuke.arc.scene.ui.layout.*;
-import io.anuke.arc.util.Log;
 import io.anuke.mindustry.type.*;
-import io.anuke.mindustry.world.modules.ItemModule;
 
 public class ItemImage extends Stack{
-
-    private ItemModule module;
-    private Item item;
 
     public ItemImage(TextureRegion region, int amount){
         Table t = new Table().left().bottom();
@@ -18,17 +13,6 @@ public class ItemImage extends Stack{
 
         add(new Image(region));
         add(t);
-    }
-
-    public ItemImage(ItemModule module, Item item){
-        Table t = new Table().left().bottom();
-        t.add(module.get(item) + "").name("item-label");
-
-        add(new Image(item.icon(Cicon.medium)));
-        add(t);
-
-        this.module = module;
-        this.item = item;
     }
 
     public ItemImage(TextureRegion region){
@@ -50,15 +34,6 @@ public class ItemImage extends Stack{
 
     @Override
     public void draw() {
-
-        if(module != null && item != null){
-            Table t = new Table().left().bottom();
-            t.add(module.get(item) + "").name("item-label");
-
-            addChildAt(0, new Image(item.icon(Cicon.medium)));
-            addChildAt(1, t);
-        }
-
         super.draw();
     }
 }
