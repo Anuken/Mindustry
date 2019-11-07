bf2db3e12cf0d69cc754b3ca002ddf66b96d336a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-07 14:07:56 +0100

    Outside the box

--- core/src/io/anuke/mindustry/ui/ReqImage.java
+++ core/src/io/anuke/mindustry/ui/ReqImage.java
@@ -7,6 +7,8 @@ import io.anuke.arc.scene.ui.Image;
 import io.anuke.arc.scene.ui.layout.Stack;
 import io.anuke.arc.scene.ui.layout.Scl;
 import io.anuke.mindustry.graphics.Pal;
+import io.anuke.mindustry.type.Item;
+import io.anuke.mindustry.world.modules.ItemModule;
 
 public class ReqImage extends Stack{
     private final Boolp valid;
@@ -28,6 +30,15 @@ public class ReqImage extends Stack{
                 Draw.reset();
             }
         });
+    }
+
+    public ReqImage(ItemModule module, Item item, Boolp valid){
+        this(new Element(){
+            @Override
+            public void draw(){
+                (new ItemImage(item.icon(Cicon.medium), module.get(item))).draw();
+            }
+        }, valid);
     }
 
     public ReqImage(TextureRegion region, Boolp valid){
