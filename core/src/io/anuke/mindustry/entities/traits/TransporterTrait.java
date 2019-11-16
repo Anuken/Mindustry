a91562492a233e6eb6cc54a404c7993152d75d1d
Violet-Scarelli <33497956+Violet-Scarelli@users.noreply.github.com>
2019-11-16 11:23:56 -0800

    Create TransporterTrait.java

---
+++ core/src/io/anuke/mindustry/entities/traits/TransporterTrait.java
@@ -0,0 +1,18 @@
+package io.anuke.mindustry.entities.traits;
+
+import io.anuke.arc.Core;
+import io.anuke.arc.graphics.Color;
+import io.anuke.arc.graphics.g2d.*;
+import io.anuke.arc.math.*;
+import io.anuke.arc.util.Time;
+import io.anuke.mindustry.content.*;
+import io.anuke.mindustry.entities.Effects;
+import io.anuke.mindustry.entities.type.*;
+import io.anuke.mindustry.gen.Call;
+import io.anuke.mindustry.graphics.*;
+import io.anuke.mindustry.type.Item;
+import io.anuke.mindustry.world.Tile;
+
+import static io.anuke.mindustry.Vars.*;
+
+//nothing in here yet, need to find proper code
