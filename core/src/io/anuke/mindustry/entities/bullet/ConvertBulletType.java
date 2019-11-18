70bbb9fdda983980b54b962adb5b4cfd6d3d6c05
Patrick 'Quezler' Mounier <patrick@queztech.net>
2019-11-18 14:02:11 +0100

    Cleanup

--- core/src/io/anuke/mindustry/entities/bullet/ConvertBulletType.java
+++ core/src/io/anuke/mindustry/entities/bullet/ConvertBulletType.java
@@ -1,27 +1,22 @@
 package io.anuke.mindustry.entities.bullet;
 
-import io.anuke.arc.util.Log;
 import io.anuke.mindustry.content.Liquids;
-import io.anuke.mindustry.entities.type.BaseUnit;
 import io.anuke.mindustry.entities.type.Bullet;
 import io.anuke.mindustry.entities.type.Unit;
-import io.anuke.mindustry.type.UnitType;
 import io.anuke.mindustry.world.Tile;
 
-public class ConvertBulletType extends LiquidBulletType {
-    public ConvertBulletType() {
+public class ConvertBulletType extends LiquidBulletType{
+    public ConvertBulletType(){
         super(Liquids.water);
     }
 
     @Override
-    public void hitTile(Bullet b, Tile tile) {
+    public void hitTile(Bullet b, Tile tile){
         tile.setTeam(b.getTeam());
     }
 
     @Override
-    public void hitUnit(Bullet b, Unit unit) {
-        super.hitUnit(b, unit);
-
+    public void hitUnit(Bullet b, Unit unit){
         unit.setTeam(b.getTeam());
     }
 }
