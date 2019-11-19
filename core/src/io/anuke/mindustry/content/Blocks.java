287a5c7d45c5f81d3ee90f305d4843f5e444dcef
Violet-Scarelli <33497956+Violet-Scarelli@users.noreply.github.com>
2019-11-19 00:00:55 -0800

    Shanked the additional draug

--- core/src/io/anuke/mindustry/content/Blocks.java
+++ core/src/io/anuke/mindustry/content/Blocks.java
@@ -1636,7 +1636,7 @@ public class Blocks implements ContentList{
             unitType = UnitTypes.draug;
             produceTime = 2500;
             size = 2;
-            maxSpawn = 2;
+            maxSpawn = 1;
             consumes.power(1.2f);
             consumes.items();
         }};
