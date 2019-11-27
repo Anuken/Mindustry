eabbd2fa32aa57ccfcca2b63ad8dd6453e19ca6c
Anuken <arnukren@gmail.com>
2019-11-27 00:07:37 -0500

    """security"""

--- core/src/io/anuke/mindustry/mod/Scripts.java
+++ core/src/io/anuke/mindustry/mod/Scripts.java
@@ -1,10 +1,12 @@
 package io.anuke.mindustry.mod;
 
+import io.anuke.arc.files.*;
 import org.graalvm.polyglot.*;
 
 public class Scripts{
     //TODO allowHostAccess(...) is obviously insecure
-    private Context context = Context.newBuilder("js").allowHostClassLookup(s -> s.startsWith("io.anuke.mindustry")).allowHostAccess(HostAccess.ALL).build();
+    private Context context = Context.newBuilder("js").allowHostClassLookup(s -> s.startsWith("io.anuke.mindustry"))
+        .allowHostAccess(HostAccess.newBuilder().allowPublicAccess(true).denyAccess(FileHandle.class).build()).build();
 
     public Scripts(){
         context.eval("js", "console.log(\"Initialized JS context.\")");
