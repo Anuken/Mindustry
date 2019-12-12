5db8520b743d4b02959a5bd04233870fe2836292
joshuaptfan <joshuaptfan@gmail.com>
2019-12-12 13:52:05 -0800

    Add rotation checks to mobile zoom

--- core/src/io/anuke/mindustry/input/MobileInput.java
+++ core/src/io/anuke/mindustry/input/MobileInput.java
@@ -588,7 +588,7 @@ public class MobileInput extends InputHandler implements GestureListener{
         }
 
         //zoom things
-        if(Math.abs(Core.input.axisTap(Binding.zoom)) > 0){
+        if(Math.abs(Core.input.axisTap(Binding.zoom)) > 0 && !Core.input.keyDown(Binding.rotateplaced) && (block == null || !block.rotate) && selectRequests.isEmpty()){
             renderer.scaleCamera(Core.input.axisTap(Binding.zoom));
         }
 
