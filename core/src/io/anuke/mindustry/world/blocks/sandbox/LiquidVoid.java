2366d25de73ab85bffc3352ab1e74ba1dea68dfe
TsjipTsjip <victor.van.herel@telenet.be>
2019-12-12 21:11:14 +0100

    Add liquid void block.

---
+++ core/src/io/anuke/mindustry/world/blocks/sandbox/LiquidVoid.java
@@ -0,0 +1,22 @@
+package io.anuke.mindustry.world.blocks.sandbox;
+
+import io.anuke.mindustry.type.Liquid;
+import io.anuke.mindustry.world.Tile;
+import io.anuke.mindustry.world.Block;
+
+public class LiquidVoid extends Block{
+
+    public LiquidVoid(String name){
+        super(name);
+        hasLiquids = true;
+        solid = true;
+        update = true;
+    }
+
+    @Override
+    public boolean acceptLiquid(Tile tile, Tile source, Liquid liquid, float amount){ return true; }
+
+    @Override
+    public void handleLiquid(Tile tile, Tile source, Liquid liquid, float amount){}
+
+}
