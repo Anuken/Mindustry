c8980163e9d2fad00db6e4d3a98766e33da01b37
Anuken <arnukren@gmail.com>
2020-01-07 10:32:00 -0500

    3D progress

---
+++ core/src/mindustry/graphics/PlanetRenderer.java
@@ -0,0 +1,31 @@
+package mindustry.graphics;
+
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g3d.*;
+import arc.util.*;
+
+public class PlanetRenderer{
+    private ImmediateRenderer3D rend = new ImmediateRenderer3D(false, true, 0);
+    private Camera3D cam = new Camera3D();
+
+    public void draw(){
+        Draw.flush();
+
+        Tmp.v1.trns(Time.time() *  2f, 30f);
+        cam.position.set(Tmp.v1.x, Tmp.v1.y, 0);
+        cam.resize(Core.graphics.getWidth(), Core.graphics.getHeight());
+        cam.lookAt(0, 0, 0);
+        cam.update();
+
+        rend.begin(cam.combined(), Gl.triangleStrip);
+        rend.color(Color.red);
+        rend.vertex(0f, 0f, 0f);
+        rend.color(Color.green);
+        rend.vertex(0f, 5f, 0f);
+        rend.color(Color.blue);
+        rend.vertex(0f, 5f, 5f);
+        rend.end();
+    }
+}
