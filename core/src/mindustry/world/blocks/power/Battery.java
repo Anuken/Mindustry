694933119e965f59bcc93002bbc2bec777dd007d
Arkanic <silverfishasaurus@gmail.com>
2020-01-13 03:45:22 +0000

    glow, my batteries, glow

--- core/src/mindustry/world/blocks/power/Battery.java
+++ core/src/mindustry/world/blocks/power/Battery.java
@@ -1,6 +1,13 @@
 package mindustry.world.blocks.power;
 
+import arc.*;
+import arc.graphics.g2d.*;
+import mindustry.entities.type.TileEntity;
+import mindustry.world.*;
+import mindustry.world.modules.PowerModule;
+
 public class Battery extends PowerDistributor{
+    public TextureRegion lightsRegion;
 
     public Battery(String name){
         super(name);
@@ -11,5 +18,19 @@ public class Battery extends PowerDistributor{
     @Override
     public void load(){
         super.load();
+        lightsRegion = Core.atlas.find(name + "-lights");
     }
+
+    @Override
+    public void draw(Tile tile){
+        super.draw(tile);
+        TileEntity ent = tile.ent();
+        PowerModule entity = ent.power;
+
+        Draw.alpha(entity.graph.getBatteryStored() / entity.graph.getBatteryCapacity());
+        Draw.rect(lightsRegion, tile.drawx(), tile.drawy());
+
+        Draw.reset();
+    }
+
 }
