80ac7f9edaff1e20ab2a8d545b2c42efc8a8ab28
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-14 19:03:49 +0100

    Reset phase bridge recipe

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -937,7 +937,7 @@ public class Blocks implements ContentList{
         }};
 
         phaseConveyor = new ItemBridge("phase-conveyor"){{
-            requirements(Category.distribution, itemBridge.requirements);
+            requirements(Category.distribution, ItemStack.with(Items.phasefabric, 5, Items.silicon, 7, Items.lead, 10, Items.graphite, 10));
             range = 12;
             canOverdrive = false;
             hasPower = true;
