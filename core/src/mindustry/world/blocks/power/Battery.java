c833ca771954763ea2fa35fd471101ccb8b0468f
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-01-25 17:42:27 +1300

    yet again...

--- core/src/mindustry/world/blocks/power/Battery.java
+++ core/src/mindustry/world/blocks/power/Battery.java
@@ -1,10 +1,35 @@
 package mindustry.world.blocks.power;
 
+import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import mindustry.world.*;
+
 public class Battery extends PowerDistributor{
+    public TextureRegion lightsRegion;
+    public Color emptyLightColor = Color.valueOf("6e7080");
+    public Color fullLightColor = Color.valueOf("fb9567");
 
     public Battery(String name){
         super(name);
         outputsPower = true;
         consumesPower = true;
     }
+    
+    @Override
+    public void load(){
+        super.load();
+        lightsRegion = Core.atlas.find(name + "-lights");
+    }
+
+    @Override
+    public void draw(Tile tile){
+        super.draw(tile);
+
+        Draw.color(emptyLightColor, fullLightColor, tile.entity.power.status);
+        Draw.rect(lightsRegion, tile.drawx(), tile.drawy());
+
+        Draw.reset();
+    }
+
 }
