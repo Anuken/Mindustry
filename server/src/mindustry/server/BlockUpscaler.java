197b4ddfc247d42039c7ad4dfb089ec7c4438932
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-27 20:12:17 +0100

    Disable building adjecent containers/vaults

--- server/src/mindustry/server/BlockUpscaler.java
+++ server/src/mindustry/server/BlockUpscaler.java
@@ -3,10 +3,13 @@ package mindustry.server;
 import arc.*;
 import arc.struct.*;
 import arc.util.*;
+import mindustry.content.*;
 import mindustry.core.GameState.*;
 import mindustry.game.*;
+import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.world.*;
+import mindustry.world.blocks.storage.*;
 import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
@@ -33,5 +36,25 @@ public class BlockUpscaler implements ApplicationListener{
                 Call.onConstructFinish(tile, tile.block().upscale.get(), -1, tile.rotation(), tile.getTeam(), true);
             });
         }
+    }
+
+    @Override
+    public void init(){
+        Events.on(BlockBuildEndEvent.class, event -> {
+            if(event.breaking) return;
+            if(!(event.tile.block() instanceof Vault)) return;
+            if(event.tile.entity.proximity().count(tile -> tile.block() instanceof Vault) == 0) return;
+
+            event.tile.setNet(scrap(event.tile.block()), event.tile.getTeam(), 0);
+        });
+    }
+
+    private Block scrap(Block block){
+        if(block.size == 1) return Blocks.scrapWall;
+        if(block.size == 2) return Blocks.scrapWallLarge;
+        if(block.size == 3) return Blocks.scrapWallHuge;
+        if(block.size == 4) return Blocks.scrapWallGigantic;
+
+        return Blocks.coreNucleus; // fallback
     }
 }
