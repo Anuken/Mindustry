0e3ef84022e5a96da10fb7c2db403377a246445c
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-31 07:27:32 +0100

    Stalemate when fps is 1 10 times in a row

--- server/src/mindustry/server/Stalemate.java
+++ server/src/mindustry/server/Stalemate.java
@@ -1,8 +1,12 @@
 package mindustry.server;
 
 import arc.*;
+import arc.util.*;
 import mindustry.game.*;
 import mindustry.game.EventType.*;
+
+import static arc.util.Log.info;
+import static mindustry.Vars.*;
 
 public class Stalemate implements ApplicationListener{
 
@@ -10,9 +14,15 @@ public class Stalemate implements ApplicationListener{
 
     @Override
     public void update(){
-        if(Core.graphics.getFramesPerSecond() > 1) return;
-        if(frozen++ < 20) return;
-        frozen = 0;
-        Events.fire(new GameOverEvent(Team.crux));
+        if(Core.graphics.getFramesPerSecond() > 1){
+            frozen = 0;
+            return;
+        }
+
+        if(frozen++ > 10){
+            frozen = 0;
+            info("&lmstalemate!");
+            Events.fire(new GameOverEvent(Team.crux));
+        }
     }
 }
