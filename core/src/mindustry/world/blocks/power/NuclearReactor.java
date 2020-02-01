7b5850ca055501d42e6f8720f0c719ba1eb4eacd
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-01 22:16:15 +0100

    Get rid of the reactor sync

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -161,7 +161,6 @@ public class NuclearReactor extends PowerGenerator{
         Timer.schedule(() -> {
             if(destroyed[0] == 0) return;
             for(Player p : playerGroup){
-                Call.onWorldDataBegin(p.con);
                 netServer.sendWorldData(p);
             }
         }, 2.5f);
