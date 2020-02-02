1a9273b7ed99ecdd028c331a99f7cdcaeedf557e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-02 09:35:38 +0100

    Revert "Get rid of the reactor sync"
    This reverts commit 7b5850ca055501d42e6f8720f0c719ba1eb4eacd.

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -161,6 +161,7 @@ public class NuclearReactor extends PowerGenerator{
         Timer.schedule(() -> {
             if(destroyed[0] == 0) return;
             for(Player p : playerGroup){
+                Call.onWorldDataBegin(p.con);
                 netServer.sendWorldData(p);
             }
         }, 2.5f);
