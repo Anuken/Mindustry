96c73a4a758c809ed9a459292dd90ec98ea2cca3
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-02 20:52:27 +0100

    Make the 3 blocks along the world border unnukable

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -139,6 +139,8 @@ public class NuclearReactor extends PowerGenerator{
         Geometry.circle(tile.x, tile.y, explosionRadius / 2, (x, y) -> {
             Tile tmp = world.tile(x, y);
             if(tmp != null && tmp.block() instanceof StaticWall) Timer.schedule(() -> {
+                if(tmp.x < 3 || tmp.y < 3) return;
+                if(tmp.x +3 >= world.width() || tmp.y +3 >= world.height()) return;
                 Call.onDeconstructFinish(tmp, Blocks.air, -1);
                 destroyed[0]++;
             }, tile.dst(tmp) / tilesize * 0.1f);
