455ce98a869c36fc9e77c6cfadf343e5612e64d4
InvalidError404 <60593448+InvalidError404@users.noreply.github.com>
2020-02-03 15:26:49 -0500

    Formatting

--- core/src/mindustry/world/modules/ItemModule.java
+++ core/src/mindustry/world/modules/ItemModule.java
@@ -12,6 +12,9 @@ public class ItemModule extends BlockModule{
     private int[] items = new int[content.items().size];
     private int total;
 
+    // Make the take() loop persistent so it does not return the same item twice in a row unless there is nothing else to return.
+    protected int takeRotation;
+
     public void forEach(ItemConsumer cons){
         for(int i = 0; i < items.length; i++){
             if(items[i] > 0){
@@ -65,9 +68,6 @@ public class ItemModule extends BlockModule{
     public int total(){
         return total;
     }
-
-    // Making the take() loop persistent so it does not return the same item twice in a row unless there is nothing else to return.
-    protected int takeRotation;
 
     public Item take(){
     	// 0-to-length loop broken in two parts. First resume the loop where it previously left off.
