e9e81f43f4c1079a55152c8907ae0d8cba9296c1
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-12 10:06:37 +0100

    Disable power void via bytelogic
    Flashes, reverting.

--- core/src/mindustry/world/blocks/sandbox/PowerVoid.java
+++ core/src/mindustry/world/blocks/sandbox/PowerVoid.java
@@ -1,5 +1,6 @@
 package mindustry.world.blocks.sandbox;
 
+import mindustry.world.*;
 import mindustry.world.blocks.PowerBlock;
 import mindustry.world.meta.BlockStat;
 
@@ -8,11 +9,17 @@ public class PowerVoid extends PowerBlock{
     public PowerVoid(String name){
         super(name);
         consumes.power(Float.MAX_VALUE);
+        stopOnDisabled = true;
     }
 
     @Override
     public void init(){
         super.init();
         stats.remove(BlockStat.powerUse);
+    }
+
+    @Override
+    public boolean shouldConsume(Tile tile){
+        return tile.entity.enabled();
     }
 }
