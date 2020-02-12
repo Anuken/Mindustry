628c611e2ff49f092eb1458f3da62d5647f24716
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-12 10:06:43 +0100

    Revert "Disable power void via bytelogic"
    This reverts commit e9e81f43f4c1079a55152c8907ae0d8cba9296c1.

--- core/src/mindustry/world/blocks/sandbox/PowerVoid.java
+++ core/src/mindustry/world/blocks/sandbox/PowerVoid.java
@@ -1,6 +1,5 @@
 package mindustry.world.blocks.sandbox;
 
-import mindustry.world.*;
 import mindustry.world.blocks.PowerBlock;
 import mindustry.world.meta.BlockStat;
 
@@ -9,17 +8,11 @@ public class PowerVoid extends PowerBlock{
     public PowerVoid(String name){
         super(name);
         consumes.power(Float.MAX_VALUE);
-        stopOnDisabled = true;
     }
 
     @Override
     public void init(){
         super.init();
         stats.remove(BlockStat.powerUse);
-    }
-
-    @Override
-    public boolean shouldConsume(Tile tile){
-        return tile.entity.enabled();
     }
 }
