f9f68c5ab5cb922b7315e8130873b181d1724edf
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-21 11:37:48 +0100

    Delete katamari

--- server/src/mindustry/server/ServerLauncher.java
+++ server/src/mindustry/server/ServerLauncher.java
@@ -82,7 +82,6 @@ public class ServerLauncher implements ApplicationListener{
         Core.app.addListener(new IdleSync());
         Core.app.addListener(new Limbo());
         Core.app.addListener(new CraterCorner());
-//        Core.app.addListener(new Katamari());
 
         mods.eachClass(Mod::init);
 
