0edbfb0137ad0847da5da52ddb95532eff5af05b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-21 11:11:40 +0100

    Upscale blocks quicker

--- server/src/mindustry/server/BlockUpscaler.java
+++ server/src/mindustry/server/BlockUpscaler.java
@@ -5,6 +5,7 @@ import arc.struct.*;
 import arc.util.*;
 import mindustry.core.GameState.*;
 import mindustry.game.*;
+import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
@@ -19,23 +20,34 @@ public class BlockUpscaler implements ApplicationListener{
     public void update(){
         if(!state.is(State.playing)) return;
 
-        if(!timer.get(0, 20)) return;
+        if(!timer.get(0, 60 * 10)) return;
 
         for(Team team : Team.base()){
-            indexer.getAllied(team, BlockFlag.scalable).each(tile -> {
-
-                if(tile == null) return;
-                if(!tile.block().flags.contains(BlockFlag.scalable)) return;
-
-                tile.getLinkedTilesAs(tile.block().upscale.get(), tmp);
-                if(tmp.select(t -> t.block() != tile.block()).size > 0) return;
-
-                float healthf = tmp.sumf(t -> t.entity.healthf()) / tmp.size;
-
-                Call.onConstructFinish(tile, tile.block().upscale.get(), -1, tile.rotation(), tile.getTeam(), true);
-
-                tile.entity.damage(tile.entity.maxHealth() - (tile.entity.maxHealth() * healthf));
-            });
+            trigger(team);
         }
+    }
+
+    @Override
+    public void init(){
+        Events.on(BlockBuildEndEvent.class, event -> {
+            if(!event.breaking) trigger(event.team);
+        });
+    }
+
+    private void trigger(Team team){
+        indexer.getAllied(team, BlockFlag.scalable).each(tile -> {
+
+            if(tile == null) return;
+            if(!tile.block().flags.contains(BlockFlag.scalable)) return;
+
+            tile.getLinkedTilesAs(tile.block().upscale.get(), tmp);
+            if(tmp.select(t -> t.block() != tile.block()).size > 0) return;
+
+            float healthf = tmp.sumf(t -> t.entity.healthf()) / tmp.size;
+
+            Call.onConstructFinish(tile, tile.block().upscale.get(), -1, tile.rotation(), tile.getTeam(), true);
+
+            tile.entity.damage(tile.entity.maxHealth() - (tile.entity.maxHealth() * healthf));
+        });
     }
 }
