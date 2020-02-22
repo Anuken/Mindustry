4cf21feeff0a339b36456b627fb18cbe2980ed5d
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-02-22 22:36:31 +0100

    Revamp rtg sharing

--- core/src/mindustry/world/blocks/power/DecayGenerator.java
+++ core/src/mindustry/world/blocks/power/DecayGenerator.java
@@ -1,11 +1,16 @@
 package mindustry.world.blocks.power;
 
+import mindustry.content.*;
+import mindustry.gen.*;
 import mindustry.type.Item;
 import mindustry.world.*;
 
 import static mindustry.Vars.netServer;
 
 public class DecayGenerator extends ItemLiquidGenerator{
+
+    private Item thorium = Items.thorium;
+    private int share = timers++;
 
     public DecayGenerator(String name){
         super(true, false, name);
@@ -43,16 +48,49 @@ public class DecayGenerator extends ItemLiquidGenerator{
 //        });
 //    }
 
+//    @Override
+//    public void handleItem(Item item, Tile tile, Tile source){
+//        super.handleItem(item, tile, source);
+//
+//        tile.entity.proximity().each(other -> {
+//            if(other.block != tile.block) return;
+//
+//            if(tile.entity.items.get(item) > other.entity.items.get(item)){
+//                other.block.handleItem(item, other, tile);
+//                tile.entity.items.remove(item, 1);
+//                netServer.titanic.addAll(tile);
+//                Call.transferItemTo(item, 1, tile.drawx(), tile.drawy(), other);
+//            }
+//        });
+//    }
+
     @Override
-    public void handleItem(Item item, Tile tile, Tile source){
-        super.handleItem(item, tile, source);
+    public void update(Tile tile){
+        super.update(tile);
+
+//        tile.entity.proximity().each(other -> {
+//            if(other.block != tile.block) return;
+//            if(tile.entity.items.get(item) == 0) return;
+//            if(!other.block.acceptItem(item, other, other)) return;
+//
+//            if(tile.entity.items.get(item) + 1 > other.entity.items.get(item)){
+//                other.block.handleItem(item, other, tile);
+//                tile.entity.items.remove(item, 1);
+//                netServer.titanic.addAll(tile);
+//                Call.transferItemTo(item, 1, tile.drawx(), tile.drawy(), other);
+//            }
+//        });
+
+        if(!tile.entity.timer.get(share, 20)) return;
 
         tile.entity.proximity().each(other -> {
             if(other.block != tile.block) return;
-            if(other.entity.items.get(item) >= tile.entity.items.get(item)) return;
-            other.block.handleItem(item, other, tile);
-            tile.entity.items.remove(item, 1);
-            netServer.titanic.addAll(other, tile);
+
+            if(tile.entity.items.has(thorium, other.entity.items.get(thorium) + 2)){
+                tile.entity.items.remove(thorium, 1);
+                netServer.titanic.addAll(tile);
+                Call.transferItemTo(thorium, 1, tile.drawx(), tile.drawy(), other);
+            }
         });
     }
 }
