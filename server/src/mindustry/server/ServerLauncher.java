50eddaac03b94d8a746023933dd3cc351dd8243f
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-03-08 20:41:15 +0100

    Disable crater corner

--- server/src/mindustry/server/ServerLauncher.java
+++ server/src/mindustry/server/ServerLauncher.java
@@ -82,7 +82,7 @@ public class ServerLauncher implements ApplicationListener{
         Core.app.addListener(new BlockUpscaler());
         Core.app.addListener(new EmojiFilter());
         Core.app.addListener(new Limbo());
-        Core.app.addListener(new CraterCorner());
+//        Core.app.addListener(new CraterCorner());
         Core.app.addListener(new BridgeBuilder());
         Core.app.addListener(spiderweb);
 
