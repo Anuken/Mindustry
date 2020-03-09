9cb191761f9dd040dd867891ad6b8636521bc32a
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-03-09 19:53:09 +0100

    Fix reactor explosions

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -142,7 +142,7 @@ public class NuclearReactor extends PowerGenerator{
             if(tmp != null && tmp.block() instanceof StaticWall) Timer.schedule(() -> {
                 if(tmp.x < 3 || tmp.y < 3) return;
                 if(tmp.x +3 >= world.width() || tmp.y +3 >= world.height()) return;
-                tile.deconstructNet();
+                tmp.deconstructNet();
             }, tile.dst(tmp) / tilesize * 0.1f);
         });
 
