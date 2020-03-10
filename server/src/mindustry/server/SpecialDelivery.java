a4986e32e9e37b8bb0d639a51acda4acf8492507
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-03-10 20:39:22 +0100

    Pulse upgrade every 10 seconds

--- server/src/mindustry/server/SpecialDelivery.java
+++ server/src/mindustry/server/SpecialDelivery.java
@@ -20,33 +20,31 @@ public class SpecialDelivery implements ApplicationListener{
     @Override
     public void update(){
         if(!state.is(State.playing)) return;
-        if(!timer.get(20)) return;
+        if(!timer.get(60 * 10)) return;
 
         state.teams.getActive().each(td -> {
             td.cores.each(ce -> {
-                Tile upgradable = Geometry.findClosest(ce.x, ce.y, indexer.getAllied(ce.getTeam(), BlockFlag.upgradable).select(t -> {
-
+                indexer.getAllied(ce.getTeam(), BlockFlag.upgradable).select(t -> {
                     if(upgrading.containsValue(t, false)) return false;
                     if(t == null || t.block.upgrade == null || t.block.upgrade.get() == null) return false;
 
                     if(!ce.items.has(t.block.upgrade.get().requirements, state.rules.buildCostMultiplier * 100)) return false;
 
                     return true;
-                }));
-                if(upgradable == null) return;
+                }).each(upgradable -> {
+                    for(ItemStack is: upgradable.block.upgrade.get().requirements){
+                        ce.items.remove(is.item, (int)(is.amount * state.rules.buildCostMultiplier));
+                    }
 
-                for(ItemStack is: upgradable.block.upgrade.get().requirements){
-                    ce.items.remove(is.item, (int)(is.amount * state.rules.buildCostMultiplier));
-                }
+                    float dst = ce.dst(upgradable);
+                    float maxTraveled = Bullets.driverBolt.lifetime * Bullets.driverBolt.speed;
 
-                float dst = ce.dst(upgradable);
-                float maxTraveled = Bullets.driverBolt.lifetime * Bullets.driverBolt.speed;
+                    float velocity = 1f;
+                    velocity += Bullets.driverBolt.drag * dst / 8.25f;
 
-                float velocity = 1f;
-                velocity += Bullets.driverBolt.drag * dst / 8.25f;
-
-                Call.createBullet(Bullets.driverBolt, td.team, ce.x, ce.y, ce.angleTo(upgradable), velocity, (dst / maxTraveled));
-                upgrading.put(bulletGroup.entitiesToAdd.get(bulletGroup.entitiesToAdd.size -1), upgradable);
+                    Call.createBullet(Bullets.driverBolt, td.team, ce.x, ce.y, ce.angleTo(upgradable), velocity, (dst / maxTraveled));
+                    upgrading.put(bulletGroup.entitiesToAdd.get(bulletGroup.entitiesToAdd.size -1), upgradable);
+                });
             });
         });
     }
