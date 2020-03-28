87d335002c4d89583ecb66239dcddbb5208ceb75
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-03-28 10:02:59 +0100

    Revert "Make reactor explosion a square"
    This reverts commit 3a4047f629ead0fb837799b587e5de8701f093b1.

--- core/src/mindustry/world/blocks/power/NuclearReactor.java
+++ core/src/mindustry/world/blocks/power/NuclearReactor.java
@@ -28,9 +28,6 @@ public class NuclearReactor extends PowerGenerator{
 
     public final Vec2 tr = new Vec2();
 
-    private static Rect rect = new Rect();
-    private static Rect hitrect = new Rect();
-
     public Color lightColor = Color.valueOf("7f19ea");
     public Color coolColor = new Color(1, 1, 1, 0f);
     public Color hotColor = Color.valueOf("ff9575a3");
@@ -154,19 +151,12 @@ public class NuclearReactor extends PowerGenerator{
 
         if(!net.server()) return;
 
-        rect.setSize(explosionRadius * tilesize / 2).setCenter(tile.drawx(), tile.drawy());
-        for(int x = 0; x < world.width(); x++){
-            for(int y = 0; y < world.height(); y++){
-                Tile t = world.tile(x, y);
-                if(t != null && t.block() instanceof StaticWall){
-                    if(t.getHitbox(hitrect).overlaps(rect)){
-                        Timer.schedule(() -> {
-                            if(!tile.border()) t.deconstructNet();
-                        }, tile.dst(t) / tilesize * 0.1f);
-                    }
-                }
-            }
-        }
+        Geometry.circle(tile.x, tile.y, explosionRadius / 2, (x, y) -> {
+            Tile tmp = world.tile(x, y);
+            if(tmp != null && tmp.block() instanceof StaticWall) Timer.schedule(() -> {
+                if(!tile.border()) tmp.deconstructNet();
+            }, tile.dst(tmp) / tilesize * 0.1f);
+        });
 
         Timer.schedule(() -> {
             for(Player p : playerGroup){
