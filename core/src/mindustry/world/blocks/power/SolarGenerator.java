e5a3db128b1201e2d9e18cfafb986b482a01b510
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-03-30 09:39:16 +0200

    Merge repair point with large solar panel

--- core/src/mindustry/world/blocks/power/SolarGenerator.java
+++ core/src/mindustry/world/blocks/power/SolarGenerator.java
@@ -1,17 +1,21 @@
 package mindustry.world.blocks.power;
 
+import arc.*;
 import arc.struct.*;
+import mindustry.content.*;
+import mindustry.entities.type.*;
+import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 
-import static mindustry.Vars.state;
+import static mindustry.Vars.*;
 
 public class SolarGenerator extends PowerGenerator{
 
     public SolarGenerator(String name){
         super(name);
         // Remove the BlockFlag.producer flag to make this a lower priority target than other generators.
-        flags = EnumSet.of();
+        flags = EnumSet.of(BlockFlag.multipart);
         entityType = GeneratorEntity::new;
     }
 
@@ -26,5 +30,19 @@ public class SolarGenerator extends PowerGenerator{
         // Solar Generators don't really have an efficiency (yet), so for them 100% = 1.0f
         stats.remove(generationType);
         stats.add(generationType, powerProduction * 60.0f, StatUnit.powerSecond);
+    }
+
+    @Override
+    public void multipart(Tile tile, Player player){
+        if(tile.block == Blocks.largeSolarPanel){
+            Tile corner = world.tile(tile.x - 1, tile.y - 1);
+
+            if(corner.block != Blocks.repairPoint){
+                corner.setNet(Blocks.repairPoint, tile.getTeam(), 0);
+                corner.block.placed(corner);
+            }else{
+                Call.setTile(player, corner, Blocks.repairPoint, tile.getTeam(), 0);
+            }
+        }
     }
 }
