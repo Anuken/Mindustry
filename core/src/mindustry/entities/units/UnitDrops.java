46b7e6e38ad7a6ff2fa66debfca30178fd727c3b
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-03 18:14:46 +0200

    Drop item into nearby player if core is unavailable

--- core/src/mindustry/entities/units/UnitDrops.java
+++ core/src/mindustry/entities/units/UnitDrops.java
@@ -3,21 +3,24 @@ package mindustry.entities.units;
 import arc.math.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.entities.*;
 import mindustry.entities.type.*;
 import mindustry.gen.*;
 import mindustry.type.*;
+
+import static mindustry.content.Items.*;
 
 public class UnitDrops{
 
     public static void dropItems(BaseUnit unit){
         TileEntity core = unit.getClosestEnemyCore();
 
-        if(core == null || core.dst(unit) > Vars.mineTransferRange){
-            return;
-        }
+        if(unit.item().amount == 0 || unit.item().item == null) return;
 
-        if(unit.item().amount > 0 && unit.item().item != null){
-            if(!Vars.headless && !Vars.data.isUnlocked(unit.item().item)) return;
+        if(core == null || core.dst(unit) > Vars.mineTransferRange){
+            Player nearby = (Player)Units.closestEnemy(unit.getTeam(), unit.x, unit.y, Vars.mineTransferRange, u -> u instanceof Player && u.acceptsItem(unit.item().item));
+            if(nearby != null) nearby.addItem(unit.item().item, unit.item().amount);
+        }else{
             unit.item().amount = core.tile.block().acceptStack(unit.item().item, unit.item().amount, core.tile, null);
             Call.transferItemTo(unit.item().item, unit.item().amount, unit.x + Mathf.range(2f), unit.y + Mathf.range(2f), core.tile);
         }
@@ -32,18 +35,18 @@ public class UnitDrops{
 
     private static Item item(UnitType type){
 
-        if (type == UnitTypes.dagger) return Items.copper;
-        if (type == UnitTypes.crawler) return Items.graphite;
-        if (type == UnitTypes.titan) return Items.lead;
+        if (type == UnitTypes.dagger) return copper;
+        if (type == UnitTypes.crawler) return graphite;
+        if (type == UnitTypes.titan) return lead;
 
-        if (type == UnitTypes.fortress) return Items.titanium;
-        if (type == UnitTypes.eruptor) return Items.plastanium;
-        if (type == UnitTypes.chaosArray) return Items.thorium;
+        if (type == UnitTypes.fortress) return titanium;
+        if (type == UnitTypes.eruptor) return plastanium;
+        if (type == UnitTypes.chaosArray) return thorium;
 
-        if (type == UnitTypes.wraith) return Items.silicon;
-        if (type == UnitTypes.ghoul) return Items.metaglass;
-        if (type == UnitTypes.revenant) return Items.surgealloy;
-        if (type == UnitTypes.lich) return Items.phasefabric;
+        if (type == UnitTypes.wraith) return silicon;
+        if (type == UnitTypes.ghoul) return metaglass;
+        if (type == UnitTypes.revenant) return surgealloy;
+        if (type == UnitTypes.lich) return phasefabric;
 
         return null;
     }
