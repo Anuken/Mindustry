da99286826a02e7ab7cf5373b2dd539e84b365ab
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-04-16 20:33:52 +0200

    Remove obsolete valid check

--- core/src/mindustry/world/blocks/distribution/CraterConveyor.java
+++ core/src/mindustry/world/blocks/distribution/CraterConveyor.java
@@ -266,11 +266,6 @@ public class CraterConveyor extends Block implements Autotiler{
         }
 
         @Override
-        public boolean isValid(){
-            return !dead && (added || sleeping);
-        }
-
-        @Override
         public void write(Writes write){
             super.write(write);
 
