f97150d14e0b9fa5f48285c9946f7d6a3971407b
Summet <55251897+Summet-V@users.noreply.github.com>
2020-05-19 23:23:00 +0300

    Add clear() function.

--- core/assets/scripts/base.js
+++ core/assets/scripts/base.js
@@ -10,6 +10,7 @@ var scriptName = "base.js"
 var modName = "none"
 
 const print = text => log(modName + "/" + scriptName, text);
+const clear = () => Vars.ui.scriptfrag.clearMessages();
 
 const extendContent = function(classType, name, params){
     return new JavaAdapter(classType, params, name)
