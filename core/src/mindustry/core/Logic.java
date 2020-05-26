aaf791173533167aa1f4bafa88cb08dad1e0a396
Anuken <arnukren@gmail.com>
2020-05-26 19:59:31 -0400

    Fixed weather not triggering serverside

--- core/src/mindustry/core/Logic.java
+++ core/src/mindustry/core/Logic.java
@@ -33,10 +33,6 @@ import static mindustry.Vars.*;
 public class Logic implements ApplicationListener{
 
     public Logic(){
-        Events.on(WorldLoadEvent.class, event -> {
-            //TODO remove later
-            //Weathers.snow.create();
-        });
 
         Events.on(WaveEvent.class, event -> {
             if(state.isCampaign()){
