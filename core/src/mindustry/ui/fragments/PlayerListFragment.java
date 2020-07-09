7c5f3762dd83d0882a51fed0b17751cda373a932
Anuken <arnukren@gmail.com>
2020-07-09 16:48:36 -0400

    Bugfixes

--- core/src/mindustry/ui/fragments/PlayerListFragment.java
+++ core/src/mindustry/ui/fragments/PlayerListFragment.java
@@ -98,7 +98,7 @@ public class PlayerListFragment extends Fragment{
                 }
             };
             table.margin(8);
-            table.add(new Image(user.icon()).setScaling(Scaling.none)).grow();
+            table.add(new Image(user.icon()).setScaling(Scaling.bounded)).grow();
 
             button.add(table).size(h);
             button.labelWrap("[#" + user.color().toString().toUpperCase() + "]" + user.name()).width(170f).pad(10);
