5f688d2078a94ba770fcaf6ea090dbd64415fe5f
_m_nr_sU <62972692+mnrsU@users.noreply.github.com>
2020-07-16 21:41:57 -0700

    Prevent mechanical and pneumatic drills from accepting cryo-cooling
    Based on a comment in https://github.com/Anuken/Mindustry/pull/2219

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -1186,7 +1186,7 @@ public class Blocks implements ContentList{
             drillTime = 600;
             size = 2;
             drawMineItem = true;
-            consumes.add(new ConsumeLiquidFilter(liquid -> liquid.temperature <= 0.5f && liquid.flammability < 0.1f, 0.05f)).update(false).boost();
+            consumes.add(new ConsumeLiquidFilter(liquid -> liquid.temperature <= 0.5f && liquid.temperature >= 0.4f && liquid.flammability < 0.1f, 0.05f)).update(false).boost();
         }};
 
         pneumaticDrill = new Drill("pneumatic-drill"){{
@@ -1195,7 +1195,7 @@ public class Blocks implements ContentList{
             drillTime = 400;
             size = 2;
             drawMineItem = true;
-            consumes.add(new ConsumeLiquidFilter(liquid -> liquid.temperature <= 0.5f && liquid.flammability < 0.1f, 0.06f)).update(false).boost();
+            consumes.add(new ConsumeLiquidFilter(liquid -> liquid.temperature <= 0.5f && liquid.temperature >= 0.4f && liquid.flammability < 0.1f, 0.05f)).update(false).boost();
         }};
 
         laserDrill = new Drill("laser-drill"){{
