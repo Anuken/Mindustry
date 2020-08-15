97aca84b26e9d190705961610780e237a8c80706
Anuken <arnukren@gmail.com>
2020-08-14 22:22:28 -0400

    arc

--- core/src/mindustry/world/blocks/logic/LogicBlock.java
+++ core/src/mindustry/world/blocks/logic/LogicBlock.java
@@ -65,9 +65,6 @@ public class LogicBlock extends Block{
 
     static String getLinkName(Block block){
         String name = block.name;
-        if(block.minfo.mod != null){
-            name = name.substring(block.minfo.mod.name.length() + 1);
-        }
         if(name.contains("-")){
             String[] split = name.split("-");
             //filter out 'large' at the end of block names
