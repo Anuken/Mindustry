540a5f30cb9911f21bd1ccfe4147c050884940c9
Anuken <arnukren@gmail.com>
2020-08-30 09:34:30 -0500

    Update Turret.java

--- core/src/mindustry/world/blocks/defense/turrets/Turret.java
+++ core/src/mindustry/world/blocks/defense/turrets/Turret.java
@@ -237,7 +237,7 @@ public abstract class Turret extends Block{
                         canShoot = unit.isShooting();
                     }else if(logicControlled()){ //logic behavior
                         canShoot = logicShooting;
-                    }else if(peekAmmo() != null){ //default AI behavior
+                    }else{ //default AI behavior
                         BulletType type = peekAmmo();
                         float speed = type.speed;
                         //slow bullets never intersect
