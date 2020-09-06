9d248fae17c7b5eb65c7607b42269296013666ca
Anuken <arnukren@gmail.com>
2020-09-06 11:54:50 -0400

    Update MassDriver.java

--- core/src/mindustry/world/blocks/distribution/MassDriver.java
+++ core/src/mindustry/world/blocks/distribution/MassDriver.java
@@ -31,7 +31,7 @@ public class MassDriver extends Block{
     public Effect shootEffect = Fx.shootBig2;
     public Effect smokeEffect = Fx.shootBigSmoke2;
     public Effect receiveEffect = Fx.mineBig;
-    public Sound shootSound = Sounds.none;
+    public Sound shootSound = Sounds.shootBig;
     public float shake = 3f;
     public @Load("@-base") TextureRegion baseRegion;
 
