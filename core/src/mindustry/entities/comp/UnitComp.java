86f31aad63f376bfcc0ff3fbe03f210bdce14760
BasedUser <konstttantin@gmail.com>
2020-09-09 21:36:02 +0300

    Update UnitComp.java

--- core/src/mindustry/entities/comp/UnitComp.java
+++ core/src/mindustry/entities/comp/UnitComp.java
@@ -77,6 +77,7 @@ abstract class UnitComp implements Healthc, Physicsc, Hitboxc, Statusc, Teamc, I
         if(sensor == LAccess.health) return health;
         if(sensor == LAccess.x) return x;
         if(sensor == LAccess.y) return y;
+        if(sensor == LAccess.name) return type.name;
         if(sensor == LAccess.team) return team.id;
         if(sensor == LAccess.shooting) return isShooting() ? 1 : 0;
         if(sensor == LAccess.shootX) return aimX();
