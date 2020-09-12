bf130674b5aa0b5d9a65f2c39f46e632e71c237d
GlennFolker <glenn.folk08@gmail.com>
2020-09-12 14:45:22 +0700

    (this isn't necessary)

--- core/src/mindustry/world/blocks/defense/Wall.java
+++ core/src/mindustry/world/blocks/defense/Wall.java
@@ -6,7 +6,6 @@ import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.util.*;
 import mindustry.entities.*;
-import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
