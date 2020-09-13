1fd98fe07c4c216f690afcb1b18dc4f5b33411c0
Anuken <arnukren@gmail.com>
2020-09-12 23:43:03 -0400

    Bugfixes (2)

--- core/src/mindustry/async/PhysicsProcess.java
+++ core/src/mindustry/async/PhysicsProcess.java
@@ -43,7 +43,7 @@ public class PhysicsProcess implements AsyncProcess{
         //find entities without bodies and assign them
         for(Physicsc entity : group){
             boolean grounded = entity.isGrounded();
-            int bits = grounded ? flying.maskBits : ground.maskBits;
+            int bits = grounded ? ground.maskBits : flying.maskBits;
 
             if(entity.physref() == null){
                 //add bodies to entities that have none
