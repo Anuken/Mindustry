e98c2b40f0d3198577a85f232a426c08950c3658
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-09-27 18:15:27 +0200

    Update ConditionOp.java

--- core/src/mindustry/logic/ConditionOp.java
+++ core/src/mindustry/logic/ConditionOp.java
@@ -3,7 +3,7 @@ package mindustry.logic;
 import arc.Core;
 import arc.input.*;
 
-public enum ConditionOp {
+public enum ConditionOp{
     equal("==", (a, b) -> Math.abs(a - b) < 0.000001, (a, b) -> a == b, false),
     greaterThan(">", (a, b) -> a > b, false),
     greaterThanEq(">=", (a, b) -> a >= b, false),
@@ -20,11 +20,11 @@ public enum ConditionOp {
     public final String symbol;
     public final Boolean alone;
 
-    ConditionOp(String symbol, CondOpLambda function, Boolean alone) {
+    ConditionOp(String symbol, CondOpLambda function, Boolean alone){
         this(symbol, function, null, alone);
     }
 
-    ConditionOp(String symbol, CondOpLambda function, CondObjOpLambda objFunction, Boolean alone) {
+    ConditionOp(String symbol, CondOpLambda function, CondObjOpLambda objFunction, Boolean alone){
         this.symbol = symbol;
         this.function = function;
         this.objFunction = objFunction;
@@ -33,15 +33,15 @@ public enum ConditionOp {
 
 
     @Override
-    public String toString() {
+    public String toString(){
         return symbol;
     }
 
-    interface CondObjOpLambda {
+    interface CondObjOpLambda{
         boolean get(Object a, Object b);
     }
 
-    interface CondOpLambda {
+    interface CondOpLambda{
         boolean get(double a, double b);
     }
 }
