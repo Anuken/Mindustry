8f860d12a28eaaf39836f183f8aa5bbc91a92126
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-09-27 18:13:17 +0200

    Update ConditionOp.java

--- core/src/mindustry/logic/ConditionOp.java
+++ core/src/mindustry/logic/ConditionOp.java
@@ -1,39 +1,47 @@
 package mindustry.logic;
 
-public enum ConditionOp{
-    equal("==", (a, b) -> Math.abs(a - b) < 0.000001, (a, b) -> a == b),
-    notEqual("not", (a, b) -> Math.abs(a - b) >= 0.000001, (a, b) -> a != b),
-    lessThan("<", (a, b) -> a < b),
-    lessThanEq("<=", (a, b) -> a <= b),
-    greaterThan(">", (a, b) -> a > b),
-    greaterThanEq(">=", (a, b) -> a >= b);
+import arc.Core;
+import arc.input.*;
+
+public enum ConditionOp {
+    equal("==", (a, b) -> Math.abs(a - b) < 0.000001, (a, b) -> a == b, false),
+    greaterThan(">", (a, b) -> a > b, false),
+    greaterThanEq(">=", (a, b) -> a >= b, false),
+    keyPressed("abc", (a, b) -> Core.input.keyDown(KeyCode.byOrdinal((int) b)), true),
+    lessThan("<", (a, b) -> a < b, false),
+    lessThanEq("<=", (a, b) -> a <= b, false),
+    notEqual("not", (a, b) -> Math.abs(a - b) >= 0.000001, (a, b) -> a != b, false);
+
 
     public static final ConditionOp[] all = values();
 
     public final CondObjOpLambda objFunction;
     public final CondOpLambda function;
     public final String symbol;
+    public final Boolean alone;
 
-    ConditionOp(String symbol, CondOpLambda function){
-        this(symbol, function, null);
+    ConditionOp(String symbol, CondOpLambda function, Boolean alone) {
+        this(symbol, function, null, alone);
     }
 
-    ConditionOp(String symbol, CondOpLambda function, CondObjOpLambda objFunction){
+    ConditionOp(String symbol, CondOpLambda function, CondObjOpLambda objFunction, Boolean alone) {
         this.symbol = symbol;
         this.function = function;
         this.objFunction = objFunction;
+        this.alone = alone;
     }
 
+
     @Override
-    public String toString(){
+    public String toString() {
         return symbol;
     }
 
-    interface CondObjOpLambda{
+    interface CondObjOpLambda {
         boolean get(Object a, Object b);
     }
 
-    interface CondOpLambda{
+    interface CondOpLambda {
         boolean get(double a, double b);
     }
 }
