29be5a5494402183245a67793483049c50b6928a
SnakkiZXZ <databagrusrus4567654@Mail.ru>
2020-09-27 16:39:38 +0500

    correction

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -1531,7 +1531,7 @@ public class Blocks implements ContentList{
         clapper = new ItemTurret("clapper"){{
             requirements(Category.turret, with(Items.plastanium, 230, Items.graphite, 150, Items.surgealloy, 130, Items.metaglass, 70));
             ammo(
-            Items.graphite, plastBomb
+            Items.plastanium, plastBomb
             );
             targetAir = false;
             health = 2400;
