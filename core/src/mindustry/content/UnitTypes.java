078a68a07da8ccf7e7c2a168e4f5d69b927b4290
Anuken <arnukren@gmail.com>
2020-09-27 23:00:38 -0400

    Fixed cores being placeable on nothing

--- core/src/mindustry/content/UnitTypes.java
+++ core/src/mindustry/content/UnitTypes.java
@@ -419,7 +419,7 @@ public class UnitTypes implements ContentList{
                 continuous = true;
                 cooldownTime = 200f;
 
-                bullet = new ContinuousLaserBulletType(17){{
+                bullet = new ContinuousLaserBulletType(20){{
                     length = 150f;
                     hitEffect = Fx.hitMeltHeal;
                     drawSize = 420f;
@@ -430,7 +430,7 @@ public class UnitTypes implements ContentList{
 
                     shootEffect = Fx.greenLaserChargeSmall;
 
-                    incendChance = 0.02f;
+                    incendChance = 0.05f;
                     incendSpread = 5f;
                     incendAmount = 1;
 
@@ -489,7 +489,7 @@ public class UnitTypes implements ContentList{
 
                 bullet = new LaserBulletType(){{
                     length = 500f;
-                    damage = 520f;
+                    damage = 550f;
                     width = 75f;
 
                     lifetime = 65f;
