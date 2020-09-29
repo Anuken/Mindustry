192437f217549ae1fd7a4e662b137279d3e43b66
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-09-28 23:44:08 -0700

    Fix

--- core/src/mindustry/world/blocks/defense/MendProjector.java
+++ core/src/mindustry/world/blocks/defense/MendProjector.java
@@ -62,7 +62,7 @@ public class MendProjector extends Block{
             for(int i = 0; i < 360; i += 60){
                 close.trns(i, 0, range + phaseRangeBoost - (4 + sin));
                 far.trns(i, 0, range + phaseRangeBoost);
-                Drawf.arrow(x * tilesize + close.x, y * tilesize + close.y, x * tilesize + far.x, y * tilesize + far.y, size * tilesize + sin, 4f + sin, phaseColor);
+                Drawf.arrow(x * tilesize + offset + close.x, y * tilesize + offset + close.y, x * tilesize + offset + far.x, y * tilesize + offset + far.y, size * tilesize + sin, 4f + sin, phaseColor);
             }
             
             Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, range + phaseRangeBoost, phaseColor);
