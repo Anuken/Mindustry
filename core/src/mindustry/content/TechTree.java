8b58f4de47082675b86412312886781a3d7605c5
Colonizor48 <43299519+Colonizor48@users.noreply.github.com>
2020-10-02 20:08:06 -0400

    Revert "Big plastanium smelter"

--- core/src/mindustry/content/TechTree.java
+++ core/src/mindustry/content/TechTree.java
@@ -162,7 +162,6 @@ public class TechTree implements ContentList{
 
                                             node(Items.plastanium, with(Items.titanium, 10000, Items.silicon, 10000), () -> {
                                                 node(plastaniumCompressor, () -> {
-                                                    node(plastaniumPress, () -> {});
                                                     node(Items.phasefabric, with(Items.thorium, 15000, Items.sand, 30000, Items.silicon, 5000), () -> {
                                                         node(phaseWeaver, () -> {
 
