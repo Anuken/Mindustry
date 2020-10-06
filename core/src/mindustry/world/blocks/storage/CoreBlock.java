048a3f056094db9144c610805242d63430e21984
MEEP of Faith <meepmatthew@gmail.com>
2020-10-06 16:06:15 -0700

    missing `@override`

--- core/src/mindustry/world/blocks/storage/CoreBlock.java
+++ core/src/mindustry/world/blocks/storage/CoreBlock.java
@@ -192,6 +192,7 @@ public class CoreBlock extends StorageBlock{
             Drawf.light(team, x, y, 30f * size, Pal.accent, 0.5f + Mathf.absin(20f, 0.1f));
         }
         
+        @Override
         public void drawTeamTop(){
             if(block.teamRegion.found()){
                 if(block.teamRegions[team.id] == block.teamRegion) Draw.color(team.color.cpy().mul(1f + Mathf.absin(Time.time()/2, 1f, 0.1f)));
