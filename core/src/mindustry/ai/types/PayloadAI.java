5a76abb6d1731e1a499b0d03edbd7bff5758909d
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-10-14 20:45:24 +0200

    Update PayloadAI.java

--- core/src/mindustry/ai/types/PayloadAI.java
+++ core/src/mindustry/ai/types/PayloadAI.java
@@ -2,24 +2,30 @@ package mindustry.ai.types;
 
 import arc.math.geom.*;
 import mindustry.entities.*;
-import mindustry.entities.units.UnitCommand;
 import mindustry.gen.*;
 import mindustry.world.blocks.*;
 
 public class PayloadAI extends FlyingAI{
+    public Teamc healingTarget;
 
     @Override
     public void updateMovement(){
         if(target instanceof Building){
+
+            if(unit.type().payloadHeal && healingTarget.within(unit, unit.type().range)){
+                unit.aim(healingTarget);
+                unit.controlWeapons(true, true);
+            }
+
             Vec2 unitPos = new Vec2(unit.x, unit.y);
             Vec2 targetPos = new Vec2(((Building) target).x, ((Building) target).y);
-            if(unitPos.dst(targetPos) <= (unit.type().range < (Float.MAX_VALUE * 0.96f) ? unit.type().range * 0.45f : 10f)) {
+            if(unitPos.dst(targetPos) <= (unit.type().range < (Float.MAX_VALUE * 0.96f) ? unit.type().range * 0.85f : 40f)) {
                 ((Payloadc) unit).dropLastPayload();
             }
         }
 
         if(target != null){
-            moveTo(target, 10f);
+            moveTo(target, 20f);
 
             unit.lookAt(target);
         }
@@ -34,5 +40,9 @@ public class PayloadAI extends FlyingAI{
         if(unit.hasWeapons()){
             updateWeapons();
         }
+
+        healingTarget = Units.findDamagedTile(unit.team, unit.x, unit.y);
+
+        if(healingTarget instanceof ConstructBlock.ConstructBuild) healingTarget = null;
     }
 }
