a92a369ad752202256371e2ea0ec1b89ae3c28d9
Leonwang4234 <62972692+Leonwang4234@users.noreply.github.com>
2020-10-17 10:25:46 -0700

    floating = true;

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -540,6 +540,7 @@ public class Blocks implements ContentList{
             flameColor = Color.valueOf("ffef99");
             itemCapacity = 30;
             boostScale = 0.15f;
+            floating = true;
 
             consumes.items(new ItemStack(Items.coal, 4), new ItemStack(Items.sand, 6), new ItemStack(Items.pyratite, 1));
             consumes.power(4f);
