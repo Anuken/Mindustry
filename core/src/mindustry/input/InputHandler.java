2e0c1cd613ae8d2629e55e10d9771c815b12df05
Summet <summet.dev@gmail.com>
2020-10-18 20:07:46 +0300

    code style
    Co-authored-by: Anuken <arnukren@gmail.com>

--- core/src/mindustry/input/InputHandler.java
+++ core/src/mindustry/input/InputHandler.java
@@ -242,7 +242,7 @@ public abstract class InputHandler implements InputProcessor, GestureListener{
             pay.dropLastPayload();
             pay.set(prevx, prevy);
             pay.controlling().each(u -> {
-                if (u instanceof Payloadc p){
+                if(u instanceof Payloadc p){
                     p.dropLastPayload();
                 }
             });
