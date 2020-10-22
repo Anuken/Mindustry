9ab0b2b7918c8f6f86d2aeab8435a4e497a3b99d
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-10-22 13:47:06 +0200

    Update LUnitControl.java

--- core/src/mindustry/logic/LUnitControl.java
+++ core/src/mindustry/logic/LUnitControl.java
@@ -15,7 +15,7 @@ public enum LUnitControl{
     mine("x", "y"),
     flag("value"),
     build("x", "y", "block", "rotation"),
-    getBlock("x", "y", "type", "building"),
+    getBlock("x", "y", "type", "building", "constructing"),
     within("x", "y", "radius", "result");
 
     public final String[] params;
