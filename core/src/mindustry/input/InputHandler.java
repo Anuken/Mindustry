73a5af2e5e65ea57185bb18c05261120c466147f
azureblue <kk0xf0@gmail.com>
2020-10-24 21:33:12 +0200

    make ending direction follow the path

--- core/src/mindustry/input/InputHandler.java
+++ core/src/mindustry/input/InputHandler.java
@@ -1213,7 +1213,14 @@ public abstract class InputHandler implements InputProcessor, GestureListener{
             line.x = point.x;
             line.y = point.y;
             if(!overrideLineRotation || diagonal){
-                line.rotation = next != null ? Tile.relativeTo(point.x, point.y, next.x, next.y) : baseRotation;
+                if (next != null){
+                    line.rotation = Tile.relativeTo(point.x, point.y, next.x, next.y);
+                }else if (block.conveyorPlacement && i > 0){
+                    Point2 prev = points.get(i - 1);
+                    line.rotation = Tile.relativeTo(prev.x, prev.y, point.x, point.y);
+                }else{
+                    line.rotation = baseRotation;
+                }                
             }else{
                 line.rotation = rotation;
             }
