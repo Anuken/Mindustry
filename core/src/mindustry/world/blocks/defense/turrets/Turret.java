7cab107e5015d7145f89eeab205e07e3bbe1b659
MEEP of Faith <meepmatthew@gmail.com>
2020-10-24 17:07:03 -0700

    Revert "Revert "Note: Don't use Arrays""
    This reverts commit 2877bc7c8124d58843ea63d5a387d6054206e41e.

--- core/src/mindustry/world/blocks/defense/turrets/Turret.java
+++ core/src/mindustry/world/blocks/defense/turrets/Turret.java
@@ -56,9 +56,11 @@ public abstract class Turret extends ReloadTurret{
     /** Currently used for artillery only. */
     public float minRange = 0f;
     public float burstSpacing = 0;
-    public float[] idleTurnRange = {45f, 90f};
+    public float idleTurnMinDist = 45f;
+    public float idleTurnMaxDist = 90f;
     public float idleTurnSpeed = 2f;
-    public float[] idleTurnTime = {240f, 720f};
+    public float idleTurnMinTime = 240f;
+    public float idleTurnMaxTime = 720f;
     public boolean alternate = false;
     public boolean targetAir = true;
     public boolean targetGround = true;
@@ -280,9 +282,9 @@ public abstract class Turret extends ReloadTurret{
             
             if(!validateTarget() && shouldTurn()){
                 if(!idleTurning){
-                    Time.run(Mathf.random(idleTurnTime[0], idleTurnTime[1]), () -> {
+                    Time.run(Mathf.random(idleTurnMinTime, idleTurnMaxTime), () -> {
                         idleTurning = true;
-                        idleTurnTarget = rotation + (Mathf.signs[(int)Mathf.round(Mathf.random(1))] * Mathf.random(idleTurnRange[0], idleTurnRange[1]));
+                        idleTurnTarget = rotation + (Mathf.signs[(int)Mathf.round(Mathf.random(1))] * Mathf.random(idleTurnMinDist, idleTurnMaxDist));
                     });
                 }else if(idleTurning){
                     turnToTarget(idleTurnTarget);
