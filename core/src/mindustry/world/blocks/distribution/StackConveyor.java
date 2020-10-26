1e12faaf659d63d9163c131c960450a41201bd87
DeltaNedas <39013340+DeltaNedas@users.noreply.github.com>
2020-10-26 13:37:53 +0000

    h
    h is for headless, which i am

--- core/src/mindustry/world/blocks/distribution/StackConveyor.java
+++ core/src/mindustry/world/blocks/distribution/StackConveyor.java
@@ -155,7 +155,8 @@ public class StackConveyor extends Block implements Autotiler{
             int[] bits = buildBlending(tile, rotation, null, true);
             if(bits[0] == 0 &&  blends(tile, rotation, 0) && !blends(tile, rotation, 2)) state = stateLoad;  // a 0 that faces into a conveyor with none behind it
             if(bits[0] == 0 && !blends(tile, rotation, 0) && blends(tile, rotation, 2)) state = stateUnload; // a 0 that faces into none with a conveyor behind it
-            if(!Vars.headless){
+
+            if(!headless){
                 blendprox = 0;
 
                 for(int i = 0; i < 4; i++){
