d271359d6d846e9e4f7a792c53337ef1a40afe98
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-11-09 09:47:01 +0900

    Update LCategory.java

--- core/src/mindustry/logic/LCategory.java
+++ core/src/mindustry/logic/LCategory.java
@@ -8,7 +8,9 @@ public enum LCategory{
     control(Color.cyan.cpy().shiftSaturation(-0.6f).mul(0.7f)),
     operations(Pal.place.cpy().shiftSaturation(-0.5f).mul(0.7f)),
     io(Pal.remove.cpy().shiftSaturation(-0.5f).mul(0.7f)),
-    units(Pal.bulletYellowBack.cpy().shiftSaturation(-0.3f).mul(0.8f));
+    units(Pal.bulletYellowBack.cpy().shiftSaturation(-0.3f).mul(0.8f)),
+    modded(Color.pink.cpy().shiftSaturation(-0.6f).mul(0.7f)),
+    custom(Pal.accent.cpy().shiftSaturation(-0.2f).mul(0.3f));
 
     public final Color color;
 
