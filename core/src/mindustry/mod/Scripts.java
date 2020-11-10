6da6c9426d2692e7547b3ff73906a682bcf105f6
DeltaNedas <deltanedas@gmail.com>
2020-11-10 08:02:39 +0000

    add file picker interface for mods

--- core/src/mindustry/mod/Scripts.java
+++ core/src/mindustry/mod/Scripts.java
@@ -4,7 +4,9 @@ import arc.*;
 import arc.assets.*;
 import arc.audio.*;
 import arc.files.*;
+import arc.func.*;
 import arc.mock.*;
+import arc.scene.ui.*;
 import arc.struct.*;
 import arc.util.*;
 import arc.util.Log.*;
@@ -114,6 +116,44 @@ public class Scripts implements Disposable{
         desc.errored = Throwable::printStackTrace;
 
         return sound;
+    }
+
+    /** Ask the user to select a file to read for a certain purpose like "Please upload a sprite" */
+    public void readFile(String purpose, String ext, Cons<String> cons){
+        selectFile(true, purpose, ext, fi -> cons.get(fi.readString()));
+    }
+
+    /** readFile but for a byte[] */
+    public void readBinFile(String purpose, String ext, Cons<byte[]> cons){
+        selectFile(true, purpose, ext, fi -> cons.get(fi.readBytes()));
+    }
+
+    /** Ask the user to write a file. */
+    public void writeFile(String purpose, String ext, String contents){
+        if(contents == null) contents = "";
+        final String fContents = contents;
+        selectFile(false, purpose, ext, fi -> fi.writeString(fContents));
+    }
+
+    /** writeFile but for a byte[] */
+    public void writeBinFile(String purpose, String ext, byte[] contents){
+        if(contents == null) contents = new byte[0];
+        final byte[] fContents = contents;
+        selectFile(false, purpose, ext, fi -> fi.writeBytes(fContents));
+    }
+
+    private void selectFile(boolean open, String purpose, String ext, Cons<Fi> cons){
+        purpose = purpose.startsWith("@") ? Core.bundle.get(purpose.substring(1)) : purpose;
+        //add purpose and extension at the top
+        String title = Core.bundle.get(open ? "open" : "save") + " - " + purpose + " (." + ext + ")";
+        Vars.platform.showFileChooser(open, title, ext, fi -> {
+            try{
+                cons.get(fi);
+            }catch(Exception e){
+                Log.err("Failed to select file '@' for a mod", fi);
+                Log.err(e);
+            }
+        });
     }
 
     //endregion
