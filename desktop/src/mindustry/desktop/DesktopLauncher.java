7f06bfa26af981d8b6921eb981ecda3afdfe2b07
Summet-V <krolik0504@gmail.com>
2020-11-10 20:52:49 +0300

    Refactor

--- desktop/src/mindustry/desktop/DesktopLauncher.java
+++ desktop/src/mindustry/desktop/DesktopLauncher.java
@@ -265,24 +265,26 @@ public class DesktopLauncher extends ClientLauncher{
             }
         }
 
-        if(useDiscord && Core.settings.getBool("discordrpc")){
-            DiscordRichPresence presence = new DiscordRichPresence();
+        if(useDiscord){
+            if(Core.settings.getBool("discordrpc")){
+                DiscordRichPresence presence = new DiscordRichPresence();
 
-            if(inGame){
-                presence.state = gameMode + gamePlayersSuffix;
-                presence.details = gameMapWithWave;
-                if(state.rules.waves){
-                    presence.largeImageText = "Wave " + state.wave;
+                if(inGame){
+                    presence.state = gameMode + gamePlayersSuffix;
+                    presence.details = gameMapWithWave;
+                    if(state.rules.waves){
+                        presence.largeImageText = "Wave " + state.wave;
+                    }
+                }else{
+                    presence.state = uiState;
                 }
+
+                presence.largeImageKey = "logo";
+
+                DiscordRPC.INSTANCE.Discord_UpdatePresence(presence);
             }else{
-                presence.state = uiState;
+                DiscordRPC.INSTANCE.Discord_ClearPresence();
             }
-
-            presence.largeImageKey = "logo";
-
-            DiscordRPC.INSTANCE.Discord_UpdatePresence(presence);
-        }else if(useDiscord){
-            DiscordRPC.INSTANCE.Discord_ClearPresence();
         }
 
         if(steam){
