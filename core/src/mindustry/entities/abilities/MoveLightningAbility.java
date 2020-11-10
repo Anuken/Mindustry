cdcb6d094ec78ede1725a8ed766ec45791b015a6
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-11-09 16:25:17 -0800

    Offset lightning spawn location in ability

--- core/src/mindustry/entities/abilities/MoveLightningAbility.java
+++ core/src/mindustry/entities/abilities/MoveLightningAbility.java
@@ -9,15 +9,17 @@ import mindustry.entities.*;
 import mindustry.gen.*;
 
 public class MoveLightningAbility extends Ability{
-    //Lightning damage
+    /** Lightning damage */
     public float damage = 35f;
-    //Chance of firing every tick. Set >= 1 to always fire lightning every tick at max speed.
+    /** Chance of firing every tick. Set >= 1 to always fire lightning every tick at max speed. */
     public float chance = 0.15f;
-    //Length of the lightning
+    /** Length of the lightning */
     public int length = 12;
-    //Speeds for when to start lightninging and when to stop getting faster
+    /** Speeds for when to start lightninging and when to stop getting faster */
     public float minSpeed = 0.8f, maxSpeed = 1.2f;
-    //Lightning color
+    /** Offset shoot location vertically */
+    public float offset = 0f;
+    /** Lightning color */
     public Color color = Color.valueOf("a9d8ff");
     
     public Effect shootEffect = Fx.sparkShoot;
@@ -25,21 +27,22 @@ public class MoveLightningAbility extends Ability{
     
     MoveLightningAbility(){}
     
-    public MoveLightningAbility(float damage, int length, float chance, float minSpeed, float maxSpeed, Color color){
+    public MoveLightningAbility(float damage, int length, float chance, float minSpeed, float maxSpeed, float offset, Color color){
         this.damage = damage;
         this.length = length;
         this.chance = chance;
         this.minSpeed = minSpeed;
         this.maxSpeed = maxSpeed;
         this.color = color;
+        this.offset = offset.
     }
     
     @Override
     public void update(Unit unit){
         float scl = Mathf.clamp((unit.vel().len() - minSpeed) / (maxSpeed - minSpeed));
         if(Mathf.chance(Time.delta * chance * scl)){
-            shootEffect.at(unit.x, unit.y, unit.rotation, color);
-            Lightning.create(unit.team, color, damage, unit.x + unit.vel.x, unit.y + unit.vel.y, unit.rotation, length);
+            shootEffect.at(unit.x + unit.vel.x + Angles.trnsx(unit.rotation, offset, 0), unit.y + unit.vel.y + Angles.trnsx(unit.rotation, offset, 0), unit.rotation, color);
+            Lightning.create(unit.team, color, damage, unit.x + unit.vel.x + Angles.trnsx(unit.rotation, offset, 0), unit.y + unit.vel.y + Angles.trnsx(unit.rotation, offset, 0), unit.rotation, length);
             shootSound.at(unit);
         }
     }
