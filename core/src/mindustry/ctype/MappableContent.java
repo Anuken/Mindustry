ad7cece21096002ecda85b8e9bb80062266138d1
younggam <61054554+younggam@users.noreply.github.com>
2020-11-15 19:52:32 +0900

    Update MappableContent.java

--- core/src/mindustry/ctype/MappableContent.java
+++ core/src/mindustry/ctype/MappableContent.java
@@ -3,10 +3,10 @@ package mindustry.ctype;
 import mindustry.*;
 
 public abstract class MappableContent extends Content{
-    public final String name, prefix;
+    public final String name;
 
     public MappableContent(String name){
-        this.name = Vars.content.transformName(name, prefix);
+        this.name = Vars.content.transformName(name);
         Vars.content.handleMappableContent(this);
     }
 
