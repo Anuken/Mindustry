2758e65e19271e3bdbdcba7e33414af22e774d0c
Anuken <arnukren@gmail.com>
2020-11-18 13:30:35 -0500

    Fixed typo

--- core/src/mindustry/ai/types/FormationAI.java
+++ core/src/mindustry/ai/types/FormationAI.java
@@ -79,7 +79,7 @@ public class FormationAI extends AIController implements FormationMember{
 
         if(unit.canBuild() && leader.canBuild() && leader.activelyBuilding()){
             unit.clearBuilding();
-            leader.addBuild(leader.buildPlan());
+            unit.addBuild(leader.buildPlan());
         }
     }
 
