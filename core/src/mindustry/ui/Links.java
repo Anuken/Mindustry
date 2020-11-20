37796a72e9138c3a5428af4e7a92902cf03619b3
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-11-20 11:56:56 +0100

    Create prefilled issue template prototype
    Bundle entry to follow in the next commit.

--- core/src/mindustry/ui/Links.java
+++ core/src/mindustry/ui/Links.java
@@ -4,8 +4,15 @@ import arc.*;
 import arc.graphics.*;
 import arc.scene.style.*;
 import arc.util.*;
+import mindustry.*;
+import mindustry.core.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.mod.Mods.*;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.*;
 
 public class Links{
     private static LinkEntry[] links;
@@ -22,7 +29,8 @@ public class Links{
             new LinkEntry("google-play", "https://play.google.com/store/apps/details?id=io.anuke.mindustry", Icon.googleplay, Color.valueOf("689f38")),
             new LinkEntry("f-droid", "https://f-droid.org/packages/io.anuke.mindustry/", Icon.android, Color.valueOf("026aa7")),
             new LinkEntry("github", "https://github.com/Anuken/Mindustry/", Icon.github, Color.valueOf("24292e")),
-            new LinkEntry("dev-builds", "https://github.com/Anuken/MindustryBuilds", Icon.githubSquare, Color.valueOf("fafbfc"))
+            new LinkEntry("dev-builds", "https://github.com/Anuken/MindustryBuilds", Icon.githubSquare, Color.valueOf("fafbfc")),
+            new LinkEntry("bug", report(), Icon.wrench, Color.valueOf("cbd97f"))
         };
     }
 
@@ -46,6 +54,15 @@ public class Links{
             this.link = link;
             this.icon = icon;
             this.title = Core.bundle.get("link." + name + ".title", Strings.capitalize(name.replace("-", " ")));
+        }
+    }
+
+    private static String report(){
+        String base = "https://github.com/Anuken/Mindustry/issues/new?assignees=&labels=bug";
+        try{
+            return base + "&body=" + URLEncoder.encode(Strings.format("**Platform**: `@`\n\n**Build**: `@`\n\n**Issue**: *Explain your issue in detail.*\n\n**Steps to reproduce**: *How you happened across the issue, and what exactly you did to make the bug happen.*\n\n**Link(s) to mod(s) used**: `@`\n\n**Save file**: *The (zipped) save file you were playing on when the bug happened. THIS IS REQUIRED FOR ANY ISSUE HAPPENING IN-GAME, REGARDLESS OF WHETHER YOU THINK IT HAPPENS EVERYWHERE. DO NOT DELETE OR OMIT THIS LINE UNLESS YOU ARE SURE THAT THE ISSUE DOES NOT HAPPEN IN-GAME.*\n\n**Crash report**: *The contents of relevant crash report files. REQUIRED if you are reporting a crash.*\n\n---\n\n*Place an X (no spaces) between the brackets to confirm that you have read the line below.*\n- [ ] **I have searched the closed and open issues to make sure that this problem has not already been reported.**", System.getProperty("os.name"), Version.combined(), Vars.mods.list().select(LoadedMod::enabled).map(l -> l.meta.author + "/" + l.name + ":" + l.meta.version)), StandardCharsets.UTF_8.toString());
+        }catch(UnsupportedEncodingException e){
+            return base + "&template=bug_report.md&title=";
         }
     }
 }
