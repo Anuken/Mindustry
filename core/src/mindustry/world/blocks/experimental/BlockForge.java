1ed2b2eb2eeae504c153a8e4b1d97f1f5894a380
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-12-01 09:28:22 +0100

    Update core/src/mindustry/world/blocks/experimental/BlockForge.java

--- core/src/mindustry/world/blocks/experimental/BlockForge.java
+++ core/src/mindustry/world/blocks/experimental/BlockForge.java
@@ -41,7 +41,7 @@ public class BlockForge extends PayloadAcceptor{
     public void setBars(){
         super.setBars();
 
-        bars.add("progress", (BlockForgeBuild entity) -> new Bar("bar.progress", Pal.ammo, () -> entity.payload != null ? 1f : entity.recipe == null ? 0f : (float)(entity.progress / entity.recipe.buildCost)));
+        bars.add("progress", (BlockForgeBuild entity) -> new Bar("bar.progress", Pal.ammo, () -> entity.recipe == null ? 0f : (float)(entity.progress / entity.recipe.buildCost)));
     }
 
     @Override
