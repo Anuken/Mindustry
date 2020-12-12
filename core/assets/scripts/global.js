d47abd4f837ac30f6f42c1f8dd33038d19a4a649
Charlotte Lily-Llaitchee Vixen <CharlotteLilyFields48@gmail.com>
2020-12-12 07:15:24 +0000

    fixed an error in global.js

--- core/assets/scripts/global.js
+++ core/assets/scripts/global.js
@@ -52,7 +52,7 @@ const cons = method => new Cons(){get: method}
 const prov = method => new Prov(){get: method}
 const func = method => new Func(){get: method}
 
-const newEffect = (lifetime, renderer) => new Effects.Effect(lifetime, new Effects.EffectRenderer({render: renderer}))
+const newEffect = (lifetime, renderer) => new Effect.Effect(lifetime, new Effects.EffectRenderer({render: renderer}))
 Call = Packages.mindustry.gen.Call
 
 importPackage(Packages.arc)
