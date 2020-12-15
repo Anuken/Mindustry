f39a61eee2a0b4e2b077b921528e2bf264ab5aaa
DeltaNedas <deltanedas@gmail.com>
2020-12-15 21:42:21 +0000

    allow controlling @this

--- core/src/mindustry/logic/LExecutor.java
+++ core/src/mindustry/logic/LExecutor.java
@@ -521,7 +521,7 @@ public class LExecutor{
         @Override
         public void run(LExecutor exec){
             Object obj = exec.obj(target);
-            if(obj instanceof Building b && b.team == exec.team && exec.linkIds.contains(b.id) && (type.cooldown <= 0 || timer.get(type.cooldown))){
+            if(obj instanceof Building b && b.team == exec.team && (exec.linkIds.contains(b.id) || b == exec.obj(thisVar)) && (type.cooldown <= 0 || timer.get(type.cooldown))){
                 if(type.isObj){
                     b.control(type, exec.obj(p1), exec.num(p2), exec.num(p3), exec.num(p4));
                 }else{
