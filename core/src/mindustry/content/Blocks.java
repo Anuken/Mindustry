118ae8e87deb0be4a90a660e273755958c7fe888
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-12-20 20:22:01 +0100

    #3889 2: electric boogaloo (#4056)

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -1262,24 +1262,27 @@ public class Blocks implements ContentList{
 
         mechanicalDrill = new Drill("mechanical-drill"){{
             requirements(Category.production, with(Items.copper, 12), true);
+            drawMineItem = true;
             tier = 2;
             drillTime = 600;
             size = 2;
-            drawMineItem = true;
+
             consumes.liquid(Liquids.water, 0.05f).boost();
         }};
 
         pneumaticDrill = new Drill("pneumatic-drill"){{
             requirements(Category.production, with(Items.copper, 18, Items.graphite, 10));
+            drawMineItem = true;
             tier = 3;
             drillTime = 400;
             size = 2;
-            drawMineItem = true;
+
             consumes.liquid(Liquids.water, 0.06f).boost();
         }};
 
         laserDrill = new Drill("laser-drill"){{
             requirements(Category.production, with(Items.copper, 35, Items.graphite, 30, Items.silicon, 30, Items.titanium, 20));
+            drawMineItem = true;
             drillTime = 280;
             size = 3;
             hasPower = true;
@@ -1293,6 +1296,7 @@ public class Blocks implements ContentList{
 
         blastDrill = new Drill("blast-drill"){{
             requirements(Category.production, with(Items.copper, 65, Items.silicon, 60, Items.titanium, 50, Items.thorium, 75));
+            drawMineItem = true;
             drillTime = 280;
             size = 4;
             drawRim = true;
