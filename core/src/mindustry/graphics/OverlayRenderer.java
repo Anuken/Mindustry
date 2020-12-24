d08ce90d637b9f019fce4cad0c54064068adb8f3
VozDuh <61590217+Voz-Duh@users.noreply.github.com>
2020-12-24 19:45:06 +0300

    absin

--- core/src/mindustry/graphics/OverlayRenderer.java
+++ core/src/mindustry/graphics/OverlayRenderer.java
@@ -113,13 +113,13 @@ public class OverlayRenderer{
                 if(dst < state.rules.enemyCoreBuildRadius * 2.2f){
                     Draw.color(Color.darkGray);
                     Lines.circle(core.x, core.y - 2, state.rules.enemyCoreBuildRadius);
-                    Draw.color(Pal.accent, core.team.color, adsin);
+                    Draw.color(Pal.accent, core.team.color, absin);
                     Lines.circle(core.x, core.y, state.rules.enemyCoreBuildRadius);
                 }
             });
 
             Draw.color(Pal.accent);
-            Draw.alpha(adsin);
+            Draw.alpha(absin);
             Lines.circle(player.x, player.y, 208.2f);
         }
 
