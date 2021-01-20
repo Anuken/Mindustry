88aea0a2690e9aaf462455edc54c6c1c9515bbee
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-20 16:25:00 +0300

    indicator not visible on enemy team

--- core/src/mindustry/world/blocks/defense/ShockMine.java
+++ core/src/mindustry/world/blocks/defense/ShockMine.java
@@ -9,12 +9,15 @@ import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
 
+import static mindustry.Vars.*;
+
 public class ShockMine extends Block{
     public final int timerDamage = timers++;
 
     public float cooldown = 80f;
     public float tileDamage = 5f;
     public float damage = 13;
+    public float indicatorWidth = 1f;
     public int length = 10;
     public int tendrils = 6;
     public Color lightningColor = Pal.lancerLaser;
@@ -38,9 +41,10 @@ public class ShockMine extends Block{
         @Override
         public void draw(){
             super.draw();
+            if(player.team() != team) return;
             Draw.color(Tmp.c1.set(Color.black).lerp(team.color, healthf() + Mathf.absin(Time.time, Math.max(healthf() * 5f, 1f), 1f - healthf())));
-            Draw.alpha(0.22f);
-            Fill.rect(x, y, 2f, 2f);
+            Draw.alpha(0.75f);
+            Fill.square(x, y, indicatorWidth);
             Draw.color();
         }
 
