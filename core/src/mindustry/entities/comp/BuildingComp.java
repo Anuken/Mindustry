17e13db1fdcf9c89252707242221726a4dd91841
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-20 18:12:05 +0300

    door fix...

--- core/src/mindustry/entities/comp/BuildingComp.java
+++ core/src/mindustry/entities/comp/BuildingComp.java
@@ -893,7 +893,7 @@ abstract class BuildingComp implements Posc, Teamc, Healthc, Buildingc, Timerc, 
 
         Draw.rect("block-border", x - block.size * tilesize / 2f + 4, y - block.size * tilesize / 2f + 4);
 
-        if(renderer.teamOutline){
+        if(renderer.teamOutline && !block.solidifes){
             Draw.z(Layer.blockUnder - 0.2f);
             Fill.square(x, y, tilesize * block.size / 2f + 1);
         }
