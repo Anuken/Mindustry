f3c76ec6c9c704a03d923cb81368fe08de763cb8
slava0135 <super.novalskiy_0135@inbox.ru>
2021-01-20 15:51:49 +0300

    new vision

--- core/src/mindustry/world/blocks/defense/ShockMine.java
+++ core/src/mindustry/world/blocks/defense/ShockMine.java
@@ -3,6 +3,8 @@ package mindustry.world.blocks.defense;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.util.Time;
+import arc.util.Tmp;
 import mindustry.entities.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
@@ -28,9 +30,6 @@ public class ShockMine extends Block{
     }
 
     public class ShockMineBuild extends Building{
-        private float progress = 0f;
-        private float blinkDelay = 40f;
-
         @Override
         public void drawTeam(){
             //no
@@ -39,21 +38,14 @@ public class ShockMine extends Block{
         @Override
         public void draw(){
             super.draw();
-            if(isActive() || progress < blinkDelay * 0.5f){
-                Draw.color(team.color);
-                Draw.alpha(0.25f);
-                Fill.rect(x, y, 2f, 2f);
-                Draw.color();
-            }
-            if(!isActive()){
-                progress += delta();
-                progress %= blinkDelay;
-            }
+            Draw.color(Tmp.c1.set(Color.black).lerp(team.color, healthf() + Mathf.absin(Time.time, Math.max(healthf() * 5f, 1f), 1f - healthf())), 0.25f);
+            Fill.rect(x, y, 2f, 2f);
+            Draw.color();
         }
 
         @Override
         public void unitOn(Unit unit){
-            if(isActive() && enabled && unit.team != team && timer(timerDamage, cooldown)){
+            if(health - tileDamage > 1 && enabled && unit.team != team && timer(timerDamage, cooldown)){
                 for(int i = 0; i < tendrils; i++){
                     Lightning.create(team, lightningColor, damage, x, y, Mathf.random(360f), length);
                 }
@@ -64,10 +56,6 @@ public class ShockMine extends Block{
         @Override
         public void drawCracks() {
             //no
-        }
-
-        private boolean isActive(){
-            return health - tileDamage > 1;
         }
     }
 }
