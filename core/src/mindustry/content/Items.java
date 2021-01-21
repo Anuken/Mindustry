047644ef0d470edc8381ec99458030b371366046
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2021-01-21 12:09:36 +0900

    full container = somewhat more than 4 full batteries

--- core/src/mindustry/content/Items.java
+++ core/src/mindustry/content/Items.java
@@ -74,7 +74,7 @@ public class Items implements ContentList{
 
         surgeAlloy = new Item("surge-alloy", Color.valueOf("f3e979")){{
             cost = 1.2f;
-            discharge = 0.5f;
+            discharge = 0.75f;
         }};
 
         sporePod = new Item("spore-pod", Color.valueOf("7457ce")){{
