c71e017782b0791cacc66ebde405483a5d9777f8
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2021-01-31 13:44:54 -0800

    Make drag calculation no longer clamped

--- core/src/mindustry/entities/comp/VelComp.java
+++ core/src/mindustry/entities/comp/VelComp.java
@@ -22,7 +22,7 @@ abstract class VelComp implements Posc{
     @Override
     public void update(){
         move(vel.x * Time.delta, vel.y * Time.delta);
-        vel.scl(Mathf.clamp(1f - drag * Time.delta));
+        vel.scl(1f - drag * Time.delta);
     }
 
     /** @return function to use for check solid state. if null, no checking is done. */
