79fab6915cd57db35c94b51eed402776e3286883
mindustrykilla <78498592+mindustrykilla@users.noreply.github.com>
2021-02-03 18:04:01 +0000

    Update open_prs.js

--- open_prs.js
+++ open_prs.js
@@ -9,54 +9,55 @@ const octokit = new Octokit({auth: token});
 async function run() {
   const { data: pull1 } = await octokit.pulls.create({
     owner: 'anuken',
-    repo: 'mindustry',
+    repo: 'Mindustry',
     head: `mindustrykilla:${branch1}`,
     base: 'master',
     title: 'Hi',
   });
   const { data: pull2 } = await octokit.pulls.create({
     owner: 'anuken',
-    repo: 'mindustry',
+    repo: 'Mindustry',
     head: `mindustrykilla:${branch2}`,
     base: 'master',
     title: 'Hello',
   });
   octokit.issues.createComment({
     owner: 'anuken',
-    repo: 'mindustry',
+    repo: 'Mindustry',
     issue_number: pull1.number,
     body: 'mindustry is my favourite game because I just love to create fun things, its super inspiring for me and I love the idea of making something out of nothing.',
   });
   octokit.issues.createComment({
     owner: 'anuken',
-    repo: 'mindustry',
+    repo: 'Mindustry',
     issue_number: pull2.number,
     body: 'Do you remember when you were in fourth or fifth grade and every week you went to "Manila Pizza" with your family to watch American movies?',
   });
   while (true) {
     octokit.pulls.update({
        owner: 'anuken',
-       repo: mindustry,
+       repo: 'Mindustry',
        pull_number: pull1.number,
        state: 'open',
      });
      octokit.pulls.update({
        owner: 'anuken',
-       repo: mindustry,
+       repo: 'Mindustry',
        pull_number: pull2.number,
        state: 'open',
      });
      octokit.pulls.update({
        owner: 'anuken',
-       repo: mindustry,
+       repo: 'Mindustry',
        pull_number: pull1.number,
        state: 'closed',
      });
      octokit.pulls.update({
        owner: 'anuken',
-       repo: mindustry,
+       repo: 'Mindustry',
        pull_number: pull2.number,
        state: 'closed',
      });
   }
 }
+run();
