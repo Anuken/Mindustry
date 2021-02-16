080fe8c3c4893ee2df3fc2f5bfb84b936e5ae35b
Anuken <arnukren@gmail.com>
2021-02-16 15:54:20 -0500

    Decreased impact reactor explosion radius

--- core/src/mindustry/world/blocks/power/ImpactReactor.java
+++ core/src/mindustry/world/blocks/power/ImpactReactor.java
@@ -24,8 +24,8 @@ public class ImpactReactor extends PowerGenerator{
 
     public float warmupSpeed = 0.001f;
     public float itemDuration = 60f;
-    public int explosionRadius = 50;
-    public int explosionDamage = 2000;
+    public int explosionRadius = 23;
+    public int explosionDamage = 1900;
 
     public Color plasma1 = Color.valueOf("ffd06b"), plasma2 = Color.valueOf("ff361b");
 
