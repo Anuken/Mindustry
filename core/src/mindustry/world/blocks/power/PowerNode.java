1ffdd9fd99965131ef7714364891db6ca8b0fbd8
Anuken <arnukren@gmail.com>
2021-02-16 12:20:30 -0500

    Fixed #4701

--- core/src/mindustry/world/blocks/power/PowerNode.java
+++ core/src/mindustry/world/blocks/power/PowerNode.java
@@ -145,7 +145,6 @@ public class PowerNode extends PowerBlock{
 
         getPotentialLinks(tile, other -> {
             Drawf.square(other.x, other.y, other.block.size * tilesize / 2f + 2f, Pal.place);
-
             insulators(tile.x, tile.y, other.tileX(), other.tileY(), cause -> {
                 Drawf.square(cause.x, cause.y, cause.block.size * tilesize / 2f + 2f, Pal.plastanium);
             });
