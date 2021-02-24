2d68d98eb2a37c72bf7492334ce16e3aa34684ab
Minxyzgo <71958008+Minxyzgo@users.noreply.github.com>
2021-02-24 19:55:34 +0800

    Update InputHandler.java

--- core/src/mindustry/input/InputHandler.java
+++ core/src/mindustry/input/InputHandler.java
@@ -544,7 +544,7 @@ public abstract class InputHandler implements InputProcessor, GestureListener{
     }
 
     public void useSchematic(Schematic schem){
-        selectRequests.addAll(schematics.toRequests(schem, Core.camera.position.x, Core.camera.position.y));
+        selectRequests.addAll(schematics.toRequests(schem, (int)Core.camera.position.x, (int)Core.camera.position.y));
     }
 
     protected void showSchematicSave(){
