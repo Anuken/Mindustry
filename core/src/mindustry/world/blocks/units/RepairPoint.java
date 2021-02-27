ae3c11d14dcc5123926826279a1052bcdf85a6d1
Leonwang4234 <62972692+Leonwang4234@users.noreply.github.com>
2021-02-27 14:34:45 -0800

    Make RepairPoints implement Ranged

--- core/src/mindustry/world/blocks/units/RepairPoint.java
+++ core/src/mindustry/world/blocks/units/RepairPoint.java
@@ -11,6 +11,7 @@ import mindustry.annotations.Annotations.*;
 import mindustry.entities.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.logic.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 
@@ -62,7 +63,7 @@ public class RepairPoint extends Block{
         return new TextureRegion[]{baseRegion, region};
     }
 
-    public class RepairPointBuild extends Building{
+    public class RepairPointBuild extends Building implements Ranged{
         public Unit target;
         public float strength, rotation = 90;
 
@@ -123,6 +124,11 @@ public class RepairPoint extends Block{
         @Override
         public BlockStatus status(){
             return Mathf.equal(efficiency(), 0f, 0.01f) ? BlockStatus.noInput : cons.status();
+        }
+
+        @Override
+        public float range(){
+            return repairRadius;
         }
 
         @Override
