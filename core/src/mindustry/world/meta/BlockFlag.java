ad621f27e32097d15b24e0f3546afdf83c179186
Oreo <70786458+AnOreo1@users.noreply.github.com>
2021-03-05 16:35:24 -0600

    Update BlockFlag.java

--- core/src/mindustry/world/meta/BlockFlag.java
+++ core/src/mindustry/world/meta/BlockFlag.java
@@ -14,8 +14,6 @@ public enum BlockFlag{
     factory,
     /** Repair point. */
     repair,
-    /** Rally point. */
-    rally,
     /** Block that stored power for resupply. */
     battery,
     /** Block used for resupply. */
