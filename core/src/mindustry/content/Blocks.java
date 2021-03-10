4264eb5683a4d0118243f5d8de8c528d7749fc51
unjown <71381907+unjown@users.noreply.github.com>
2021-03-10 10:22:59 +0800

    buff shale

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -298,7 +298,7 @@ public class Blocks implements ContentList{
 
         shale = new Floor("shale"){{
             variants = 3;
-            attributes.set(Attribute.oil, 1f);
+            attributes.set(Attribute.oil, 1.5f);
         }};
 
         stoneWall = new StaticWall("stone-wall"){{
