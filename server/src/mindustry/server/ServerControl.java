a55039447ad1e93a39a4e618c170a512dcd03360
Skat <55407440+skykatik@users.noreply.github.com>
2021-04-11 17:26:47 +0400

    Add file name printing (#5093)

--- server/src/mindustry/server/ServerControl.java
+++ server/src/mindustry/server/ServerControl.java
@@ -339,7 +339,11 @@ public class ServerControl implements ApplicationListener{
             if(!maps.all().isEmpty()){
                 info("Maps:");
                 for(Map map : maps.all()){
-                    info("  @: &fi@ / @x@", map.name().replace(' ', '_'), map.custom ? "Custom" : "Default", map.width, map.height);
+                    if(map.custom){
+                        info("  @ (@): &fiCustom / @x@", map.name().replace(' ', '_'), map.file.name(), map.width, map.height);
+                    }else{
+                        info("  @: &fiDefault / @x@", map.name().replace(' ', '_'), map.width, map.height);
+                    }
                 }
             }else{
                 info("No maps found.");
