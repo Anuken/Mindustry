fbddc0abc94d65be49f5c7275a96b766416d79fc
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2021-04-16 17:57:40 -0700

    Turn MiningRange into a UnitType variable. (#5112)
    * Turn MiningRange into a UnitType variable.
    
    Long range mining go brrrrr
    
    * Anuke Approved

--- core/src/mindustry/ai/types/MinerAI.java
+++ core/src/mindustry/ai/types/MinerAI.java
@@ -19,7 +19,7 @@ public class MinerAI extends AIController{
 
         if(!(unit.canMine()) || core == null) return;
 
-        if(unit.mineTile != null && !unit.mineTile.within(unit, unit.type.range)){
+        if(unit.mineTile != null && !unit.mineTile.within(unit, unit.type.miningRange)){
             unit.mineTile(null);
         }
 
@@ -44,9 +44,9 @@ public class MinerAI extends AIController{
                 }
 
                 if(ore != null){
-                    moveTo(ore, unit.type.range / 2f, 20f);
+                    moveTo(ore, unit.type.miningRange / 2f, 20f);
 
-                    if(unit.within(ore, unit.type.range)){
+                    if(unit.within(ore, unit.type.miningRange)){
                         unit.mineTile = ore;
                     }
 
