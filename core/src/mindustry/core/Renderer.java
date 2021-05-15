d5747df56a8ea72052a4f0b995beb0398cf8569e
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-05-15 16:28:07 +0300

    Update Renderer.java

--- core/src/mindustry/core/Renderer.java
+++ core/src/mindustry/core/Renderer.java
@@ -34,7 +34,7 @@ public class Renderer implements ApplicationListener{
     public FrameBuffer effectBuffer = new FrameBuffer();
     public boolean animateShields, drawWeather = true, drawStatus;
     /** minZoom = zooming out, maxZoom = zooming in */
-    public float minZoom = 0.01f, maxZoom = 66f;
+    public float minZoom = 0.01f, maxZoom = 65f;
 
     private @Nullable CoreBuild landCore;
     private Color clearColor = new Color(0f, 0f, 0f, 1f);
