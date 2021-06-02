462a64bf21710fa0738f9526697dab62466e0a00
Anuken <arnukren@gmail.com>
2021-06-02 16:51:52 -0400

    Netcode updates

--- core/src/mindustry/net/Packet.java
+++ core/src/mindustry/net/Packet.java
@@ -2,7 +2,17 @@ package mindustry.net;
 
 import arc.util.io.*;
 
+import java.io.*;
+
 public abstract class Packet{
+    //internally used by generated code
+    //TODO intermediate buffers should ONLY be needed for:
+    //readObject
+    //readBuilding
+    //readUnit (possibly)
+    protected static final ReusableByteInStream BAIS = new ReusableByteInStream();
+    protected static final Reads READ = new Reads(new DataInputStream(BAIS));
+
     //these are constants because I don't want to bother making an enum to mirror the annotation enum
 
     /** Does not get handled unless client is connected. */
@@ -14,6 +24,10 @@ public abstract class Packet{
 
     public void read(Reads read){}
     public void write(Writes write){}
+
+    public void read(Reads read, int length){
+        read(read);
+    }
 
     public int getPriority(){
         return priorityNormal;
