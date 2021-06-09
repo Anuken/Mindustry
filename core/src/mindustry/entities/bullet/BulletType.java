Anuken/Mindustry

commit 9f087214a1d86afed0c8f1819d12285b5c4a7c54

Just a Few Small Changes to Status Effect Stat Displays (#5361)
* Messing Around with Status Effect Stats

* Longer, but cleaner and less buggy

Co-authored-by: Leonwang4234 <leonwang4234@gmail.com>

core/src/mindustry/entities/bullet/BulletType.java
@@ -401,8 +401,14 @@ public class BulletType extends Content implements Cloneable{
             //pierceBuilding is not enabled by default, because a bullet may want to *not* pierce buildings
         }
 
-        if(lightningType == null){
-            lightningType = !collidesAir ? Bullets.damageLightningGround : Bullets.damageLightning;
+        if(lightning > 0){
+            if(status == StatusEffects.none){
+                status = StatusEffects.shocked;
+            }
+
+            if(lightningType == null){
+                lightningType = !collidesAir ? Bullets.damageLightningGround : Bullets.damageLightning;
+            }
         }
     }
 

core/src/mindustry/world/meta/StatValues.java
@@ -270,10 +270,6 @@ public class StatValues{
                         sep(bt, "@bullet.incendiary");
                     }
 
-                    if(type.status != StatusEffects.none){
-                        sep(bt, (type.minfo.mod == null ? type.status.emoji() : "") + "[stat]" + type.status.localizedName);
-                    }
-
                     if(type.homingPower > 0.01f){
                         sep(bt, "@bullet.homing");
                     }
@@ -284,6 +280,10 @@ public class StatValues{
 
                     if(type.fragBullet != null){
                         sep(bt, "@bullet.frag");
+                    }
+
+                    if(type.status != StatusEffects.none){
+                        sep(bt, (type.minfo.mod == null ? type.status.emoji() : "") + "[stat]" + type.status.localizedName);
                     }
                 }).padTop(unit ? 0 : -9).left().get().background(unit ? null : Tex.underline);
 

commit 28203ca6e917c876cc0cdba92c4c6070038fd455

Fixed regression introduced in #5361

core/src/mindustry/entities/bullet/BulletType.java
@@ -407,10 +407,10 @@ public class BulletType extends Content implements Cloneable{
             if(status == StatusEffects.none){
                 status = StatusEffects.shocked;
             }
+        }
 
-            if(lightningType == null){
-                lightningType = !collidesAir ? Bullets.damageLightningGround : Bullets.damageLightning;
-            }
+        if(lightningType == null){
+            lightningType = !collidesAir ? Bullets.damageLightningGround : Bullets.damageLightning;
         }
     }
 