efcae883fb871b3caab75f014a90a32e83df4aae
Anuken <arnukren@gmail.com>
2021-06-16 11:09:18 -0400

    Experimental core-capture PvP map / Editor filter fixes

--- core/src/mindustry/editor/MapInfoDialog.java
+++ core/src/mindustry/editor/MapInfoDialog.java
@@ -3,10 +3,10 @@ package mindustry.editor;
 import arc.*;
 import arc.scene.ui.*;
 import arc.struct.*;
-import arc.util.*;
 import mindustry.*;
 import mindustry.game.*;
 import mindustry.io.*;
+import mindustry.maps.filters.*;
 import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
 
@@ -74,7 +74,11 @@ public class MapInfoDialog extends BaseDialog{
             t.row();
             t.add("@editor.generation").padRight(8).left();
             t.button("@edit", () -> {
-                generate.show(maps.readFilters(editor.tags.get("genfilters", "")),
+                //randomize so they're not all the same seed
+                var res = maps.readFilters(editor.tags.get("genfilters", ""));
+                res.each(GenerateFilter::randomize);
+
+                generate.show(res,
                 filters -> {
                     //reset seed to 0 so it is not written
                     filters.each(f -> f.seed = 0);
