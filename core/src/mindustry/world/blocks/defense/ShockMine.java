126479c4e91afda6483550d106b91a2d5d0650f9
MEEP of Faith <meepmatthew@gmail.com>
2021-06-23 12:14:03 -0700

    Allow shock mines to spawn bullets

--- core/src/mindustry/world/blocks/defense/ShockMine.java
+++ core/src/mindustry/world/blocks/defense/ShockMine.java
@@ -5,6 +5,7 @@ import arc.graphics.g2d.*;
 import arc.math.*;
 import mindustry.annotations.Annotations.*;
 import mindustry.entities.*;
+import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
@@ -18,6 +19,8 @@ public class ShockMine extends Block{
     public int length = 10;
     public int tendrils = 6;
     public Color lightningColor = Pal.lancerLaser;
+    public int shots = 6;
+    public BulletType bullet;
     public float teamAlpha = 0.3f;
     public @Load("@-team-top") TextureRegion teamRegion;
 
@@ -46,16 +49,25 @@ public class ShockMine extends Block{
 
         @Override
         public void drawCracks(){
-
+            //no
         }
 
         @Override
         public void unitOn(Unit unit){
             if(enabled && unit.team != team && timer(timerDamage, cooldown)){
-                for(int i = 0; i < tendrils; i++){
-                    Lightning.create(team, lightningColor, damage, x, y, Mathf.random(360f), length);
-                }
+                triggered();
                 damage(tileDamage);
+            }
+        }
+
+        public void triggered(){
+            for(int i = 0; i < tendrils; i++){
+                Lightning.create(team, lightningColor, damage, x, y, Mathf.random(360f), length);
+            }
+            if(bullet != null){
+                for(int i = 0; i < shots; i++){
+                    bullet.create(this, x, y, Mathf.random(360f));
+                }
             }
         }
     }
