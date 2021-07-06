5693605f31810dbfa2c71863e3aa3539339b7d5e
Anuken <arnukren@gmail.com>
2021-07-06 11:10:11 -0400

    Correct Bullet#scaleVelocity documentation

--- core/src/mindustry/content/UnitTypes.java
+++ core/src/mindustry/content/UnitTypes.java
@@ -2155,6 +2155,7 @@ public class UnitTypes implements ContentList{
                 bullet = new EmpBulletType(){{
                     float rad = 100f;
 
+                    scaleVelocity = true;
                     lightOpacity = 0.7f;
                     unitDamageScl = 0.8f;
                     healPercent = 20f;
