0ba26b22c083af807756601ed51b762c8fbbd1cb
Anuken <arnukren@gmail.com>
2021-07-06 16:32:42 -0400

    Fixed #5548

--- annotations/src/main/java/mindustry/annotations/impl/AssetsProcess.java
+++ annotations/src/main/java/mindustry/annotations/impl/AssetsProcess.java
@@ -98,7 +98,7 @@ public class AssetsProcess extends BaseProcessor{
         });
 
         for(Element elem : elements){
-            Seq.with(((TypeElement)elem).getEnclosedElements()).each(e -> e.getKind() == ElementKind.FIELD, field -> {
+            Seq.with(elem.getEnclosedElements()).each(e -> e.getKind() == ElementKind.FIELD, field -> {
                 String fname = field.getSimpleName().toString();
                 if(fname.startsWith("default")){
                     loadStyles.addStatement("arc.Core.scene.addStyle(" + field.asType().toString() + ".class, mindustry.ui.Styles." + fname + ")");
@@ -124,7 +124,7 @@ public class AssetsProcess extends BaseProcessor{
             String name = p.nameWithoutExtension();
 
             if(names.contains(name)){
-                BaseProcessor.err("Duplicate file name: " + p.toString() + "!");
+                BaseProcessor.err("Duplicate file name: " + p + "!");
             }else{
                 names.add(name);
             }
