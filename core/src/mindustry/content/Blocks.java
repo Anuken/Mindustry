14e4203ee7354fecacff0862b917495cc70f8901
Anuken <arnukren@gmail.com>
2021-07-20 17:59:47 -0400

    #4912

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -304,7 +304,7 @@ public class Blocks implements ContentList{
 
         shale = new Floor("shale"){{
             variants = 3;
-            attributes.set(Attribute.oil, 1f);
+            attributes.set(Attribute.oil, 1.6f);
         }};
 
         stoneWall = new StaticWall("stone-wall"){{
