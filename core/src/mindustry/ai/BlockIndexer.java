90bc18e1bd3d1c99fb0cd24b5a78d720d6505576
Anuken <arnukren@gmail.com>
2021-07-31 08:38:03 -0400

    Fixed #5677

--- core/src/mindustry/ai/BlockIndexer.java
+++ core/src/mindustry/ai/BlockIndexer.java
@@ -181,7 +181,7 @@ public class BlockIndexer{
     /** Returns all damaged tiles by team. */
     public Seq<Building> getDamaged(Team team){
         if(damagedTiles[team.id] == null){
-            return null;
+            return damagedTiles[team.id] = new Seq<>();
         }
 
         return damagedTiles[team.id];
