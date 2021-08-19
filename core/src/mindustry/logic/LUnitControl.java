18309e7419627f09cc719345ff4d7f46632c2b51
yetaGitacc <36358141+yetaGitacc@users.noreply.github.com>
2021-08-20 03:30:28 +0800

    move new parser to end adapting older logic

--- core/src/mindustry/logic/LUnitControl.java
+++ core/src/mindustry/logic/LUnitControl.java
@@ -16,7 +16,7 @@ public enum LUnitControl{
     mine("x", "y"),
     flag("value"),
     build("x", "y", "block", "rotation", "config"),
-    getBlock("x", "y", "floor", "type", "building"),
+    getBlock("x", "y", "type", "building", "floor"),
     within("x", "y", "radius", "result");
 
     public final String[] params;
