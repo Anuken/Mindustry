1f6ab70b84121d3918c9f33d7165b2eaaeb62d89
yetaGitacc <36358141+yetaGitacc@users.noreply.github.com>
2021-08-20 03:28:48 +0800

    adapt for older logics

--- core/src/mindustry/logic/LExecutor.java
+++ core/src/mindustry/logic/LExecutor.java
@@ -474,19 +474,19 @@ public class LExecutor{
                     case getBlock -> {
                         float range = Math.max(unit.range(), buildingRange);
                         if(!unit.within(x1, y1, range)){
+                            exec.setobj(p3, null);
                             exec.setobj(p4, null);
-                            exec.setobj(p5, null);
                         }else{
-                            if(exec.bool(p3)){
+                            if(exec.bool(p5)){
                                 Block floor = world.floorWorld(x1,y1);
-                                exec.setobj(p4, floor);
-                                exec.setobj(p5, null);
+                                exec.setobj(p3, floor);
+                                exec.setobj(p4, null);
                             }else{
                                 Tile tile = world.tileWorld(x1, y1);
                                 //any environmental solid block is returned as StoneWall, aka "@solid"
                                 Block block = tile == null ? null : !tile.synthetic() ? (tile.solid() ? Blocks.stoneWall : Blocks.air) : tile.block();
-                                exec.setobj(p4, block);
-                                exec.setobj(p5, tile != null && tile.build != null ? tile.build : null);
+                                exec.setobj(p3, block);
+                                exec.setobj(p4, tile != null && tile.build != null ? tile.build : null);
                             }
                         }
                     }
