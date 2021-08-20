3b1a31e832178f014b5aed2ffd9716c91f4e7e2f
SoMall-dumpling <57857764+SoMall-dumpling@users.noreply.github.com>
2021-08-20 12:20:43 +0800

    Update OverdriveProjector.java
    Now using Math.max.

--- core/src/mindustry/world/blocks/defense/OverdriveProjector.java
+++ core/src/mindustry/world/blocks/defense/OverdriveProjector.java
@@ -75,7 +75,7 @@ public class OverdriveProjector extends Block{
     @Override
     public void setBars(){
         super.setBars();
-        bars.add("boost", (OverdriveBuild entity) -> new Bar(() -> Core.bundle.format("bar.boost", (int)(entity.realBoost() * 100 - 100)), () -> Pal.accent, () -> entity.realBoost() / (hasBoost ? speedBoost + speedBoostPhase : speedBoost)));
+        bars.add("boost", (OverdriveBuild entity) -> new Bar(() -> Core.bundle.format("bar.boost", Math.max((int)(entity.realBoost() * 100 - 100), 0)), () -> Pal.accent, () -> entity.realBoost() / (hasBoost ? speedBoost + speedBoostPhase : speedBoost)));
     }
 
     public class OverdriveBuild extends Building implements Ranged{
