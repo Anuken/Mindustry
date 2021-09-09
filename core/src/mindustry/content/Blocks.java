671098cc28b4c1c886be1c5736e4f87d63bdd2ff
itcannotbe <45280919+itcannotbe@users.noreply.github.com>
2021-09-09 23:16:47 +0800

    Add Build Cost Multiplier to Distributor (#5967)
    It just makes sense.

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -1020,6 +1020,7 @@ public class Blocks implements ContentList{
 
         distributor = new Router("distributor"){{
             requirements(Category.distribution, with(Items.lead, 4, Items.copper, 4));
+            buildCostMultiplier = 3f;
             size = 2;
         }};
 
