88d1165909e300a59c3990a55a516868f1e4c416
Anuken <arnukren@gmail.com>
2021-10-23 09:26:30 -0400

    Fixed #6220 / Fixed #6217

--- core/src/mindustry/world/blocks/defense/OverdriveProjector.java
+++ core/src/mindustry/world/blocks/defense/OverdriveProjector.java
@@ -109,7 +109,7 @@ public class OverdriveProjector extends Block{
                 float realRange = range + phaseHeat * phaseRangeBoost;
 
                 charge = 0f;
-                indexer.eachBlock(this, realRange, other -> true, other -> other.applyBoost(realBoost(), reload + 1f));
+                indexer.eachBlock(this, realRange, other -> other.block.canOverdrive, other -> other.applyBoost(realBoost(), reload + 1f));
             }
 
             if(timer(timerUse, useTime) && efficiency() > 0 && consValid()){
