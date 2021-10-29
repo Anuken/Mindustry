e16384adb36916d1be65521cc664948937f4414c
Anuken <arnukren@gmail.com>
2021-10-29 11:29:05 -0400

    Fixed #6259

--- core/src/mindustry/entities/comp/PayloadComp.java
+++ core/src/mindustry/entities/comp/PayloadComp.java
@@ -93,7 +93,6 @@ abstract class PayloadComp implements Posc, Rotc, Hitboxc, Unitc{
     void pickup(Building tile){
         tile.pickedUp();
         tile.tile.remove();
-        tile.tile = Vars.emptyTile;
         addPayload(new BuildPayload(tile));
         Fx.unitPickup.at(tile);
         Events.fire(new PickupEvent(self(), tile));
