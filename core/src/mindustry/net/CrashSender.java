4a94d9f2b3d7cba537a991acab752561c67dc616
Anuken <arnukren@gmail.com>
2021-10-30 22:30:00 -0400

    Remove mod count from crash

--- core/src/mindustry/net/CrashSender.java
+++ core/src/mindustry/net/CrashSender.java
@@ -28,7 +28,7 @@ public class CrashSender{
         + "Version: " + Version.combined() + (Vars.headless ? " (Server)" : "") + "\n"
         + "OS: " + OS.osName + " x" + (OS.osArchBits) + " (" + OS.osArch + ")\n"
         + "Java Version: " + OS.javaVersion + "\n"
-        + (mods == null ? "<no mod init>" : mods.list().size + " Mods" + (mods.list().isEmpty() ? "" : ": " + mods.list().select(LoadedMod::shouldBeEnabled).toString(", ", mod -> mod.name + ":" + mod.meta.version)))
+        + (mods == null ? "<no mod init>" : "Mods: " + (!mods.list().contains(LoadedMod::shouldBeEnabled) ? "none (vanilla)" : mods.list().select(LoadedMod::shouldBeEnabled).toString(", ", mod -> mod.name + ":" + mod.meta.version)))
         + "\n\n" + error;
     }
 
