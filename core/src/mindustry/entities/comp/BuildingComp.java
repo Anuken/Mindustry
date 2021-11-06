5b4cffdfd6ba4f12d1ef089df633d98bd572baca
Lele567 <48808663+Lele567@users.noreply.github.com>
2021-11-06 18:40:33 -0400

    DestroyBullet

--- core/src/mindustry/entities/comp/BuildingComp.java
+++ core/src/mindustry/entities/comp/BuildingComp.java
@@ -1032,7 +1032,12 @@ abstract class BuildingComp implements Posc, Teamc, Healthc, Buildingc, Timerc, 
 
     /** Called *after* the tile has been removed. */
     public void afterDestroyed(){
-
+        if (block.destroyBullet != null){
+            /** I really do not like that the bullet will not destroy derelict
+             but I can't do anything about it without using a random team
+             which may or may not cause issues with servers and js*/
+            block.destroyBullet.create(this, Team.derelict, x, y, rotdeg());
+        }
     }
 
     /** @return the cap for item amount calculations, used when this block explodes. */
