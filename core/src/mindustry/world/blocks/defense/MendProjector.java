07696c7f92c13f568c00292202aada42aa104004
Anuken <arnukren@gmail.com>
2021-11-25 11:33:44 -0500

    Closes #6327

--- core/src/mindustry/world/blocks/defense/MendProjector.java
+++ core/src/mindustry/world/blocks/defense/MendProjector.java
@@ -121,7 +121,7 @@ public class MendProjector extends Block{
             float f = 1f - (Time.time / 100f) % 1f;
 
             Draw.color(baseColor, phaseColor, phaseHeat);
-            Draw.alpha(heat * Mathf.absin(Time.time, 10f, 1f) * 0.5f);
+            Draw.alpha(heat * Mathf.absin(Time.time, 50f / Mathf.PI2, 1f) * 0.5f);
             Draw.rect(topRegion, x, y);
             Draw.alpha(1f);
             Lines.stroke((2f * f + 0.2f) * heat);
