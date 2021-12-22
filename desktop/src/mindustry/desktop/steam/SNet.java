d0c30147d8ef31af1c6f35272fa3cabd3f6f8174
Anuken <arnukren@gmail.com>
2021-12-22 00:05:02 -0500

    semicolons begone

--- desktop/src/mindustry/desktop/steam/SNet.java
+++ desktop/src/mindustry/desktop/steam/SNet.java
@@ -402,7 +402,7 @@ public class SNet implements SteamNetworkingCallback, SteamMatchmakingCallback, 
         public SteamConnection(SteamID sid){
             super(sid.getAccountID() + "");
             this.sid = sid;
-            Log.info("Create STEAM client @", sid.getAccountID());
+            Log.info("Created STEAM connection: @", sid.getAccountID());
         }
 
         @Override
