37ba8ac66b7cf0c7f5379bf0d458223bcbe40e61
Anuken <arnukren@gmail.com>
2021-12-28 23:05:46 -0500

    ContentParser liquids support

--- core/src/mindustry/mod/ContentParser.java
+++ core/src/mindustry/mod/ContentParser.java
@@ -271,6 +271,7 @@ public class ContentParser{
                             case "item" -> block.consumes.item(find(ContentType.item, child.asString()));
                             case "items" -> block.consumes.add((Consume)parser.readValue(ConsumeItems.class, child));
                             case "liquid" -> block.consumes.add((Consume)parser.readValue(ConsumeLiquid.class, child));
+                            case "liquids" -> block.consumes.add((Consume)parser.readValue(ConsumeLiquids.class, child));
                             case "coolant" -> block.consumes.add((Consume)parser.readValue(ConsumeCoolant.class, child));
                             case "power" -> {
                                 if(child.isNumber()){
