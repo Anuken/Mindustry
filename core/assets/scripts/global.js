361a256a372e0131cd0e82d206ffbbe2ae9342d7
smol <75618732+SMOLKEYS@users.noreply.github.com>
2022-01-30 00:04:52 +0800

    PlayerConnectionConfirmed

--- core/assets/scripts/global.js
+++ core/assets/scripts/global.js
@@ -199,3 +199,4 @@ const ResizeEvent = Packages.mindustry.game.EventType.ResizeEvent
 const LoseEvent = Packages.mindustry.game.EventType.LoseEvent
 const WinEvent = Packages.mindustry.game.EventType.WinEvent
 const Trigger = Packages.mindustry.game.EventType.Trigger
+const PlayerConnectionConfirmed = Packages.mindustry.game.EventType.PlayerConnectionConfirmed
