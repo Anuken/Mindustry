eb87fbecc498ebb36a3f3a89282090bb20e16ed5
hortiSquash <45213805+hortiSquash@users.noreply.github.com>
2022-02-08 23:54:48 +0100

    Update LogicDisplay.java

--- core/src/mindustry/world/blocks/logic/LogicDisplay.java
+++ core/src/mindustry/world/blocks/logic/LogicDisplay.java
@@ -103,7 +103,7 @@ public class LogicDisplay extends Block{
             Draw.blend(Blending.disabled);
             Draw.draw(Draw.z(), () -> {
                 if(buffer != null){
-                    Draw.rect(Draw.wrap(buffer.getTexture()), x, y, buffer.getWidth() * scaleFactor * Draw.scl, -buffer.getWidth() * scaleFactor * Draw.scl);
+                    Draw.rect(Draw.wrap(buffer.getTexture()), x, y, buffer.getWidth() * scaleFactor * Draw.scl, -buffer.getHeight() * scaleFactor * Draw.scl);
                 }
             });
             Draw.blend();
