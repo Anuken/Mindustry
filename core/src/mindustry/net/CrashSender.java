5dd75167c765d1f28a2fdebef3ca3ead32e7d3f9
Anuken <arnukren@gmail.com>
2022-02-16 13:26:55 -0500

    More crash info

--- core/src/mindustry/net/CrashSender.java
+++ core/src/mindustry/net/CrashSender.java
@@ -27,7 +27,10 @@ public class CrashSender{
         return report
         + "Version: " + Version.combined() + (Vars.headless ? " (Server)" : "") + "\n"
         + "OS: " + OS.osName + " x" + (OS.osArchBits) + " (" + OS.osArch + ")\n"
+        + ((OS.isAndroid || OS.isIos) && app != null ? "Android API level: " + Core.app.getVersion() + "\n" : "")
         + "Java Version: " + OS.javaVersion + "\n"
+        + "Runtime Available Memory: " + (Runtime.getRuntime().maxMemory() / 1024 / 1024) + "mb\n"
+        + "Cores: " + Runtime.getRuntime().availableProcessors() + "\n"
         + (mods == null ? "<no mod init>" : "Mods: " + (!mods.list().contains(LoadedMod::shouldBeEnabled) ? "none (vanilla)" : mods.list().select(LoadedMod::shouldBeEnabled).toString(", ", mod -> mod.name + ":" + mod.meta.version)))
         + "\n\n" + error;
     }
