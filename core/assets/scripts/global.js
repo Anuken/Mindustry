4b41a914eb48aee4ea2f94440e47022b6db7a0c7
buthed010203 <naguiar010203@gmail.com>
2022-05-26 11:27:22 -0400

    Remove newEffect const from js
    This has been broken since feb of 2020...

--- core/assets/scripts/global.js
+++ core/assets/scripts/global.js
@@ -20,7 +20,6 @@ const cons = method => new Cons(){get: method}
 const prov = method => new Prov(){get: method}
 const func = method => new Func(){get: method}
 
-const newEffect = (lifetime, renderer) => new Effect.Effect(lifetime, new Effect.EffectRenderer({render: renderer}))
 Call = Packages.mindustry.gen.Call
 
 //js 'extend(Base, ..., {})' = java 'new Base(...) {}'
