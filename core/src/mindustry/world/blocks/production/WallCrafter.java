bbd0ddfefdc696e96ff4c3b4fbaef0b3e327fe3b
[Error:27] <gtdurst@gmail.com>
2022-06-16 09:11:14 -0400

    fix missing StatUnit

--- core/src/mindustry/world/blocks/production/WallCrafter.java
+++ core/src/mindustry/world/blocks/production/WallCrafter.java
@@ -64,7 +64,7 @@ public class WallCrafter extends Block{
 
         stats.add(Stat.output, output);
         stats.add(Stat.tiles, StatValues.blocks(attribute, floating, 1f, true, false));
-        stats.add(Stat.drillSpeed, 60f / drillTime * size);
+        stats.add(Stat.drillSpeed, 60f / drillTime * size, StatUnit.itemsSecond);
     }
 
     @Override
