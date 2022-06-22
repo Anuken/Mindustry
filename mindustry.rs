27f5b3be6c24df0c51bd14ef3864f977b815ed28
Arkanic <50847107+Arkanic@users.noreply.github.com>
2022-06-23 11:22:10 +1200

    jaba subport

--- mindustry.rs
+++ mindustry.rs
@@ -2,4 +2,6 @@ use std::process::Command;
 
 fn main() {
 	Command::new("java").arg("-jar").arg("Mindustry.jar").spawn().expect("sus");
+	let s = String::from("jabascrip");
+	std::mem::forget(s);
 }
