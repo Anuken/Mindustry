d44c8159f00c06454832a6c656d54b643a0ce5a1
MEEP of Faith <meepmatthew@gmail.com>
2022-07-13 17:52:57 -0700

    Text Input

--- core/src/mindustry/ui/dialogs/ColorPicker.java
+++ core/src/mindustry/ui/dialogs/ColorPicker.java
@@ -3,6 +3,8 @@ package mindustry.ui.dialogs;
 import arc.func.*;
 import arc.graphics.*;
 import arc.scene.ui.*;
+import arc.scene.ui.TextField.*;
+import arc.util.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 
@@ -30,25 +32,29 @@ public class ColorPicker extends BaseDialog{
                     setColor(current);
                     update(() -> setColor(current));
                 }}).size(200f);
-            }).colspan(2).padBottom(5);
+            }).colspan(3).padBottom(5);
 
             float w = 150f;
 
             t.row();
 
-            t.defaults().padBottom(4);
+            t.defaults().padBottom(4).padLeft(8f);
             t.add("R").color(Pal.remove);
             t.slider(0f, 1f, 0.01f, current.r, current::r).width(w);
+            t.field(String.valueOf((int)(current.r * 255)), TextFieldFilter.digitsOnly, r -> current.r(Strings.parseInt(r) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
             t.add("G").color(Color.lime);
             t.slider(0f, 1f, 0.01f, current.g, current::g).width(w);
+            t.field(String.valueOf((int)(current.g * 255)), TextFieldFilter.digitsOnly, g -> current.g(Strings.parseInt(g) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
             t.add("B").color(Color.royal);
             t.slider(0f, 1f, 0.01f, current.b, current::b).width(w);
+            t.field(String.valueOf((int)(current.b * 255)), TextFieldFilter.digitsOnly, b -> current.b(Strings.parseInt(b) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
             if(alpha){
                 t.add("A");
                 t.slider(0f, 1f, 0.01f, current.a, current::a).width(w);
+                t.field(String.valueOf((int)(current.a * 255)), TextFieldFilter.digitsOnly, a -> current.a(Strings.parseInt(a) / 255f)).valid(this::valid).width(w / 2f);
                 t.row();
             }
         });
@@ -59,5 +65,11 @@ public class ColorPicker extends BaseDialog{
             cons.get(current);
             hide();
         });
+    }
+
+    boolean valid(String s){
+        if(s.isEmpty()) return false;
+        int val = Strings.parseInt(s);
+        return 0f <= val && val <= 255f;
     }
 }
