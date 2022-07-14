386ddb4604684534e4776b6ebcc495c3d139a9bf
MEEP of Faith <meepmatthew@gmail.com>
2022-07-13 18:00:29 -0700

    Center

--- core/src/mindustry/ui/dialogs/ColorPicker.java
+++ core/src/mindustry/ui/dialogs/ColorPicker.java
@@ -39,20 +39,20 @@ public class ColorPicker extends BaseDialog{
             t.row();
 
             t.defaults().padBottom(4).padLeft(8f);
-            t.add("R").color(Pal.remove);
+            t.add("R").color(Pal.remove).padLeft(0f);
             t.slider(0f, 1f, 0.01f, current.r, current::r).width(w);
             t.field(String.valueOf((int)(current.r * 255)), TextFieldFilter.digitsOnly, r -> current.r(Strings.parseInt(r) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
-            t.add("G").color(Color.lime);
+            t.add("G").color(Color.lime).padLeft(0f);
             t.slider(0f, 1f, 0.01f, current.g, current::g).width(w);
             t.field(String.valueOf((int)(current.g * 255)), TextFieldFilter.digitsOnly, g -> current.g(Strings.parseInt(g) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
-            t.add("B").color(Color.royal);
+            t.add("B").color(Color.royal).padLeft(0f);
             t.slider(0f, 1f, 0.01f, current.b, current::b).width(w);
             t.field(String.valueOf((int)(current.b * 255)), TextFieldFilter.digitsOnly, b -> current.b(Strings.parseInt(b) / 255f)).valid(this::valid).width(w / 2f);
             t.row();
             if(alpha){
-                t.add("A");
+                t.add("A").padLeft(0f);
                 t.slider(0f, 1f, 0.01f, current.a, current::a).width(w);
                 t.field(String.valueOf((int)(current.a * 255)), TextFieldFilter.digitsOnly, a -> current.a(Strings.parseInt(a) / 255f)).valid(this::valid).width(w / 2f);
                 t.row();
