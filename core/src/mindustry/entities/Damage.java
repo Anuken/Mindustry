9eaf4dafbd04e0f22912397db2ae070849e40011
MEEP of Faith <meepmatthew@gmail.com>
2022-08-21 11:17:27 -0700

    hittable check for Damage#status

--- core/src/mindustry/entities/Damage.java
+++ core/src/mindustry/entities/Damage.java
@@ -440,7 +440,7 @@ public class Damage{
     /** Applies a status effect to all enemy units in a range. */
     public static void status(Team team, float x, float y, float radius, StatusEffect effect, float duration, boolean air, boolean ground){
         Cons<Unit> cons = entity -> {
-            if(entity.team == team || !entity.within(x, y, radius) || (entity.isFlying() && !air) || (entity.isGrounded() && !ground)){
+            if(entity.team == team || !entity.checkTarget(air, ground) || !entity.hittable() || !entity.within(x, y, radius)){
                 return;
             }
 
