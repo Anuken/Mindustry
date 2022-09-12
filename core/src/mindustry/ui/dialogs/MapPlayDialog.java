f7028536db4457d73f99a70430558477b1b9f349
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-09-12 09:55:57 +0800

    i forgor row

--- core/src/mindustry/ui/dialogs/MapPlayDialog.java
+++ core/src/mindustry/ui/dialogs/MapPlayDialog.java
@@ -51,7 +51,10 @@ public class MapPlayDialog extends BaseDialog{
 
         rules = map.applyRules(selectedGamemode);
 
-        if(!map.description().isEmpty()) cont.add(map.description()).wrap();
+        if(!map.description().isEmpty()){
+            cont.add(map.description()).wrap();
+            cont.row();
+        }
 
         Table selmode = new Table();
         selmode.add("@level.mode").colspan(2);
