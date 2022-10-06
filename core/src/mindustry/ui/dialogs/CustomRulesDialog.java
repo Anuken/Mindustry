b69cf344940d90ec122fea8e88a22676e88fc71a
Anuken <arnukren@gmail.com>
2022-10-06 14:17:55 -0400

    Rule cleanup/clarification

--- core/src/mindustry/ui/dialogs/CustomRulesDialog.java
+++ core/src/mindustry/ui/dialogs/CustomRulesDialog.java
@@ -180,9 +180,9 @@ public class CustomRulesDialog extends BaseDialog{
                 () -> {}, () -> {}
         )).left().width(300f).row();
 
-        check("@rules.hidebannedblocks", b -> rules.hideBannedBlocks = b, () -> rules.hideBannedBlocks);
         main.button("@bannedblocks", () -> showBanned("@bannedblocks", ContentType.block, rules.bannedBlocks, Block::canBeBuilt)).left().width(300f).row();
-        check("@whitelist", b -> rules.blockWhitelist = b, () -> rules.blockWhitelist);
+        check("@rules.hidebannedblocks", b -> rules.hideBannedBlocks = b, () -> rules.hideBannedBlocks);
+        check("@bannedblocks.whitelist", b -> rules.blockWhitelist = b, () -> rules.blockWhitelist);
 
         //TODO objectives would be nice
         if(experimental && false){
@@ -200,7 +200,7 @@ public class CustomRulesDialog extends BaseDialog{
         number("@rules.unitcostmultiplier", f -> rules.unitCostMultiplier = f, () -> rules.unitCostMultiplier);
 
         main.button("@bannedunits", () -> showBanned("@bannedunits", ContentType.unit, rules.bannedUnits, u -> !u.isHidden())).left().width(300f).row();
-        check("@whitelist", b -> rules.unitWhitelist = b, () -> rules.unitWhitelist);
+        check("@bannedunits.whitelist", b -> rules.unitWhitelist = b, () -> rules.unitWhitelist);
 
         title("@rules.title.enemy");
         check("@rules.attack", b -> rules.attackMode = b, () -> rules.attackMode);
