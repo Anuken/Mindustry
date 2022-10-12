2c6c419c9018711f0df741ceeab75f0ade7d7ea8
FlippingPotatoes <75582921+FlippingPotatoes@users.noreply.github.com>
2022-10-12 16:37:40 -0400

    Update LExecutor.java

--- core/src/mindustry/logic/LExecutor.java
+++ core/src/mindustry/logic/LExecutor.java
@@ -512,10 +512,18 @@ public class LExecutor{
                             exec.setobj(p4, null);
                         }else{
                             Tile tile = world.tileWorld(x1, y1);
-                            //any environmental solid block is returned as StoneWall, aka "@solid"
-                            Block block = tile == null ? null : !tile.synthetic() ? (tile.solid() ? Blocks.stoneWall : Blocks.air) : tile.block();
-                            exec.setobj(p3, block);
-                            exec.setobj(p4, tile != null && tile.build != null ? tile.build : null);
+                            if(tile == null){
+                                exec.setobj(p3, null);
+                            }else{
+                                if(floor){
+                                exec.setobj(p3, tile.overlay() != null ? tile.floor() : tile.overlay());
+                                }else{
+                                    //any environmental solid block is returned as StoneWall, aka "@solid"
+                                    Block block = !tile.synthetic() ? (tile.solid() ? Blocks.stoneWall : Blocks.air) : tile.block();
+                                    exec.setobj(p3, block);
+                                    }
+                                exec.setobj(p4, tile != null && tile.build != null ? tile.build : null);
+                            }
                         }
                     }
                     case itemDrop -> {
