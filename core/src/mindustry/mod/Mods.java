c9790bde2acd45e103f26dc4f4bd31fb117639a2
deltanedas <39013340+deltanedas@users.noreply.github.com>
2022-10-30 21:02:55 +0000

    j

--- core/src/mindustry/mod/Mods.java
+++ core/src/mindustry/mod/Mods.java
@@ -32,7 +32,7 @@ import static mindustry.Vars.*;
 
 public class Mods implements Loadable{
     private static final String[] metaFiles = {"mod.json", "mod.hjson", "plugin.json", "plugin.hjson"};
-    private static final ObjectIntMap<String> blacklistedMods = ObjectIntMap.with("ui-lib", 4);
+    private static final ObjectIntMap<String> blacklistedMods = ObjectIntMap.of("ui-lib", 4);
 
     private Json json = new Json();
     private @Nullable Scripts scripts;
@@ -1061,10 +1061,10 @@ public class Mods implements Loadable{
 
         /** Some mods are known to cause issues with the game; this detects and returns whether a mod is manually blacklisted. */
         public boolean isBlacklisted(){
-        	var version = blacklistedMods.get(name);
-        	if(version == null) return false;
+        	if(!blacklistedMods.contains(name)) return false;
 
-        	//check if using patched version
+        	//mod is blacklisted, check if using patched version
+        	var version = blacklistedMods.get(name);
             return Version.isAtLeast(version, 0, meta.version);
         }
 
