b93636267a53c28901e65bc91732feebddb9cadf
way-zer <himc.wicp@gmail.com>
2022-12-19 16:03:15 +0800

    （C/S) Fix TinyLake/MindustryX#4 in `isPathImpassable`. Fix Anuken#7952

--- core/src/mindustry/entities/comp/UnitComp.java
+++ core/src/mindustry/entities/comp/UnitComp.java
@@ -105,7 +105,7 @@ abstract class UnitComp implements Healthc, Physicsc, Hitboxc, Statusc, Teamc, I
     }
 
     public boolean isPathImpassable(int tileX, int tileY){
-        return !type.flying && type.pathCost.getCost(team.id, pathfinder.get(tileX, tileY)) == -1;
+        return !type.flying && world.tile(tileX, tileY) != null && type.pathCost.getCost(team.id, pathfinder.get(tileX, tileY)) == -1;
     }
 
 
