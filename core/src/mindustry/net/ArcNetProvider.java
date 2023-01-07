54a9955e30164ffa3e829be76c57b9f004f3ae1d
Anuken <arnukren@gmail.com>
2023-01-07 12:44:29 -0500

    #8126

--- core/src/mindustry/net/ArcNetProvider.java
+++ core/src/mindustry/net/ArcNetProvider.java
@@ -252,8 +252,9 @@ public class ArcNetProvider implements NetProvider{
     public void discoverServers(Cons<Host> callback, Runnable done){
         Seq<InetAddress> foundAddresses = new Seq<>();
         long time = Time.millis();
+
         client.discoverHosts(port, multicastGroup, multicastPort, 3000, packet -> {
-            Core.app.post(() -> {
+            synchronized(foundAddresses){
                 try{
                     if(foundAddresses.contains(address -> address.equals(packet.getAddress()) || (isLocal(address) && isLocal(packet.getAddress())))){
                         return;
@@ -266,7 +267,7 @@ public class ArcNetProvider implements NetProvider{
                     //don't crash when there's an error pinging a a server or parsing data
                     e.printStackTrace();
                 }
-            });
+            }
         }, () -> Core.app.post(done));
     }
 
