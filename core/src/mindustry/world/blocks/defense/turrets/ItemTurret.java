83625ea9d5749eed8685e269c3d9728f67080c57
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2023-01-30 15:53:17 +0300

    It doesn't work

--- core/src/mindustry/world/blocks/defense/turrets/ItemTurret.java
+++ core/src/mindustry/world/blocks/defense/turrets/ItemTurret.java
@@ -82,17 +82,6 @@ public class ItemTurret extends Turret{
     }
 
     public class ItemTurretBuild extends TurretBuild{
-
-        @Override
-        public void onProximityAdded(){
-            super.onProximityAdded();
-
-            //add first ammo item to cheaty blocks so they can shoot properly
-            if(cheating() && ammoTypes.size > 0){
-                handleItem(this, ammoTypes.entries().next().key);
-            }
-        }
-
         @Override
         public void updateTile(){
             unit.ammo((float)unit.type().ammoCapacity * totalAmmo / maxAmmo);
