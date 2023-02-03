05d7727b3cff950b3234b900a1561c81c4cb3448
MEEP of Faith <meepmatthew@gmail.com>
2023-02-03 12:40:48 -0800

    Payloads go boom.

--- core/src/mindustry/world/blocks/payloads/BuildPayload.java
+++ core/src/mindustry/world/blocks/payloads/BuildPayload.java
@@ -52,6 +52,11 @@ public class BuildPayload implements Payload{
     }
 
     @Override
+    public void destroyed(){
+        build.onDestroyed();
+    }
+
+    @Override
     public ItemStack[] requirements(){
         return build.block.requirements;
     }
