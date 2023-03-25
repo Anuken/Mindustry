0daafc5ff3a51f5fbd046db2f4631083f1c083cc
Anuken <arnukren@gmail.com>
2023-03-25 11:11:06 -0400

    Steam kick bugfixes

--- core/src/mindustry/net/BeControl.java
+++ core/src/mindustry/net/BeControl.java
@@ -151,7 +151,7 @@ public class BeControl{
                         Log.info("&lcAutosaved.");
 
                         netServer.kickAll(KickReason.serverRestarting);
-                        Threads.sleep(32);
+                        Threads.sleep(500);
 
                         Log.info("&lcVersion downloaded, exiting. Note that if you are not using a auto-restart script, the server will not restart automatically.");
                         //replace old file with new
