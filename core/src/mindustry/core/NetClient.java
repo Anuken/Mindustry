bdf1783f7e416a0e5d68f6bf773233e3c45b31dc
code-explorer786 <68312688+code-explorer786@users.noreply.github.com>
2023-03-27 14:37:01 +0700

    Only fire when valid. (Currently deciding on whether or not to add ChatEvent without the player.)

--- core/src/mindustry/core/NetClient.java
+++ core/src/mindustry/core/NetClient.java
@@ -201,13 +201,13 @@ public class NetClient implements ApplicationListener{
             Sounds.chatMessage.play();
         }
 
-        //display raw unformatted text above player head
         if(playersender != null && unformatted != null){
+            //display raw unformatted text above player head
             playersender.lastText(unformatted);
             playersender.textFadeTime(1f);
-        }
 
-        Events.fire(new PlayerChatEvent(playersender, unformatted != null ? unformatted : message != null ? message : ""));
+            Events.fire(new PlayerChatEvent(playersender, unformatted));
+        }
     }
 
     //equivalent to above method but there's no sender and no console log
