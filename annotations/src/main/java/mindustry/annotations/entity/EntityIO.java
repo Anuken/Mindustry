b6f5d424da757cac9b6287121cbdd7ea6be2703a
Anuken <arnukren@gmail.com>
2023-05-30 20:08:48 -0400

    Select command from reconstructor / Units save command when controlled

--- annotations/src/main/java/mindustry/annotations/entity/EntityIO.java
+++ annotations/src/main/java/mindustry/annotations/entity/EntityIO.java
@@ -118,7 +118,7 @@ public class EntityIO{
         }
     }
 
-    void writeSync(MethodSpec.Builder method, boolean write, Seq<Svar> syncFields, Seq<Svar> allFields) throws Exception{
+    void writeSync(MethodSpec.Builder method, boolean write, Seq<Svar> allFields) throws Exception{
         this.method = method;
         this.write = write;
 
@@ -138,6 +138,7 @@ public class EntityIO{
             //add code for reading revision
             for(RevisionField field : rev.fields){
                 Svar var = allFields.find(s -> s.name().equals(field.name));
+                if(var == null || var.has(NoSync.class)) continue;
                 boolean sf = var.has(SyncField.class), sl = var.has(SyncLocal.class);
 
                 if(sl) cont("if(!islocal)");
