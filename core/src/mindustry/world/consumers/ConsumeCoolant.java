e5ded1f2ddf5efb9db03891cad74a333db28efba
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2023-07-25 18:47:46 -0700

    Allow fluid type configuration for ConsumeCoolant (#8841)
    * Allow fluid type configuration for ConsumeCoolant
    
    * Methods

--- core/src/mindustry/world/consumers/ConsumeCoolant.java
+++ core/src/mindustry/world/consumers/ConsumeCoolant.java
@@ -3,10 +3,18 @@ package mindustry.world.consumers;
 /** A ConsumeLiquidFilter that consumes specific coolant, selected based on stats. */
 public class ConsumeCoolant extends ConsumeLiquidFilter{
     public float maxTemp = 0.5f, maxFlammability = 0.1f;
+    public boolean allowLiquid = true, allowGas = false;
 
-    public ConsumeCoolant(float amount){
-        this.filter = liquid -> liquid.coolant && !liquid.gas && liquid.temperature <= maxTemp && liquid.flammability < maxFlammability;
+    public ConsumeCoolant(float amount, boolean allowLiquid, boolean allowGas){
+        this.allowLiquid = allowLiquid;
+        this.allowGas = allowGas;
+
+        this.filter = liquid -> liquid.coolant && (this.allowLiquid && !liquid.gas || this.allowGas && liquid.gas) && liquid.temperature <= maxTemp && liquid.flammability < maxFlammability;
         this.amount = amount;
+    }
+    
+    public ConsumeCoolant(float amount){
+        this(amount, true, false);
     }
 
     public ConsumeCoolant(){
