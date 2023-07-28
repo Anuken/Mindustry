b3d79de2bdda5174ee3e501b515584bc6e9be81e
MEEP of Faith <meepmatthew@gmail.com>
2023-07-28 14:47:57 -0700

    Despawn unit chance

--- core/src/mindustry/entities/bullet/BulletType.java
+++ core/src/mindustry/entities/bullet/BulletType.java
@@ -194,6 +194,8 @@ public class BulletType extends Content implements Cloneable{
     public @Nullable UnitType spawnUnit;
     /** Unit spawned when this bullet hits something or despawns due to it hitting the end of its lifetime. */
     public @Nullable UnitType despawnUnit;
+    /** The chance for despawn units to spawn. */
+    public float despawnUnitChance = 1;
     /** Amount of units spawned when this bullet despawns. */
     public int despawnUnitCount = 1;
     /** Random offset distance from the original bullet despawn/hit coordinate. */
@@ -491,11 +493,13 @@ public class BulletType extends Content implements Cloneable{
     }
 
     public void createUnits(Bullet b, float x, float y){
-        if(despawnUnit != null){
-            for(int i = 0; i < despawnUnitCount; i++){
-                Tmp.v1.rnd(Mathf.random(despawnUnitRadius));
-                var u = despawnUnit.spawn(b.team, x + Tmp.v1.x, y + Tmp.v1.y);
-                u.rotation = faceOutwards ? Tmp.v1.angle() : b.rotation();
+        if(Mathf.chance(despawnUnitChance)){
+            if(despawnUnit != null){
+                for(int i = 0; i < despawnUnitCount; i++){
+                    Tmp.v1.rnd(Mathf.random(despawnUnitRadius));
+                    var u = despawnUnit.spawn(b.team, x + Tmp.v1.x, y + Tmp.v1.y);
+                    u.rotation = faceOutwards ? Tmp.v1.angle() : b.rotation();
+                }
             }
         }
     }
