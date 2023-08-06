b31ba175bd4c93de6ff16a47152ce2951d808d34
Zerenta <80897573+Zerenta@users.noreply.github.com>
2023-08-06 03:19:03 -0400

    Update EnergyFieldAbility.java
    Ty balam

--- core/src/mindustry/entities/abilities/EnergyFieldAbility.java
+++ core/src/mindustry/entities/abilities/EnergyFieldAbility.java
@@ -28,7 +28,7 @@ public class EnergyFieldAbility extends Ability{
     public float x, y;
     public boolean targetGround = true, targetAir = true, hitBuildings = true, hitUnits = true;
     public int maxTargets = 25;
-    public float healPercent = 1f;
+    public float healPercent = 3f;
 
     public float layer = Layer.bullet - 0.001f, blinkScl = 20f, blinkSize = 0.1f;
     public float effectRadius = 5f, sectorRad = 0.14f, rotateSpeed = 0.5f;
