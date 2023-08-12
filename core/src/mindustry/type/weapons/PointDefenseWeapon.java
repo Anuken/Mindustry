a7e8dd126edf2a3e40358582d5a4cc52f61657d9
南门阳德 <66559840+nanmenyangde@users.noreply.github.com>
2023-08-12 23:40:25 +0800

    Fix point defense weapons ignore damage multiplier (#8922)

--- core/src/mindustry/type/weapons/PointDefenseWeapon.java
+++ core/src/mindustry/type/weapons/PointDefenseWeapon.java
@@ -9,6 +9,8 @@ import mindustry.entities.units.*;
 import mindustry.gen.*;
 import mindustry.type.*;
 
+import static mindustry.Vars.*;
+
 /**
  * Note that this requires several things:
  * - A bullet with positive maxRange
@@ -49,8 +51,10 @@ public class PointDefenseWeapon extends Weapon{
     protected void shoot(Unit unit, WeaponMount mount, float shootX, float shootY, float rotation){
         if(!(mount.target instanceof Bullet target)) return;
 
-        if(target.damage() > bullet.damage){
-            target.damage(target.damage() - bullet.damage);
+        // not sure whether it should multiply by the damageMultiplier of the unit
+        float bulletDamage = bullet.damage * unit.damageMultiplier() * state.rules.unitDamage(unit.team);
+        if(target.damage() > bulletDamage){
+            target.damage(target.damage() - bulletDamage);
         }else{
             target.remove();
         }
