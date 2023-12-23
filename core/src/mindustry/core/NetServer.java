2b2ea079dd10ec1abf8c3076336201cc15cb1551
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-12-23 10:18:13 +0530

    Actually, just remove the trace logging

--- core/src/mindustry/core/NetServer.java
+++ core/src/mindustry/core/NetServer.java
@@ -777,9 +777,6 @@ public class NetServer implements ApplicationListener{
                 }else{
                     NetClient.traceInfo(other, info);
                 }
-                if(Config.logTraceRequests.bool()){
-                    info("&lc@ &fi&lk[&lb@&fi&lk]&fb has requested trace info of @ &fi&lk[&lb@&fi&lk]&fb.", player.plainName(), player.uuid(), other.plainName(), other.uuid());
-                }
             }
             case switchTeam -> {
                 if(params instanceof Team team){
