288ab37e6f330b456f6db79fa527dfac1d034079
Anuken <arnukren@gmail.com>
2023-12-28 16:35:54 -0500

    Fixed derelict break hint persisting forever

--- core/src/mindustry/ui/fragments/HintsFragment.java
+++ core/src/mindustry/ui/fragments/HintsFragment.java
@@ -18,6 +18,7 @@ import mindustry.gen.*;
 import mindustry.input.*;
 import mindustry.ui.*;
 import mindustry.world.*;
+import mindustry.world.blocks.ConstructBlock.*;
 import mindustry.world.blocks.payloads.PayloadBlock.*;
 import mindustry.world.meta.*;
 
@@ -86,6 +87,12 @@ public class HintsFragment{
         Events.on(BuildingCommandEvent.class, e -> {
             if(e.building instanceof PayloadBlockBuild<?>){
                 events.add("factorycontrol");
+            }
+        });
+
+        Events.on(BlockBuildBeginEvent.class, e -> {
+            if(e.unit == player.unit() && e.tile.build instanceof ConstructBuild c && c.prevBuild != null && c.prevBuild.contains(b -> b.team == Team.derelict)){
+                events.add("derelictbreak");
             }
         });
     }
@@ -169,7 +176,7 @@ public class HintsFragment{
         conveyorPathfind(() -> control.input.block == Blocks.titaniumConveyor, () -> Core.input.keyRelease(Binding.diagonal_placement) || (mobile && Core.settings.getBool("swapdiagonal"))),
         boost(visibleDesktop, () -> !player.dead() && player.unit().type.canBoost, () -> Core.input.keyDown(Binding.boost)),
         blockInfo(() -> !(state.isCampaign() && state.rules.sector == SectorPresets.groundZero.sector && state.wave < 3), () -> ui.content.isShown()),
-        derelict(() -> ui.hints.events.contains("derelictmouse") && !isTutorial.get(), () -> false),
+        derelict(() -> ui.hints.events.contains("derelictmouse") && !isTutorial.get(), () -> ui.hints.events.contains("derelictbreak")),
         payloadPickup(() -> isSerpulo() && !player.unit().dead && player.unit() instanceof Payloadc p && p.payloads().isEmpty(), () -> player.unit() instanceof Payloadc p && p.payloads().any()),
         payloadDrop(() -> !player.unit().dead && player.unit() instanceof Payloadc p && p.payloads().any(), () -> player.unit() instanceof Payloadc p && p.payloads().isEmpty()),
         waveFire(() -> Groups.fire.size() > 0 && Blocks.wave.unlockedNow(), () -> indexer.getFlagged(state.rules.defaultTeam, BlockFlag.extinguisher).size > 0),
