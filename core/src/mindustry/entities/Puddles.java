b0cc7d2e85b622cf277cf5997df2e88a3052abf5
Anuken <arnukren@gmail.com>
2024-01-12 15:13:11 -0500

    Fixed #9445

--- core/src/mindustry/entities/Puddles.java
+++ core/src/mindustry/entities/Puddles.java
@@ -27,7 +27,7 @@ public class Puddles{
 
     /** Returns the Puddle on the specified tile. May return null. */
     public static @Nullable Puddle get(Tile tile){
-        return world.tiles.getPuddle(tile.array());
+        return tile == null ? null : world.tiles.getPuddle(tile.array());
     }
 
     public static void deposit(Tile tile, Tile source, Liquid liquid, float amount, boolean initial){
