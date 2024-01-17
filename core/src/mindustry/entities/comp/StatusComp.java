9e076df47c185161fde990a98f941173fceedc93
ApsZoldat <apszoldat@gmail.com>
2024-01-17 18:06:33 +0300

    Change javadoc for statusSpeed()

--- core/src/mindustry/entities/comp/StatusComp.java
+++ core/src/mindustry/entities/comp/StatusComp.java
@@ -127,7 +127,7 @@ abstract class StatusComp implements Posc, Flyingc{
         return entry;
     }
 
-    /** Uses a dynamic status effect to override speed. */
+    /** Uses a dynamic status effect to override speed (in tiles/second). */
     public void statusSpeed(float speed){
         //type.speed should never be 0
         applyDynamicStatus().speedMultiplier = speed / (type.speed * 60f / 8f);
