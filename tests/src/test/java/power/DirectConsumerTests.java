89878378e50db0e3d140b24fb8c4a3c1cdf494ee
dloe <dylanloe@gmail.com>
2024-02-04 10:01:44 -0800

    Added Paramterized Tests for both ways of doing it

--- tests/src/test/java/power/DirectConsumerTests.java
+++ tests/src/test/java/power/DirectConsumerTests.java
@@ -7,11 +7,49 @@ import mindustry.world.blocks.power.PowerGenerator.*;
 import mindustry.world.blocks.power.*;
 import mindustry.world.blocks.production.*;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /** Tests for direct power consumers. */
 public class DirectConsumerTests extends PowerTestFixture{
+
+
+    //https://www.arhohuttunen.com/junit-5-parameterized-tests/
+    //Option 1
+    // Uses a method to provide parameters via stream arguments
+    @ParameterizedTest
+    @MethodSource("noPNoItemsParameters")
+    void noPowerRequestedWithNoItemsParameterized(int siliconAmount, int leadAmount, float producedPower, float requestedPower, float expectedSatisfaction)
+    {
+        testUnitFactory(siliconAmount, leadAmount, producedPower,requestedPower, expectedSatisfaction);
+    }
+
+    private static Stream<Arguments> noPNoItemsParameters()
+    {
+        return Stream.of(
+                arguments(0, 0, 0.08f, 0.08f, 1f),
+                arguments(0, 0, 0.08f, 0.08f, 1f),
+                arguments(0, 0, 0.08f, 0.08f, 1f)
+        );
+    }
+
+    //Option 2
+    //https://stackoverflow.com/questions/61483452/parameterized-test-with-two-arguments-in-junit-5-jupiter
+    // CsvSource aka comma seperated source
+    @ParameterizedTest
+    @CsvSource({"0, 0, 0.08f, 0.08f, 1f", "0, 0, 0.08f, 0.08f, 1f", "0, 0, 0.08f, 0.08f, 1f"})
+    public void noPowerRequestedWithNoItemsParameterized2(int siliconAmount, int leadAmount, float producedPower, float requestedPower, float expectedSatisfaction)
+    {
+        testUnitFactory(siliconAmount, leadAmount, producedPower,requestedPower, expectedSatisfaction);
+    }
+
 
     @Test
     void noPowerRequestedWithNoItems(){
