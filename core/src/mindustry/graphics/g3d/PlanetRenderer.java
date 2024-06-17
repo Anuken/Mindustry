049ef415728ef82a9fb68486479a05bf1d7ad5b1
zeutd <107938101+zeutd@users.noreply.github.com>
2024-06-17 11:52:50 +0800

    Update PlanetRenderer.java

--- core/src/mindustry/graphics/g3d/PlanetRenderer.java
+++ core/src/mindustry/graphics/g3d/PlanetRenderer.java
@@ -36,7 +36,7 @@ public class PlanetRenderer implements Disposable{
     //seed: 8kmfuix03fw
     public final CubemapMesh skybox = new CubemapMesh(new Cubemap("cubemaps/stars/"));
 
-    public @Nullable PlanetParams　mainParams；
+    public @Nullable PlanetParams mainParams；
 
     public PlanetRenderer(){
         projector.setScaling(1f / 150f);
