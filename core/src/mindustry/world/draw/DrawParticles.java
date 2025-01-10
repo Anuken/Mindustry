a11827ba062f19b427df39e3ad5ce655d39d5494
Twcash <triarch48@gmail.com>
2025-01-10 07:22:27 -0600

    Forgot rotation

--- core/src/mindustry/world/draw/DrawParticles.java
+++ core/src/mindustry/world/draw/DrawParticles.java
@@ -13,7 +13,7 @@ public class DrawParticles extends DrawBlock{
     public float x = 0, y = 0;
     public float alpha = 0.5f;
     public int particles = 30;
-    public float particleLife = 70f, particleRad = 7f, particleSize = 3f, fadeMargin = 0.4f, rotateScl = 3f;
+    public float particleRotation= 0, particleLife = 70f, particleRad = 7f, particleSize = 3f, fadeMargin = 0.4f, rotateScl = 3f;
     public boolean reverse = false;
     public Interp particleInterp = new PowIn(1.5f);
     public Interp particleSizeInterp = Interp.slope;
@@ -41,7 +41,8 @@ public class DrawParticles extends DrawBlock{
                     build.x + x + Angles.trnsx(angle, len),
                     build.y + y + Angles.trnsy(angle, len),
                     sides,
-                    particleSize * particleSizeInterp.apply(fin) * build.warmup()
+                    particleSize * particleSizeInterp.apply(fin) * build.warmup(),
+                        particleRotation
                 );
             }
 
