3bdfd5b63448c9110432b06778370ee91876e950
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-02-04 12:21:14 -0800

    Fix null crash on startup

--- core/src/mindustry/ui/fragments/MenuFragment.java
+++ core/src/mindustry/ui/fragments/MenuFragment.java
@@ -28,28 +28,7 @@ public class MenuFragment{
     private Button currentMenu;
     private MenuRenderer renderer;
     private Seq<MenuButton> customButtons = new Seq<>();
-    public Seq<MenuButton> desktopButtons = new Seq();
-
-    public MenuFragment(){
-        if(!mobile){
-            desktopButtons = Seq.with(
-                new MenuButton("@play", Icon.play,
-                    new MenuButton("@campaign", Icon.play, () -> checkPlay(ui.planet::show)),
-                    new MenuButton("@joingame", Icon.add, () -> checkPlay(ui.join::show)),
-                    new MenuButton("@customgame", Icon.terrain, () -> checkPlay(ui.custom::show)),
-                    new MenuButton("@loadgame", Icon.download, () -> checkPlay(ui.load::show))
-                ),
-                new MenuButton("@database.button", Icon.menu,
-                    new MenuButton("@schematics", Icon.paste, ui.schematics::show),
-                    new MenuButton("@database", Icon.book, ui.database::show),
-                    new MenuButton("@about.button", Icon.info, ui.about::show)
-                ),
-                new MenuButton("@editor", Icon.terrain, () -> checkPlay(ui.maps::show)), steam ? new MenuButton("@workshop", Icon.steam, platform::openWorkshop) : null,
-                new MenuButton("@mods", Icon.book, ui.mods::show),
-                new MenuButton("@settings", Icon.settings, ui.settings::show)
-            );
-        }
-    }
+    public Seq<MenuButton> desktopButtons = null;
 
     public void build(Group parent){
         renderer = new MenuRenderer();
@@ -208,6 +187,25 @@ public class MenuFragment{
         container.table(background, t -> {
             t.defaults().width(width).height(70f);
             t.name = "buttons";
+
+            if(desktopButtons == null){
+                desktopButtons = Seq.with(
+                    new MenuButton("@play", Icon.play,
+                        new MenuButton("@campaign", Icon.play, () -> checkPlay(ui.planet::show)),
+                        new MenuButton("@joingame", Icon.add, () -> checkPlay(ui.join::show)),
+                        new MenuButton("@customgame", Icon.terrain, () -> checkPlay(ui.custom::show)),
+                        new MenuButton("@loadgame", Icon.download, () -> checkPlay(ui.load::show))
+                    ),
+                    new MenuButton("@database.button", Icon.menu,
+                        new MenuButton("@schematics", Icon.paste, ui.schematics::show),
+                        new MenuButton("@database", Icon.book, ui.database::show),
+                        new MenuButton("@about.button", Icon.info, ui.about::show)
+                    ),
+                    new MenuButton("@editor", Icon.terrain, () -> checkPlay(ui.maps::show)), steam ? new MenuButton("@workshop", Icon.steam, platform::openWorkshop) : null,
+                    new MenuButton("@mods", Icon.book, ui.mods::show),
+                    new MenuButton("@settings", Icon.settings, ui.settings::show)
+                );
+            }
 
             buttons(t, desktopButtons.toArray(MenuButton.class));
             buttons(t, customButtons.toArray(MenuButton.class));
