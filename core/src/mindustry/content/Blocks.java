0b4a7b3b161a0084f76c14d27839d9ef8939f85d
SomeonesShade <SomeonesShade@gmail.com>
2025-02-05 02:24:47 +0800

    Added scaleLifetimeOffset and Centered Ripple Projectiles (#10292)
    * Added scaleLifetimeOffset
    Changed ripple to have its shot centered, calculated by: (1 - velocityRnd/2)^-1
    
    * unneccessary parenthesis
    
    * Update Turret.java
    
    one last adjustement, hopefully

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -3754,6 +3754,7 @@ public class Blocks{
             ammoUseEffect = Fx.casing3Double;
             ammoPerShot = 2;
             velocityRnd = 0.2f;
+            scaleLifetimeOffset = 1f / 9f;
             recoil = 6f;
             shake = 2f;
             range = 290f;
