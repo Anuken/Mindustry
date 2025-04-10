e608a0fea437974e2f4c07019f32325506b4fe06
Anuken <arnukren@gmail.com>
2025-04-10 11:40:47 -0400

    Buildable beam link block

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -2487,11 +2487,12 @@ public class Blocks{
         }};
 
         beamLink = new LongPowerNode("beam-link"){{
-            requirements(Category.power, BuildVisibility.editorOnly, with());
+            requirements(Category.power, with(Items.beryllium, 250, Items.silicon, 250, Items.oxide, 150, Items.carbide, 75, Items.surgeAlloy, 75, Items.phaseFabric, 75));
             size = 3;
             maxNodes = 1;
-            laserRange = 1000f;
+            laserRange = 500f;
             autolink = false;
+            sameBlockConnection = true;
             laserColor2 = Color.valueOf("ffd9c2");
             laserScale = 0.8f;
             scaledHealth = 130;
