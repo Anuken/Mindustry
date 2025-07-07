54fe3c87325bf5c6200fe003f1b8a43495e090e8
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-07-08 00:34:07 +0200

    real

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -4810,8 +4810,7 @@ public class Blocks{
         }};
 
         afflict = new PowerTurret("afflict"){{
-            requirements(Category.turret, with(Items.surgeAlloy, 125, Items.silicon, 200, Items.graphite, 250, Items.oxide, 40));
-            
+            requirements(Category.turret, with(Items.surgeAlloy, 100, Items.silicon, 200, Items.graphite, 250, Items.oxide, 40));
             buildCostMultiplier = 1.5f;
 
             shootType = new BasicBulletType(){{
