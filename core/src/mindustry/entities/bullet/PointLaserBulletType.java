c9be98b4456653cdc9539eb2c26d7df73798a7f4
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-07-14 16:45:10 +0200

    holy shit

--- core/src/mindustry/entities/bullet/PointLaserBulletType.java
+++ core/src/mindustry/entities/bullet/PointLaserBulletType.java
@@ -4,6 +4,7 @@ import arc.*;
 import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
+import arc.math.geom.*;
 import arc.util.*;
 import mindustry.content.*;
 import mindustry.entities.*;
@@ -26,20 +27,24 @@ public class PointLaserBulletType extends BulletType{
     public float damageInterval = 5f;
 
     public float shake = 0f;
+    public float length = 100f;
 
     public PointLaserBulletType(){
-        removeAfterPierce = false;
         speed = 0f;
+        lifetime = 1f;
         despawnEffect = Fx.none;
         lifetime = 20f;
         impact = true;
         keepVelocity = false;
         collides = false;
         pierce = true;
+        pierceBuilding = true;
         hittable = false;
         absorbable = false;
         optimalLifeFract = 0.5f;
         shootEffect = smokeEffect = Fx.none;
+        lifetime = 1f;
+        delayFrags = true;
 
         //just make it massive, users of this bullet can adjust as necessary
         drawSize = 1000f;
@@ -79,10 +84,6 @@ public class PointLaserBulletType extends BulletType{
         updateTrailEffects(b);
         updateBulletInterval(b);
 
-        if(b.timer.get(0, damageInterval)){
-            Damage.collidePoint(b, b.team, hitEffect, b.aimX, b.aimY);
-        }
-
         if(b.timer.get(1, beamEffectInterval)){
             beamEffect.at(b.aimX, b.aimY, beamEffectSize * b.fslope(), hitColor);
         }
@@ -90,6 +91,41 @@ public class PointLaserBulletType extends BulletType{
         if(shake > 0){
             Effect.shake(shake, shake, b);
         }
+    }
+
+    @Override
+    public void handlePierce(Bullet b, float initialHealth, float x, float y){
+        float sub = Math.max(initialHealth * pierceDamageFactor, 0);
+
+        if(b.damage <= 0){
+            b.fdata = Math.min(b.fdata, b.dst(x, y));
+            return;
+        }
+
+        //subtract health from each consecutive pierce
+        b.damage -= Math.min(b.damage, sub);
+
+        //bullet was stopped, decrease furthest distance
+        if(b.damage <= 0f){
+            b.fdata = Math.min(b.fdata, b.dst(x, y));
+        }
+    }
+
+    @Override
+    protected float calculateRange(){
+        return length;
+    }
+
+    @Override
+    public void init(Bullet b){
+        super.init(b);
+
+        b.fdata = length;
+        Damage.collideLine(b, b.team, b.x, b.y, b.rotation(), length, false, false, pierceCap);
+        float resultLen = b.fdata;
+
+        Vec2 nor = Tmp.v1.trns(b.rotation(), 1f).nor();
+        boolean any = b.collided.size > 0;
     }
 
     @Override
@@ -126,5 +162,15 @@ public class PointLaserBulletType extends BulletType{
                 intervalBullet.create(b, b.aimX, b.aimY, ang + Mathf.range(intervalRandomSpread) + intervalAngle + ((i - (intervalBullets - 1f)/2f) * intervalSpread));
             }
         }
+    }
+
+    @Override
+    public boolean testCollision(Bullet bullet, Building tile){
+        return bullet.team != tile.team;
+    }
+
+    @Override
+    public void hitTile(Bullet b, Building build, float x, float y, float initialHealth, boolean direct){
+        handlePierce(b, initialHealth, x, y);
     }
 }
