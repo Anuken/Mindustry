66ff5900b7160c3b1900689bebf067aed11927ef
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-08-25 21:42:13 +0200

    aaa

--- core/src/mindustry/entities/abilities/ShieldArcAbility.java
+++ core/src/mindustry/entities/abilities/ShieldArcAbility.java
@@ -63,7 +63,7 @@ public class ShieldArcAbility extends Ability{
     };
 
     protected static final Cons<Unit> unitConsumer = unit -> {
-        if(unit.team != paramUnit.team &&
+        if(paramField.data > 0 &&
             !(unit.within(paramPos, paramField.radius - paramField.width) && paramPos.within(unit.x - unit.deltaX, unit.y - unit.deltaY, paramField.radius - paramField.width)) &&
             (Tmp.v1.set(unit).add(unit.deltaX, unit.deltaY).within(paramPos, paramField.radius + paramField.width) || unit.within(paramPos, paramField.radius + paramField.width)) &&
             (Angles.within(paramPos.angleTo(unit), paramUnit.rotation + paramField.angleOffset, paramField.angle / 2f) || Angles.within(paramPos.angleTo(unit.x + unit.deltaX, unit.y + unit.deltaY), paramUnit.rotation + paramField.angleOffset, paramField.angle / 2f))){
