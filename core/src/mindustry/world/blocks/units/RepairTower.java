9942ef6b0c5ad6d04402387184c5856af3e755dc
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-08-28 02:38:59 +0200

    almost

--- core/src/mindustry/world/blocks/units/RepairTower.java
+++ core/src/mindustry/world/blocks/units/RepairTower.java
@@ -28,6 +28,7 @@ public class RepairTower extends Block{
         super(name);
         update = true;
         solid = true;
+        suppressable = true;
     }
 
     @Override
@@ -62,6 +63,10 @@ public class RepairTower extends Block{
                         targets.add(u);
                     }
                 });
+            }
+
+            if(checkSuppression()){
+                return;
             }
 
             boolean any = false;
