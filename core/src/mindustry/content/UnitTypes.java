3c1ccc7daaaddfc7be23553198b5acd1f099490f
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-09-21 17:22:53 +0200

    rollback

--- core/src/mindustry/content/UnitTypes.java
+++ core/src/mindustry/content/UnitTypes.java
@@ -2816,7 +2816,7 @@ public class UnitTypes{
             for(float f : new float[] { 34f / 4f, -36f / 4f }){
                 int fi = i ++;
                 weapons.add(new Weapon("vanquish-point-weapon"){{
-                    reload = 30f;
+                    reload = 22f;
                     x = 48f / 4f;
                     y = f;
                     shootY = 5.5f;
