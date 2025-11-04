11e3f522b3e129eb0af492652ae5b410818b3c75
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2025-11-04 12:30:32 -0800

    Apply to generators

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -2503,6 +2503,10 @@ public class Blocks{
             ambientSoundVolume = 0.03f;
             generateEffect = Fx.generatespark;
 
+            explosionShake = 1f;
+            explosionScorchSize = 1;
+            explosionIgnitionChance = 0.1f;
+
             consume(new ConsumeItemFlammable());
             consume(new ConsumeItemExplode());
 
@@ -2531,6 +2535,10 @@ public class Blocks{
 
             ambientSound = Sounds.smelter;
             ambientSoundVolume = 0.06f;
+
+            explosionShake = 1f;
+            explosionScorchSize = 2;
+            explosionIgnitionChance = 0.1f;
 
             consume(new ConsumeItemFlammable());
             consume(new ConsumeItemExplode());
@@ -2600,6 +2608,10 @@ public class Blocks{
             itemDuration = 360f;
             powerProduction = 15f;
             heating = 0.02f;
+
+            explosionShake = 4f;
+            explosionScorchSize = 8;
+            explosionIgnitionChance = 0.5f;
 
             consumeItem(Items.thorium);
             consumeLiquid(Liquids.cryofluid, heating / coolantPower).update(false);
