08fd0b4e1851ac5542fa7b68edaf8259e471bcc8
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-11-09 05:40:26 +0100

    scepter

--- core/src/mindustry/content/UnitTypes.java
+++ core/src/mindustry/content/UnitTypes.java
@@ -200,11 +200,81 @@ public class UnitTypes{
 
             abilities.add(new ShieldRegenFieldAbility(25f, 250f, 60f * 1, 60f));
 
-            BulletType smallBullet = new BasicBulletType(3f, 10){{
-                width = 7f;
-                height = 9f;
-                lifetime = 50f;
-            }};
+            BulletType smallBullet = new RailBulletType(){{
+                    length = 150f;
+                    damage = 15f;
+                    pierceArmor = true;
+                    pierceDamageFactor = 0.8f;
+                    hitColor = Pal.bulletYellowBack;
+                    hitEffect = endEffect = Fx.hitBulletColor;
+
+                    smokeEffect = Fx.colorSpark;
+
+                    endEffect = new Effect(14f, e -> {
+                        color(e.color);
+                        Drawf.tri(e.x, e.y, e.fout() * 1.5f, 5f, e.rotation);
+                    });
+
+                    shootEffect = new MultiEffect(
+                    new Effect(5, e -> {
+                        color(e.color);
+                        float w = 1.2f + 7 * e.fout();
+
+                        Drawf.tri(e.x, e.y, w, 30f * e.fout(), e.rotation);
+                        color(e.color);
+
+                        for(int i : Mathf.signs){
+                            Drawf.tri(e.x, e.y, w * 0.9f, 18f * e.fout(), e.rotation + i * 90f);
+                        }
+
+                        Drawf.tri(e.x, e.y, w, 4f * e.fout(), e.rotation + 180f);
+                    })
+                    ,new Effect(2, e -> {
+                        Color color = Pal.bulletYellow;
+
+                        color(color);
+                        float w = 1.2f + 7 * e.fout();
+
+                        Drawf.tri(e.x, e.y, w, 30f * e.fout(), e.rotation);
+                        color(color);
+
+                        for(int i : Mathf.signs){
+                            Drawf.tri(e.x, e.y, w * 0.9f, 18f * e.fout(), e.rotation + i * 90f);
+                        }
+
+                        Drawf.tri(e.x, e.y, w, 4f * e.fout(), e.rotation + 180f);
+                    }));
+
+                    lineEffect = new Effect(20f, e -> {
+                        if(!(e.data instanceof Vec2 v)) return;
+
+                        color(e.color);
+                        stroke(e.fout() * 0.9f + 0.6f);
+
+                        Fx.rand.setSeed(e.id);
+                        for(int i = 0; i < 7; i++){
+                            Fx.v.trns(e.rotation, Fx.rand.random(8f, v.dst(e.x, e.y) - 8f));
+                            Lines.lineAngleCenter(e.x + Fx.v.x, e.y + Fx.v.y, e.rotation + e.finpow(), e.foutpowdown() * 20f * Fx.rand.random(0.5f, 1f) + 0.3f);
+                        }
+
+                        e.scaled(14f, b -> {
+                            stroke(b.fout() * 1.5f);
+                            color(e.color);
+                            Lines.line(e.x, e.y, v.x, v.y);
+                        });
+                    });
+
+                    fragBullets = 1;
+                    fragBullet = new BombBulletType(27f, 25f){{
+                        width = 8f;
+                        height = 12f;
+                        hitEffect = Fx.flakExplosion;
+                        shootEffect = Fx.none;
+                        smokeEffect = Fx.none;
+
+                        damage = splashDamage * 0.5f;
+                    }};
+                }};
 
             weapons.add(
             new Weapon("scepter-weapon"){{
@@ -222,7 +292,7 @@ public class UnitTypes{
                 shoot.shots = 3;
                 shoot.shotDelay = 4f;
 
-                bullet = new BasicBulletType(8f, 80){{
+                bullet = new BasicBulletType(8f, 70){{
                     width = 11f;
                     height = 20f;
                     lifetime = 27f;
@@ -235,19 +305,21 @@ public class UnitTypes{
                 }};
             }},
 
-            new Weapon("mount-weapon"){{
-                reload = 13f;
+            new Weapon("scepter-mount"){{
+                reload = 10f;
                 x = 8.5f;
                 y = 6f;
                 rotate = true;
+                shootSound = Sounds.shootSnap;
                 ejectEffect = Fx.casing1;
                 bullet = smallBullet;
             }},
-            new Weapon("mount-weapon"){{
-                reload = 16f;
+            new Weapon("scepter-mount"){{
+                reload = 13f;
                 x = 8.5f;
                 y = -7f;
                 rotate = true;
+                shootSound = Sounds.shootSnap;
                 ejectEffect = Fx.casing1;
                 bullet = smallBullet;
             }}
