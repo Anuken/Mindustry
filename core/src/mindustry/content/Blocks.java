3717f33a996134407d53f7415dd6eb4196a1e802
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2025-12-19 22:55:49 +0100

    air factory nerf

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -6194,7 +6194,7 @@ public class Blocks{
         }};
 
         airFactory = new UnitFactory("air-factory"){{
-            requirements(Category.units, with(Items.copper, 60, Items.lead, 70));
+            requirements(Category.units, with(Items.copper, 60, Items.lead, 70, Items.silicon, 60));
             plans = Seq.with(
                 new UnitPlan(UnitTypes.flare, 60f * 15, with(Items.silicon, 15)),
                 new UnitPlan(UnitTypes.mono, 60f * 35, with(Items.silicon, 30, Items.lead, 15))
