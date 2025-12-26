6cdc05444625eb9698f5d08b86b0122d44beb61d
Anuken <arnukren@gmail.com>
2025-12-25 19:02:44 -0500

    Fixed #11493

--- core/src/mindustry/core/Logic.java
+++ core/src/mindustry/core/Logic.java
@@ -204,7 +204,8 @@ public class Logic implements ApplicationListener{
     public void play(){
         state.set(State.playing);
         //grace period of 2x wave time before game starts
-        state.wavetime = (state.rules.initialWaveSpacing <= 0 ? state.rules.waveSpacing * 2 : state.rules.initialWaveSpacing) * (state.isCampaign() ? state.getPlanet().campaignRules.difficulty.waveTimeMultiplier : 1f);;
+        state.wavetime = (state.rules.initialWaveSpacing <= 0 ? state.rules.waveSpacing * 2 : state.rules.initialWaveSpacing) * (state.isCampaign() ? state.getPlanet().campaignRules.difficulty.waveTimeMultiplier : 1f);
+        state.stats = new GameStats();
         Events.fire(new PlayEvent());
 
         //add starting items
