d85c725478178fef9fc6aa6e4e8200b0133d9c5d
Agzam4 <75160721+Agzam4@users.noreply.github.com>
2026-01-15 17:15:09 +0300

    added comment

--- core/src/mindustry/ui/dialogs/JoinDialog.java
+++ core/src/mindustry/ui/dialogs/JoinDialog.java
@@ -407,6 +407,9 @@ public class JoinDialog extends BaseDialog{
         //otherwise use the cached list + the extra servers that may have been included by mods
         var servers = fetchedServers ? defaultServers : tmpServers.clear().addAll(cachedServers).addAll(defaultServers);
         
+        //The system delays sending pings ~100ms
+        //when there are multiple asynchronous calls
+        //so shuffle to every server has a chance to be on top
         servers.shuffle();
         for(int i = 0; i < servers.size; i ++){
             ServerGroup group = servers.get(i);
