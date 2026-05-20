d4cbf1252bbcee393094f8ddf6515e4e43ff9437
Anuken <arnukren@gmail.com>
2026-05-20 02:24:27 -0400

    Redundant check removed

--- core/src/mindustry/world/blocks/ConstructBlock.java
+++ core/src/mindustry/world/blocks/ConstructBlock.java
@@ -386,7 +386,7 @@ public class ConstructBlock extends Block{
                         int target = Mathf.round(requirements[i].amount * state.rules.buildCostMultiplier * state.rules.deconstructRefundMultiplier);
                         int remaining = target - itemsLeft[i];
 
-                        if(requirements[i].item.unlockedNowHost() && !state.rules.infiniteResources){
+                        if(requirements[i].item.unlockedNowHost()){
                             core.items.add(requirements[i].item, Mathf.clamp(remaining, 0, core.storageCapacity - core.items.get(requirements[i].item)));
                         }
                         itemsLeft[i] = target;
