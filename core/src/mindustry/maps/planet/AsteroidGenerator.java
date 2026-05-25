b841b01eaf3c7015db740252c36a0760cd0198f3
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2026-05-25 02:16:08 +0200

    remove old stupid comment from pull

--- core/src/mindustry/maps/planet/AsteroidGenerator.java
+++ core/src/mindustry/maps/planet/AsteroidGenerator.java
@@ -147,8 +147,6 @@ public class AsteroidGenerator extends BlankPlanetGenerator{
         state.rules.borderDarkness = false;
         state.rules.waves = true;
 
-        //TODO ???
-        //state.rules.hiddenBuildItems.addAll(Items.plastanium, Items.surgeAlloy);
         //TODO maybe make this off by default everywhere
         state.rules.hideSpawns = false;
         //TODO better wavegen, do it by hand even
