afccfcd1600081a803452d8f10c1a640ae215c30
Pyratite No Lead <1164806786@qq.com>
2026-06-01 22:45:21 +0800

    Add a scalable interface for TypeIO IV

--- core/src/mindustry/io/TypeObject.java
+++ core/src/mindustry/io/TypeObject.java
@@ -6,6 +6,7 @@ import arc.util.io.*;
 
 public interface TypeObject{
     IntMap<Func<Reads, Object>> readers = new IntMap<>();
+    IntMap<String> names = new IntMap<>();
 
     static Func<Reads, Object> handler(int i){
         return readers.get(i);
@@ -14,9 +15,12 @@ public interface TypeObject{
     /**
      * Registers a custom type reader by id. This is mostly used for mods that need to transmit custom data.
      * @param id a mod-specific, unique id for identifying this type object.
-     * */
-    static void register(int id, Func<Reads, Object> reader){
+     * @param name a mod-specific, unique name for identifying this type object. Prefix preferred,
+     */
+    static void register(int id, String name, Func<Reads, Object> reader){
+        if(readers.get(id) != null) throw new IllegalArgumentException("Two type object cannot have the same id! (issue: '" + name + "' and '" + names.get(id) + "')");
         readers.put(id, reader);
+        names.put(id, name);
     }
 
     int objectID();
