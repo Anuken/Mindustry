deb8068a2c9d18804ef9f922380c59f6550f9187
Rylid-Umil <81439905+rylid-umil@users.noreply.github.com>
2026-06-14 16:38:00 -0700

    Okay okay, one last change.

--- core/src/mindustry/type/Weapon.java
+++ core/src/mindustry/type/Weapon.java
@@ -121,8 +121,10 @@ public class Weapon implements Cloneable{
     public boolean ignoreRotation = false;
     /** If true, this weapon cannot be used to attack targets. */
     public boolean noAttack = false;
-    /** min velocity required for this weapon to shoot */
+    /** min velocity required for this weapon to shoot. -1 to disable the limit. */
     public float minShootVelocity = -1f;
+    /** max velocity at which this weapon to shoot. -1 to disable the limit. */
+    public float maxShootVelocity = -1f;
     /** should the shoot effects follow the unit (effects need followParent set to true for this to work) */
     public boolean parentizeEffects;
     /** internal value used for alternation - do not change! */
@@ -438,7 +440,7 @@ public class Weapon implements Cloneable{
         !(bullet.killShooter && mount.totalShots > 0) && //if the bullet kills the shooter, you should only ever be able to shoot once
         (!alternate || wasFlipped == flipSprite) &&
         mount.warmup >= minWarmup && //must be warmed up
-        velLen >= minShootVelocity && //check velocity requirements
+        velLen >= minShootVelocity && (maxShootVelocity == -1 || velLen <= maxShootVelocity)  && //check velocity requirements
         (mount.reload <= 0.0001f || (alwaysContinuous && mount.bullet == null)) && //reload has to be 0, or it has to be an always-continuous weapon
         (alwaysShooting || Angles.within(rotate ? mount.rotation : unit.rotation + baseRotation, mount.targetRotation, shootCone)) //has to be within the cone
         ){
