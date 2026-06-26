32e95a69a9657863b7519f570f19987bb714cce7
Cardillan <122014763+cardillan@users.noreply.github.com>
2026-06-26 20:40:57 +0200

    Fix mod settings not displayed correctly (#12255)

--- core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -353,7 +353,7 @@ public class SettingsMenuDialog extends BaseDialog{
         menu.button("@settings.data", Icon.save, style, isize, () -> dataDialog.show()).marginLeft(marg).row();
         menu.button("@settings.dev", Icon.fileCode, style, isize, () -> visible(3)).marginLeft(marg).row();
 
-        int i = 3;
+        int i = 4;
         for(var cat : categories){
             int index = i;
             if(cat.icon == null){
