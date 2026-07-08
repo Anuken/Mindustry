492dd719976f8c086654c14f24896196e98e208c
m1cxzfw3q <mcxzfwq@outlook.com>
2026-07-08 21:48:25 +0800

    test

--- core/src/mindustry/input/DesktopInput.java
+++ core/src/mindustry/input/DesktopInput.java
@@ -970,6 +970,14 @@ public class DesktopInput extends InputHandler{
         float ya = Core.input.axis(Binding.moveY);
         boolean boosted = (unit instanceof Mechc && unit.isFlying());
 
+        boolean canShoot = false;
+        for(var weapon : unit.type.weapons){
+            if(weapon.canShootWhenBoosting){
+                canShoot = true;
+                break;
+            }
+        }
+
         if(settings.getBool("detach-camera")){
             Vec2 targetPos = camera.position;
 
@@ -987,7 +995,7 @@ public class DesktopInput extends InputHandler{
         }
 
         float mouseAngle = Angles.mouseAngle(unit.x, unit.y);
-        boolean aimCursor = omni && player.shooting && unit.type.hasWeapons() && unit.type.faceTarget && !boosted;
+        boolean aimCursor = omni && player.shooting && unit.type.hasWeapons() && unit.type.faceTarget && (!boosted || canShoot);
 
         if(aimCursor){
             unit.lookAt(mouseAngle);
