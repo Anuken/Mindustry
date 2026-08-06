5f77b10bf9b675400641149a9925aae56d58a016
JasonP01 <131086642+JasonP01@users.noreply.github.com>
2026-08-06 19:20:35 +0200

    fix status value

--- core/src/mindustry/world/meta/StatValues.java
+++ core/src/mindustry/world/meta/StatValues.java
@@ -724,6 +724,14 @@ public class StatValues{
 
                     if(type.lightning > 0){
                         sep(bt, Core.bundle.format("bullet.lightning", type.lightning, type.lightningDamage < 0 ? type.damage : type.lightningDamage));
+                        // show lightning-specific status chance override if present
+                        if(type.status != StatusEffects.none && type.lightningStatusChance > -1f){
+                            sep(bt,
+                                    (type.lightningStatusChance < 1f ? Core.bundle.format("stat.chance", Strings.autoFixed(type.lightningStatusChance * 100f, 4)) : "") +
+                                            (type.status.hasEmoji() ? type.status.emoji() + " " : "") + "[stat]" + type.status.localizedName +
+                                            (type.status.reactive ? "" : "[lightgray] ~ [stat]" + Strings.autoFixed(type.statusDuration / 60f, 1) + "[lightgray] " + Core.bundle.get("unit.seconds"))
+                            ).with(c -> withTooltip(c, type.status));
+                        }
                     }
 
                     if(type instanceof LaserBulletType b && b.lightningSpacing > 0){
