699a379a3f382c876a13fac793bab14aeadfa0eb
redloong <mirens648@gmail.com>
2026-08-16 10:02:31 +0200

    new

--- core/src/mindustry/logic/LStatements.java
+++ core/src/mindustry/logic/LStatements.java
@@ -1500,7 +1500,7 @@ public class LStatements{
 
             row(table);
 
-            table.add("effect ");
+            table.add("effect");
             fields(table, effect, str -> effect = str).left();
         }
 
