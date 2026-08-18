9fa446c52b289137b025e817bdc76b777d85a47f
m1cxzfw3q <mcxzfwq@outlook.com>
2026-08-19 05:28:45 +0800

    update

--- core/src/mindustry/world/blocks/payloads/BuildPayload.java
+++ core/src/mindustry/world/blocks/payloads/BuildPayload.java
@@ -62,9 +62,7 @@ public class BuildPayload implements Payload{
     public void update(@Nullable Unit unitHolder, @Nullable Building buildingHolder){
         if(unitHolder != null && (!build.block.updateInUnits || (!state.rules.unitPayloadUpdate && !build.block.alwaysUpdateInUnits))) return;
 
-        Tile payloadTile = emptyTile.payload();
-        payloadTile.x = (short) (build.x / 8);
-        payloadTile.y = (short) (build.y / 8);
+        Tile payloadTile = new Tile((int) (build.x / 8), (int) (build.y / 8)).payload();
         payloadTile.setBlock(build.block, build.team, build.rotation, () -> build);
 
         build.tile = payloadTile;
