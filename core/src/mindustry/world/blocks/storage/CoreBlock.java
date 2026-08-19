ce1d5bfe1af1fa4dda4b889e2b2cd9eff8e2dea1
m1cxzfw3q <mcxzfwq@outlook.com>
2026-08-19 23:03:23 +0800

    update

--- core/src/mindustry/world/blocks/storage/CoreBlock.java
+++ core/src/mindustry/world/blocks/storage/CoreBlock.java
@@ -91,9 +91,8 @@ public class CoreBlock extends StorageBlock{
     }
 
     @Remote(called = Loc.server)
-    public static void playerSpawn(CoreBuild core, Player player){
-        if(player == null || core.tile == null) return;
-
+    public static void playerSpawn(Tile tile, Player player){
+        if(player == null || tile == null || !(tile.build instanceof CoreBuild core)) return;
 
         UnitType spawnType = ((CoreBlock)core.block).unitType;
         if(core.wasVisible){
@@ -103,7 +102,7 @@ public class CoreBlock extends StorageBlock{
         player.set(core);
 
         if(!net.client()){
-            Unit unit = spawnType.create(core.team());
+            Unit unit = spawnType.create(tile.team());
             //reset reload so that the player can't shoot immediately
             for(var mount : unit.mounts){
                 mount.reload = mount.weapon.reload;
@@ -587,7 +586,7 @@ public class CoreBlock extends StorageBlock{
             //do not try to respawn in unsupported environments at all
             if(!unitType.supportsEnv(state.rules.env) || !allowSpawn) return;
 
-            Call.playerSpawn(this, player);
+            Call.playerSpawn(tile, player);
         }
 
         @Override
