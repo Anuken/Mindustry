dd5f319ba445b3d5806f1440b2e80b3131c86ece
redloong <mirens648@gmail.com>
2026-09-01 10:35:20 +0200

    add

--- core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
+++ core/src/mindustry/ui/dialogs/SettingsMenuDialog.java
@@ -558,6 +558,7 @@ public class SettingsMenuDialog extends BaseDialog{
 
         graphics.checkPref("skipcoreanimation", false);
         graphics.checkPref("hidedisplays", false);
+        graphics.checkPref("logiclocalization", true);
 
         if(OS.isMac){
             graphics.checkPref("macnotch", false);
