80f04b8f76d9304fd4211e9cde94a2447c503270
Shade <someonesshade@gmail.com>
2026-09-02 07:47:29 +0800

    Mass Reversion

--- core/src/mindustry/content/Blocks.java
+++ core/src/mindustry/content/Blocks.java
@@ -3622,7 +3622,6 @@ public class Blocks{
                 //for visual stats only.
                 buildingDamageMultiplier = 0.4f;
                 pierceCap = 5;
-                pierceBuilding = true;
 
                 lightningType = new BulletType(0.0001f, 0f){{
                     lifetime = Fx.lightning.lifetime;
