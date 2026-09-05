ca3c0f30b86ab141eaa2e5185e7ae120c179395c
Anuken <arnukren@gmail.com>
2026-09-05 14:09:24 -0400

    Fixed tests

--- tests/src/test/java/ModTestAllure.java
+++ tests/src/test/java/ModTestAllure.java
@@ -17,6 +17,8 @@ public class ModTestAllure extends GenericModTest{
 
     @Test
     public void begin(){
+        //As of September 5, the Allure repository has been deleted or made private. I do not know why.
+        if(true) return;
         grabMod("https://github.com/LixieWulf/Allure/archive/c94e83e07ab918c0402b8f3897bba832e42432f0.zip");
         checkExistence("allure");
 
