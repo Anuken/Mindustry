4368c6e60eece15bb15db22f66f88b42d00bef79
Anuken <arnukren@gmail.com>
2026-09-13 21:20:47 -0400

    Fixed #12657

--- core/src/mindustry/world/blocks/power/ThermalGenerator.java
+++ core/src/mindustry/world/blocks/power/ThermalGenerator.java
@@ -44,6 +44,15 @@ public class ThermalGenerator extends PowerGenerator{
     }
 
     @Override
+    public void afterPatch(){
+        super.afterPatch();
+        if(outputLiquid != null){
+            outputsLiquid = true;
+            hasLiquids = true;
+        }
+    }
+
+    @Override
     public void setStats(){
         super.setStats();
 
@@ -82,7 +91,7 @@ public class ThermalGenerator extends PowerGenerator{
                 generateEffect.at(x + Mathf.range(3f), y + Mathf.range(3f));
             }
 
-            if(outputLiquid != null){
+            if(outputLiquid != null && hasLiquids){
                 float added = Math.min(productionEfficiency * delta() * outputLiquid.amount, liquidCapacity - liquids.get(outputLiquid.liquid));
                 liquids.add(outputLiquid.liquid, added);
                 dumpLiquid(outputLiquid.liquid);
