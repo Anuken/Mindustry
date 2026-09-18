2e2a7fa6bc53da8c51e5c61766bdbad2a0d04b30
cardillan <122014763+cardillan@users.noreply.github.com>
2026-09-18 07:36:20 +0200

    Moved the specialized unsigned long parsing routine from Strings directly to LAssembler

--- core/src/mindustry/logic/LAssembler.java
+++ core/src/mindustry/logic/LAssembler.java
@@ -107,31 +107,48 @@ public class LAssembler{
     }
 
     double parseDouble(String symbol){
-        //trivial optimization: avoid unnecessary parsing of logic constants
-        if(symbol.isEmpty() || symbol.charAt(0) == '@') return Double.NaN;
+        //fail fast for obvious non-numbers
+        if(symbol.isEmpty() || !isNumStart(symbol.charAt(0))) return Double.NaN;
 
         //parse hex/binary syntax
-        if(symbol.startsWith("0b")) return parseLong(false, symbol, 2, 2, symbol.length());
-        if(symbol.startsWith("+0b")) return parseLong(false, symbol, 2, 3, symbol.length());
-        if(symbol.startsWith("-0b")) return parseLong(true,symbol,  2, 3, symbol.length());
-        if(symbol.startsWith("0x")) return parseLong(false,symbol,  16, 2, symbol.length());
-        if(symbol.startsWith("+0x")) return parseLong(false,symbol,  16, 3, symbol.length());
-        if(symbol.startsWith("-0x")) return parseLong(true,symbol,  16, 3, symbol.length());
+        if(symbol.startsWith("0b")) return parseHexOrBin(false, symbol, true, 2);
+        if(symbol.startsWith("+0b")) return parseHexOrBin(false, symbol, true, 3);
+        if(symbol.startsWith("-0b")) return parseHexOrBin(true, symbol, true, 3);
+        if(symbol.startsWith("0x")) return parseHexOrBin(false, symbol, false, 2);
+        if(symbol.startsWith("+0x")) return parseHexOrBin(false, symbol, false, 3);
+        if(symbol.startsWith("-0x")) return parseHexOrBin(true, symbol, false, 3);
         if(symbol.startsWith("%[") && symbol.endsWith("]") && symbol.length() > 3) return parseNamedColor(symbol);
         if(symbol.startsWith("%") && (symbol.length() == 7 || symbol.length() == 9)) return parseColor(symbol);
 
         return Strings.parseDouble(symbol, Double.NaN);
     }
 
-    double parseLong(boolean negative, String s, int radix, int start, int end){
-        if(start >= end) return Double.NaN;
-        while(start < end && s.charAt(start) == '0') start++;
-        if(start == end) return 0; //one or more zeroes
+    boolean isNumStart(char c){
+        //note that 'e10' isn't a valid number; '%ffffff' is. Hex numbers start with '0x'.
+        return c >= '0' && c <= '9' || c == '.' || c == '-' || c == '+' || c == '%';
+    }
 
-        //parseUnsignedLong will refuse a leading plus or minus sign, which is desired here
-        //using zero for invalid value, as zero can't be returned at this point
-        long l = Strings.parseUnsignedLong(s, radix, start, end, 0);
-        return l == 0 ? Double.NaN : negative ? -l : l;
+    //parses *unsigned* hex or bin number, including negative ones (0xffffffffffffffff as -1)
+    //detects overflow by input length and uses bit manipulation to avoid signed arithmetics
+    double parseHexOrBin(boolean negative, String s, boolean binary, int offset){
+        int end = s.length();
+        if(offset >= end) return Double.NaN;
+
+        int pos = offset;
+        while(pos < end && s.charAt(pos) == '0') pos ++;    //skip leading zeros to avoid incorrect overflow detection
+
+        int shift = binary ? 1 : 4;
+        if(end - pos > 64 / shift) return Double.NaN;
+
+        long acc = 0;
+        int radix = 1 << shift;
+        while(pos < end){
+            int digit = Character.digit(s.charAt(pos), radix);
+            if(digit < 0) return Double.NaN;
+            acc = acc << shift | digit;
+            pos ++;
+        }
+        return negative ? -acc : acc;
     }
 
     double parseColor(String symbol){
