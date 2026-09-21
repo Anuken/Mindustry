318ca1cc8ef21d1fd23d5d4767b3bbca4c5a8a76
Anuken <arnukren@gmail.com>
2026-09-21 15:42:20 -0400

    Cleanup

--- core/src/mindustry/world/Block.java
+++ core/src/mindustry/world/Block.java
@@ -1206,14 +1206,6 @@ public class Block extends UnlockableContent implements Senseable{
         return consume;
     }
 
-    public void setupRequirements(Category cat, ItemStack[] stacks){
-        requirements(cat, stacks);
-    }
-
-    public void setupRequirements(Category cat, BuildVisibility visible, ItemStack[] stacks){
-        requirements(cat, visible, stacks);
-    }
-
     public void requirements(Category cat, ItemStack[] stacks, boolean unlocked){
         requirements(cat, BuildVisibility.shown, stacks);
         this.alwaysUnlocked = unlocked;
